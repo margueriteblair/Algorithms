f0f12487e2e81d5482041528777d6bf2b7a46bfa
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-10 23:37:08 -0400

    datatypes

---
+++ 30DaysOfCode/datatypes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class datatypes {
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "Hackerrank ";
+
+        Scanner scanner = new Scanner(System.in);
+
+        int i2 = scanner.nextInt();
+        int d2 = scanner.nextDouble();
+        scan.nextLine();
+        String s2 = scanner.nextLine();
+
+        System.out.println(i+i2);
+        System.out.println(d+d2);
+        System.out.println(s+s2);
+    }
+}
