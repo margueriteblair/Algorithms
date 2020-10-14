689e657f0d0456f255603f4d90d8c8af047b5f28
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-13 21:45:30 -0400

    interjecting second array arg into first at a given index

---
+++ basic-fcc/slicesplice.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class slicesplice {
+    public static void main(String[] args) {
+        spliceAndSlice(new int[] {1, 2, 3}, new int[] {4, 5}, 1);
+        //frankenSplice([1, 2, 3], [4, 5], 1)
+    }
+
+    public static void spliceAndSlice(int[] arr1, int[] arr2, int n) {
+        int[] finalArr = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, finalArr, 0, n);
+        System.arraycopy(arr2, 0, finalArr, n, arr2.length);
+        System.arraycopy(arr1, n, finalArr, (n + arr2.length), arr1.length - 1);
+        // Arrays.copyOfRange(arr1, 0, n) Arrays.copyOfRange(arr2, 0, arr2.length - 1);
+        System.out.println(Arrays.toString(finalArr));
+    }
+}
