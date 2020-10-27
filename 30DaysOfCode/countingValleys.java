e839fe2f2d8fad2adb546992031183b054a36dbd
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-27 18:21:20 -0400

    counting valleys

---
+++ 30DaysOfCode/countingValleys.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+    // Write your code here
+        int total = 0; //0 is sea level
+        int valleyCount = 0;
+
+        for (String step : path.split("")) {
+            if (step.equals("U")) {
+                total++;
+            } else {
+                total--;
+            }
+            if (total == 0 && step.equals("U")) {
+                valleyCount++;
+            }
+        }
+    
+        return valleyCount;
+        
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- 30DaysOfCode/dd.java
+++ 30DaysOfCode/dd.java
@@ -4,10 +4,11 @@ public class dd {
     public static void main(String[] args) {
         parsedamageDie("2d4");
         parsedamageDie("31d44");
+        parsedamageDie("d20");
         
     }
 
     public static void parsedamageDie(String damageDie) {
-        System.out.println(Arrays.toString(damageDie.split("d")));
+        System.out.println((damageDie.split("d")[0]) == "");
     }
 }
