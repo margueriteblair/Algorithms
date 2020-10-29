86d8a1cb121181450e21b0633acf5610103c8ae3
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-29 19:31:08 -0400

    apples and oranges

---
+++ hackerrank-java/blackjack.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class blackjack {
+    public static void main(String[] args) {
+        String[] suites = {"Spades", "Diamonds", "Clubs", "Hearts"};
+        
+        
+
+  
+    }
+}
