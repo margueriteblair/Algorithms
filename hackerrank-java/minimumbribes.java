28df9eb2d54a8b014c566c89f41109bd3ff578d6
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-29 21:39:35 -0400

    more concise version of the algorithm

--- hackerrank-java/minimumbribes.java
+++ hackerrank-java/minimumbribes.java
@@ -10,25 +10,19 @@ public class Solution {
 
     // Complete the minimumBribes function below.
     static void minimumBribes(int[] q) {
-        
+        int swaps = 0;
         for(int i=0; i< q.length; i++){
             if((q[i] - (i+1)) > 2){
                 System.out.println("Too chaotic");
                 return; 
-            }
-        }
-        int swaps=0;   
-        for(int i=0; i < q.length; i++){
-            for(int j=i+1; j < q.length; j++){
-                if(q[i] > q[j]){ 
-                    int tmp=q[j];
-                    q[j]=q[i];
-                    q[i]=tmp;
+            } 
+            for (int j = i+1; j < q.length; j++) {
+                if(q[i] > q[j]) {
                     swaps++;
+                    }
                 }
-            }
+            
         }
-        
         System.out.println(swaps);
 
     }
