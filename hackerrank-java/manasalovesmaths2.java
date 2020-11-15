93dbc3f3ca8f2d69e4f8e098dcf3ba0b5a175921
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-15 17:04:38 -0500

    troubleshooting for the longs

---
+++ hackerrank-java/manasalovesmaths2.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+    
+
+
+public class manasalovesmaths2 {
+    static List<String> permList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println(solve("78")); //this is correct
+        System.out.println(solve("344447401973121441077768855432158702762861375430"));
+        // System.out.println(solve("684235100752110815577359844276924659182357568871521286361018621208416841520627779"));
+
+    }
+
+    // Complete the solve function below.
+    static String solve(String n) {
+        generatePermutation(n, 0, n.length());
+        System.out.println(permList);
+        if (permList.size() > 0) {
+            permList.clear();
+            return "YES";
+        }
+        return "NO";
+}
+
+public static String swapString(String a, int i, int j) {  
+
+    char[] b =a.toCharArray();  
+    char ch;  
+    ch = b[i];  
+    b[i] = b[j];  
+    b[j] = ch;  
+    return String.valueOf(b);  
+}
+
+public static void generatePermutation(String str, int start, int end) {  
+    //Prints the permutations  
+    if (start == end-1 && Integer.parseInt(str) % 8 == 0) {
+        permList.add(str); 
+    } else {  
+        for (int i = start; i < end; i++) {  
+            str = swapString(str,start,i);  
+            generatePermutation(str,start+1,end);  
+            str = swapString(str,start,i);  
+        } 
+
+    }  
+}
+}
