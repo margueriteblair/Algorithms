a1b4a76579a2f8ae2dfe8aa181259f2ed7d94b9a
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-15 02:17:59 -0500

    summing different combinations in a sub array but maintaining contiguity

---
+++ hackerrank-java/subArraySummation.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int numNegArrays = 0;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(scanner.next());
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum < 0) {
+                    numNegArrays++;
+                }
+            }
+        }
+        
+        System.out.println(numNegArrays);
+        
+        
+    }
+}
