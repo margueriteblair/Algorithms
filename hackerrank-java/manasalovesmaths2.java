77b90e533d517d014a6569ff726d3a786a748bdd
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-15 17:32:51 -0500

    new big integer method of doing this

--- hackerrank-java/manasalovesmaths2.java
+++ hackerrank-java/manasalovesmaths2.java
@@ -9,13 +9,14 @@ import java.math.BigInteger;
 
 public class manasalovesmaths2 {
     static List<String> permList = new ArrayList<>();
+    static BigInteger eight = new BigInteger("8");
 
     public static void main(String[] args) {
         System.out.println(solve("78")); //this is correct
-        var A = new BigInteger("344447401973121441077768855432158702762861375430");
-        System.out.println(A.remainder(new BigInteger("8")));
+        // var A = new BigInteger("344447401973121441077768855432158702762861375430");
+        // System.out.println(A.remainder(new BigInteger("8")));
         // A.remainder(8);
-        // System.out.println(solve("344447401973121441077768855432158702762861375430"));
+        System.out.println(solve("344447401973121441077768855432158702762861375430"));
         // System.out.println(solve("684235100752110815577359844276924659182357568871521286361018621208416841520627779"));
 
     }
@@ -43,7 +44,7 @@ public static String swapString(String a, int i, int j) {
 
 public static void generatePermutation(String str, int start, int end) {  
     //Prints the permutations  
-    if (start == end-1 && Integer.parseInt(str) % 8 == 0) {
+    if (start == end-1 && new BigInteger(str).remainder(eight) == BigInteger.ZERO) {
         permList.add(str); 
     } else {  
         for (int i = start; i < end; i++) {  
