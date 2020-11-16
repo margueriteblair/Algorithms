68d73530fa331cd110bd6d3d965e35de0501c191
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-16 14:11:00 -0500

    ice cream parlor algorithm using a hashmap

---
+++ hackerrank-java/icecreamparlor.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the whatFlavors function below.
+    static void whatFlavors(int[] cost, int money) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < cost.length; i++) {
+            int otherIndex = money - cost[i];
+            if (map.containsKey(otherIndex)) {
+                System.out.println((map.get(otherIndex) + 1) + " " + (i+1));
+            }
+            map.put(cost[i], i);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int money = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] cost = new int[n];
+
+            String[] costItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int costItem = Integer.parseInt(costItems[i]);
+                cost[i] = costItem;
+            }
+
+            whatFlavors(cost, money);
+        }
+
+        scanner.close();
+    }
+}
