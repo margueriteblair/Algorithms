956dc13a8cf187e08fdba39763f2d4730dbe17be
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-20 16:16:43 -0500

    comparator interface

---
+++ hackerrank-java/comparatorinterface.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Player {
+	String name;
+	int score;
+
+	Player(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+}
+
+class Checker implements Comparator<Player> {
+  	// complete this method
+    @Override
+	public int compare(Player a, Player b) {
+        if (a.score > b.score) {
+            return -1;
+        } else if (a.score == b.score) {
+            return a.name.compareTo(b.name);
+            // return 0;
+        } else {
+            return 1;
+        }
+    
+    }
+}
+
