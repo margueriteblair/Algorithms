d559cfad2024eb62ebc6670f6b3d7da6f9664248
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-22 12:29:35 -0500

    proper solution that matches all test cases converted int array to integer array

--- hackerrank-java/arraypermuations.java
+++ hackerrank-java/arraypermuations.java
@@ -1,80 +1,13 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
-public class Solution {
-
-    // Complete the twoArrays function below.
-    static String twoArrays(int k, int[] A, int[] B) {
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] + B[i] < k) {
-                int j = i;
-                while (A[i] + B[j] != k) {
-                    if (j == A.length-1) {
-                        return "NO";
-                    }
-                    j++;
-                }
-                int temp = B[i];
-                B[i] = B[j];
-                B[j] = temp;
-                // System.out.println(Arrays.toString(A));
-                // System.out.println(Arrays.toString(B));
-                twoArrays(k, A, B);
-            }
+static String twoArrays(Integer k, Integer[] A, Integer[] B) {
+    Arrays.sort(A);
+    Arrays.sort(B, Collections.reverseOrder());
+    for (int i = 0; i < A.length; i++) {
+        if (A[i] + B[i] < k) {
+            return "NO";
         }
-        return "YES";
-
-
     }
+    return "YES";
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int q = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int qItr = 0; qItr < q; qItr++) {
-            String[] nk = scanner.nextLine().split(" ");
-
-            int n = Integer.parseInt(nk[0]);
-
-            int k = Integer.parseInt(nk[1]);
-
-            int[] A = new int[n];
-
-            String[] AItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int i = 0; i < n; i++) {
-                int AItem = Integer.parseInt(AItems[i]);
-                A[i] = AItem;
-            }
-
-            int[] B = new int[n];
-
-            String[] BItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int i = 0; i < n; i++) {
-                int BItem = Integer.parseInt(BItems[i]);
-                B[i] = BItem;
-            }
-
-            String result = twoArrays(k, A, B);
-
-            bufferedWriter.write(result);
-            bufferedWriter.newLine();
-        }
-
-        bufferedWriter.close();
-
-        scanner.close();
-    }
+//note for this program to work, you'll need to change all the instances of primitive int to Integer
+//you cannot directly sort in reverse order with Arrays.sort(B, Collections.reverseOrder()) with a primitive type array!
 }
