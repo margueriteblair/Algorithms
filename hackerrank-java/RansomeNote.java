e23d396783fa5761ef4f8bef169e2dbe57f98027
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-22 13:42:28 -0500

    cleanup because the sorts arent necessary

--- hackerrank-java/RansomeNote.java
+++ hackerrank-java/RansomeNote.java
@@ -1,7 +1,6 @@
 public class RansomNote {
     static void checkMagazine(String[] magazine, String[] note) {
-        Arrays.sort(note);
-        Arrays.sort(magazine);
+
         List<String> magazineList = new ArrayList<>(Arrays.asList(magazine));
     
         for (String word : note) {
