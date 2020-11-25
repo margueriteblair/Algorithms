e291ad2a3dae89e57f5d441aa79886ff7f2ca15b
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-25 16:33:17 -0500

    one line edit to include case where the number is actually 0

--- hackerrank-java/DArray.java
+++ hackerrank-java/DArray.java
@@ -1,12 +1,13 @@
 public class DArray {
     public static void main(String[] args) {
-       System.out.println(hasZero(90));
+       System.out.println(hasZero(0));
        System.out.println(hasZero(88));
 
     }
 
     static boolean hasZero(int num) {
         int rem;
+        if (num == 0) return true;
         while (num > 0) {
             rem = num % 10;
             if (rem == 0)
