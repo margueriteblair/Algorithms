9a68f23b2f93730321f15b31084ee0e9ee8ac828
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-25 16:37:02 -0500

    actual solution at the bottom

--- hackerrank-java/DArray.java
+++ hackerrank-java/DArray.java
@@ -17,3 +17,23 @@ public class DArray {
         return false;
     }
 }
+
+public static boolean canWin(int leap, int[] game) {
+    // Return true if you can win the game; otherwise, return false.
+    return isSolvable(leap, game, 0);
+}
+private static boolean isSolvable(int leap, int[] game, int i) {
+// Base Cases
+if (i >= game.length) {
+    return true;
+} else if (i < 0 || game[i] == 1) {
+    return false;
+}
+        
+game[i] = 1; // marks as visited
+
+// Recursive Cases
+return isSolvable(leap, game, i + leap) || 
+       isSolvable(leap, game, i + 1) || 
+       isSolvable(leap, game, i - 1);
+}
