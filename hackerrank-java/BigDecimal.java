cf3733cab936aa4153da5caca20e1c2020c45072
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-28 18:28:42 -0500

    new version of sorting with big decimal and comparator object

--- hackerrank-java/BigDecimal.java
+++ hackerrank-java/BigDecimal.java
@@ -42,3 +42,46 @@ class Solution{
         }
     }
 }
+
+//second version of the algorithm
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+    // Arrays.sort(s, Collections.reverseOrder(new Comparator<String>() {
+    //     @Override
+    //     public int compare(String str1, String str2) {
+    //         BigDecimal bg1 = new BigDecimal(str1);
+    //         BigDecimal bg2 = new BigDecimal(str2);
+    //         return bg1.compareTo(bg2);
+    //     }
+    // }));
+    Arrays.sort(s, Collections.reverseOrder(new Comparator<String>() {
+    @Override
+    public int compare(String a1, String a2) {
+        if (a1 == null || a2 == null) {
+            return 0;
+        }
+        BigDecimal a = new BigDecimal(a1);
+        BigDecimal b = new BigDecimal(a2);
+        return a.compareTo(b);
+    }
+}));
+        
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
