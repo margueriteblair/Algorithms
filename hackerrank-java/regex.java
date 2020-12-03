75e156b76c7dd77ebfec85b756ce45de76c8031c
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-03 14:37:47 -0500

    second potential solution

--- hackerrank-java/regex.java
+++ hackerrank-java/regex.java
@@ -18,3 +18,7 @@ class MyRegex {
     String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
     public String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
 }
+
+class myRegex{
+    public String pattern = "((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]))\\b";
+}
