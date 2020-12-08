80ca12d8abce24dafe565428dfcaed8c365dc5d5
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-08 17:26:33 -0500

    second version of the program

--- hackerrank-java/DavisSteps.java
+++ hackerrank-java/DavisSteps.java
@@ -48,3 +48,61 @@ public class Solution {
         scanner.close();
     }
 }
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the stepPerms function below.
+public static int stepPerms(int n) {
+        int[] array = new int[n];
+        if (n == 1) {
+            return 1;
+        }
+        else if(n == 2) {
+            return 2;
+        }
+        else if(n == 3) {
+            return 4;
+        }
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 4;
+        for (int i = 3; i < n; i++) {
+            array[i] = array[i-1] + array[i-2] + array[i-3];
+        }
+        return array[array.length-1];
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int s = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int sItr = 0; sItr < s; sItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int res = stepPerms(n);
+
+            bufferedWriter.write(String.valueOf(res));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
