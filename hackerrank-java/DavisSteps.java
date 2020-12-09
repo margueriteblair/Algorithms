319e375559a74b0ebf0cc860b78b267ff7c37b37
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-09 15:02:25 -0500

    commenting out other solution so i can actually run

--- hackerrank-java/DavisSteps.java
+++ hackerrank-java/DavisSteps.java
@@ -1,53 +1,53 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+// import java.io.*;
+// import java.math.*;
+// import java.security.*;
+// import java.text.*;
+// import java.util.*;
+// import java.util.concurrent.*;
+// import java.util.regex.*;
 
-public class Solution {
+// public class Solution {
 
-    // Complete the stepPerms function below.
-    private static Map<Integer, Integer> map = new HashMap<>();
-        private static int stepPerms(int n) {
-        if(n < 0)
-            return 0;
+//     // Complete the stepPerms function below.
+//     private static Map<Integer, Integer> map = new HashMap<>();
+//         private static int stepPerms(int n) {
+//         if(n < 0)
+//             return 0;
 
-        if(n == 0)
-            return 1;
+//         if(n == 0)
+//             return 1;
 
-        if(!map.containsKey(n)) {
-            int count = stepPerms(n-1) + stepPerms(n-2) + stepPerms(n-3);
-            map.put(n, count);
-        }
-    return map.get(n);
-}
+//         if(!map.containsKey(n)) {
+//             int count = stepPerms(n-1) + stepPerms(n-2) + stepPerms(n-3);
+//             map.put(n, count);
+//         }
+//     return map.get(n);
+// }
 
 
-    private static final Scanner scanner = new Scanner(System.in);
+//     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//     public static void main(String[] args) throws IOException {
+//         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int s = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//         int s = scanner.nextInt();
+//         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int sItr = 0; sItr < s; sItr++) {
-            int n = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//         for (int sItr = 0; sItr < s; sItr++) {
+//             int n = scanner.nextInt();
+//             scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-            int res = stepPerms(n);
+//             int res = stepPerms(n);
 
-            bufferedWriter.write(String.valueOf(res));
-            bufferedWriter.newLine();
-        }
+//             bufferedWriter.write(String.valueOf(res));
+//             bufferedWriter.newLine();
+//         }
 
-        bufferedWriter.close();
+//         bufferedWriter.close();
 
-        scanner.close();
-    }
-}
+//         scanner.close();
+//     }
+// }
 
 
 import java.io.*;
