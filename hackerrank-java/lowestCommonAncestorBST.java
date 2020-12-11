40536b375130ca89ec529cd008d90e902f8f2369
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-10 20:07:33 -0500

    lowest common ancestors

---
+++ hackerrank-java/lowestCommonAncestorBST.java
@@ -0,0 +1,12 @@
+public static Node lca(Node root, int v1, int v2) {
+    if(root.data < v1 && root.data < v2){
+        return lca(root.right,v1,v2);
+    }
+    //Bigger than both
+    if(root.data > v1 && root.data > v2){
+        return lca(root.left,v1,v2);
+    }
+
+    //Else solution already found
+    return root;
+    }
