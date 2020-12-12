f337322f312d27d3a039230963e91f569caf23c2
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-12 00:27:43 -0500

    version one

--- BSTrees/LevelOrderTraversal.java
+++ BSTrees/LevelOrderTraversal.java
@@ -1,41 +1,9 @@
-import java.io.*;
-import java.util.*;
-class Node{
-    Node left,right;
-    int data;
-    Node(int data){
-        this.data=data;
-        left=right=null;
-    }
+Queue<Node> queue = new LinkedList();
+queue.add(root);
+
+while(!queue.isEmpty()){
+    Node current = queue.remove();
+    System.out.print(current.data+" ");
+    if (current.left!=null) queue.add(current.left);
+    if (current.right!=null) queue.add(current.right);
 }
-class Solution{
-
-	static void levelOrder(Node root){
-        int h = height(root);
-        for (int i = 0; i < h; i++) {
-            printLevel(root, i);
-        }
-      
-    }
-    
-    static int height(Node root) {
-        int heightL;
-        int heightR;
-        if (root == null) return 0;
-        else {
-            heightL = height(root.left);
-            heightR = height(root.right);
-            return heightR > heightL ? heightR+1 : heightL+1;
-        }
-    }
-    
-    static void printLevel(Node root, int level) {
-        if (root == null) return;
-        if (level == 1) System.out.print(root.data + " ");
-        if (level > 1) {
-            printLevel(root.left, level - 1);
-            printLevel(root.right, level - 1);
-        }
-    }
-
-	public static Node insert(Node root,int data){
