ff9d96b5a10786ca50a567147b419393e0e3fc3f
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-17 15:51:35 -0500

    array reversal algo

---
+++ hackerrank-java/ArrayReversal.java
@@ -0,0 +1,15 @@
+public class ArrayReversal {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    static int[] reverseArray(int[] a) {
+        for (int i = 0; i < a.length/2; i++) {
+            int temp = a[i];
+            a[i] = a[a.length-1-i];
+            a[a.length-1-i] = temp;
+        }
+        return a;
+    }
+}
