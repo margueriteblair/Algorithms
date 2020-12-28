0ad5720fcdaccb4c6d4e941a1a22601da6969aab
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-28 16:35:05 -0500

    testing with regular integer division

--- hackerrank-java/PracticeWithNumbers.java
+++ hackerrank-java/PracticeWithNumbers.java
@@ -1,7 +1,9 @@
 public class PracticeWithNumbers {
     public static void main(String[] args) {
-        System.out.println(10/0); //returns ArithmeticException
+        // System.out.println(10/0); //returns ArithmeticException
         System.out.println(10/0.0); //returns Infinity
+        System.out.println(10/30f); //returns .33333
+        System.out.println(10/30); //returns 0
     }
     
 }
