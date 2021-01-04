30b91dd2870dc9693e7dd1662441257f248155bb
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-04 13:01:05 -0500

    simple array sum

---
+++ hackerrank-java/SumArray.java
@@ -0,0 +1,10 @@
+public class SumArray {
+    static int simpleArraySum(int[] ar) {
+        int sum = 0;
+        for (int i = 0; i < ar.length; i++) {
+            sum += ar[i];
+        }
+        return sum;
+
+    }
+}
