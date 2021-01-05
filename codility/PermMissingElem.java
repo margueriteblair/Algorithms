62cb0890e8e45055610e2616f3a345b60efa9c9a
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-04 21:28:26 -0500

    two functioning solutions to perm missing elem

--- codility/PermMissingElem.java
+++ codility/PermMissingElem.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 public class PermMissingElem {
     public static void main(String[] args) {
-        missing(new int[] {2, 4, 1, 3, 6});
+        // missing(new int[] {2, 4, 1, 3, 6});
+        missing2(new int[] {2, 4, 1, 3, 6});
     }
     public static int missing(int[] A) {
         Arrays.sort(A);
@@ -14,6 +15,18 @@ public class PermMissingElem {
             }
         }
         return missing;
+    }
+
+    public static int missing2(int[] A) {
+        //sum{1---n+1}-sum(inputArr)
+        int sumInput = 0;
+        for (int num : A) {
+            sumInput += num;
+        }
+        int sumSeq = (int)(Math.pow(A.length+1, 2)+A.length+1)/2;
+        int missingNum = sumSeq-sumInput;
+        System.out.println(missingNum);
+        return missingNum;
     }
 
 }
