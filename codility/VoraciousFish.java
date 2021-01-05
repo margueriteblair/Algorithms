8669f968d84217e4c5ab9bb743f23be119d51fa1
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-05 15:08:22 -0500

    second solution

--- codility/VoraciousFish.java
+++ codility/VoraciousFish.java
@@ -25,4 +25,25 @@ public class VoraciousFish {
         // System.out.println(Arrays.toString(A));
         return survivors;
     }
+
+    public int solution2(int[] A, int[] B) {
+        Stack<Integer> stacky = new Stack<>();
+        int survivors = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (B[i] == 1) {
+                stacky.push(A[i]);
+            } else {
+                int weighDown = stacky.isEmpty() ? -1  : stacky.pop();
+                while (weighDown != -1 && weighDown < A[i]) {
+                    weighDown = stacky.isEmpty() ? -1  : stacky.pop();
+                }
+                if (weighDown == -1) {
+                    survivors++;
+                } else {
+                    stacky.push(weighDown);
+                }
+            }
+        }
+        return survivors+stacky.size();
+    }
 }
