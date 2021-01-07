0319168ff2b5ee472cf4e2f43509c198a6847df4
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-07 15:00:15 -0500

    passing cars algorithm part 2 but written more closely to how i would have interpreted the problem

--- codility/PassingCars.java
+++ codility/PassingCars.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class PassingCars {
     public static void main(String[] args) {
         solution(new int[] {0, 1, 0, 1, 1});
@@ -10,10 +12,30 @@ public class PassingCars {
         for (int i = A.length-1; i >= 0; i--) {
             sumsArr[i] = A[i] + sumsArr[i+1];
         }
+        System.out.println(Arrays.toString(sumsArr));
         for (int i = 0; i < A.length; i++) {
             if (A[i] == 0) passingCount+=sumsArr[i];
         }
 
         return passingCount > 1000000000 ? -1 : passingCount;
+    }
+
+    public static int solution2(int[] A) {
+        int[] sumsArr = new int[A.length];
+        int sumsSum = 0;
+        for (int i = A.length-1; i >= 0; i--) {
+            sumsArr[i] = A[i] + sumsSum;
+            sumsSum += A[i];
+        }
+        int count = 0;
+        for (int i= 0; i < A.length; i++) {
+            if (A[i] == 0) {
+                count += sumsArr[i];
+            }
+        }
+        if (count > 1000000000) {
+            return -1;
+        } 
+        return count;
     }
 }
