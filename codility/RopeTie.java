78f1d7cc7ebd872cafe628f4ae5d012751082e66
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-07 22:59:16 -0500

    putting an instance in the main method

--- codility/RopeTie.java
+++ codility/RopeTie.java
@@ -1,6 +1,6 @@
 public class RopeTie {
     public static void main(String[] args) {
-        
+        solution(4, new int[] {1,2,3,4,5,6,7,8,9});
     }
 
     public static int solution(int K, int[] A) {
