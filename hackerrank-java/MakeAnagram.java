03478a07adc580c29c209c267f440652a54de07a
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-09 13:54:38 -0500

    second version

--- hackerrank-java/MakeAnagram.java
+++ hackerrank-java/MakeAnagram.java
@@ -1,12 +1,40 @@
+public class MakeAnagram {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int solution(String a, String b) {
+        int[] freq = new int[26];
+        a.chars().forEach((c) -> {
+            freq[c - 97]++;
+        });
+        b.chars().forEach((c) -> {
+            freq[c - 97]--;
+        });
+        
+        return Arrays.stream(freq).map(Math::abs).sum();
+    }
+
+    static int makeAnagram(String a, String b) {
+        Map<Character, Integer> count = new HashMap<>();
+        for( char ch: a.toCharArray() ) {
+            int ct = count.containsKey(ch) ? count.get(ch) : 0;
+            count.put(ch, (ct + 1));
+        }
+        
+        for( char ch: b.toCharArray() ) {
+            int ct = count.containsKey(ch) ? count.get(ch) : 0;
+            count.put(ch, (ct - 1));
+        }
+        
+        List<Integer> values = new ArrayList<>( count.values() );
+        int total = 0;
+        for( Integer v: values ) {
+            total += Math.abs(v);
+        }
+        return total;
+    }
+
+}
 
 
-
-int[] freq = new int[26];
-a.chars().forEach((c) -> {
-    freq[c - 97]++;
-});
-b.chars().forEach((c) -> {
-    freq[c - 97]--;
-});
-
-return Arrays.stream(freq).map(Math::abs).sum();
