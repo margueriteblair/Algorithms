c556edf71fe1591adf459c635b4111b1afe08200
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-09 12:33:16 -0500

    very time inefficient solution to seeing how many anagram substrings exist within a string

---
+++ hackerrank-java/SherlockAnagram2.java
@@ -0,0 +1,56 @@
+public class SherlockAnagram2 {
+    public static void main(String[] args) {
+        
+    }
+
+    static int sherlockAndAnagrams(String s) {
+        char[] charArr = s.toCharArray();
+        int pairsCount = 0;
+        List<String> subStringList = new ArrayList<>();
+        for (int len = 1; len <= charArr.length; len++) {
+            for (int i = 0; i <= charArr.length-len; i++) {
+                int j = i + len - 1;
+                String currString = "";
+                for (int k = i; k <= j; k++) {
+                    currString += String.valueOf(charArr[k]);
+                }
+                subStringList.add(currString);
+
+            }
+        }
+        
+        for (int i = 0; i < subStringList.size(); i++) {
+            for (int j = i+1; j < subStringList.size(); j++) {
+                if (isMatching(subStringList.get(i), subStringList.get(j)) == true) {
+                    pairsCount++;
+                }
+            }
+        }
+        return pairsCount;
+        
+        
+        }
+        
+        static boolean isMatching(String s1, String s2) {
+            HashMap<Character, Integer> s1Map = new HashMap<>();
+            HashMap<Character, Integer> s2Map = new HashMap<>();
+            
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1Map.containsKey(s1.charAt(i))) {
+                    s1Map.put(s1.charAt(i), s1Map.get(s1.charAt(i))+1);
+                } else {
+                    s1Map.put(s1.charAt(i), 1);
+                }
+            }
+            
+            for (int i = 0; i < s2.length(); i++) {
+                if (s2Map.containsKey(s2.charAt(i))) {
+                    s2Map.put(s2.charAt(i), s2Map.get(s2.charAt(i))+1);
+                } else {
+                    s2Map.put(s2.charAt(i), 1);
+                }
+            }
+            
+            return s1Map.equals(s2Map);
+        }
+}
