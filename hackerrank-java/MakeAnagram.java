9f65346b2bef8c28f00d4fa90b966136d55b89f2
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-09 13:38:54 -0500

    make anagram algorithm

---
+++ hackerrank-java/MakeAnagram.java
@@ -0,0 +1,8 @@
+int[] freq = new int[26];
+a.chars().forEach((c) -> {
+    freq[c - 97]++;
+});
+b.chars().forEach((c) -> {
+    freq[c - 97]--;
+});
+return Arrays.stream(freq).map(Math::abs).sum();
