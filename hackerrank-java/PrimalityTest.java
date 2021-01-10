fb78430121499f1c223d603094f6d2d3c0093e35
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-09 21:39:41 -0500

    adding the stipulation that if n is greater than one then it cannot be prime

--- hackerrank-java/PrimalityTest.java
+++ hackerrank-java/PrimalityTest.java
@@ -4,6 +4,7 @@ public class PrimalityTest {
     }    
     
     public static String primality(int n) {
+        if (n == 1) return "Not prime";
         for (int i = 2; i < Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return "Not prime";
