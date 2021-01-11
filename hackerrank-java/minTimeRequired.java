c128e2fbce70be64f284037b8f5910efaeb6f0e0
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-11 10:45:18 -0500

    rough draft but exceeds time limits on hackerrank

---
+++ hackerrank-java/minTimeRequired.java
@@ -0,0 +1,20 @@
+public class minTimeRequired {
+    public static void main(String[] args) {
+        
+    }
+
+    static long minTime(long[] machines, long goal) {
+        long itemCount = 0;
+        long day = 1;
+        // Arrays.sort(machines);
+        while (itemCount < goal) {
+            for (int i = 0; i < machines.length; i++) {
+            if (day % machines[i] == 0) {
+                itemCount++;
+            }
+        }
+        day++;
+        }
+        return day-1;
+    }
+}
