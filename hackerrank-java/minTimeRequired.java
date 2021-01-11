b7c8aea4bf55c19438777a0cb5fa854bdda9e129
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-11 11:46:28 -0500

    correct and time efficient answer using a search method

--- hackerrank-java/minTimeRequired.java
+++ hackerrank-java/minTimeRequired.java
@@ -20,4 +20,26 @@ public class minTimeRequired {
         }
         return day-1;
     }
+
+    static long minTime(long[] machines, long goal) {
+        Arrays.sort(machines);
+        long max = machines[machines.length - 1];
+        long minDays = 0;
+        long maxDays = max*goal;
+        long result = -1;
+        while (minDays < maxDays) {
+            long mid = (minDays + maxDays) / 2;
+            long unit = 0;
+            for (long machine : machines) {
+                unit += mid / machine;
+            }
+            if (unit < goal) {
+                minDays = mid+1;
+            } else {
+                result = mid;
+                maxDays = mid;
+            }
+        }
+        return result;
+    }
 }
