e8b5eff0fd894b978447bea43ac43ae7fd6cf319
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-13 18:53:46 -0500

    first draft solves about 50 percent of test cases

---
+++ hackerrank-java/ArrayManipulation.java
@@ -0,0 +1,19 @@
+public class ArrayManipulation {
+    public static void main(String[] args) {
+        
+    }
+
+    static long arrayManipulation(int n, int[][] queries) {
+        int[] resultingQueries = new int[n];
+        long maxManipulation = 0;
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                resultingQueries[j-1] += queries[i][2];
+                if (resultingQueries[j-1] > maxManipulation) {
+                    maxManipulation = resultingQueries[j-1];
+                }
+            }
+        }
+        return maxManipulation;
+    }
+}
