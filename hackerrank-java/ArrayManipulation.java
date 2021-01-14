e76ff9465150d35f10a95b85e31c0ca8d700180e
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-13 19:34:05 -0500

    second solution

--- hackerrank-java/ArrayManipulation.java
+++ hackerrank-java/ArrayManipulation.java
@@ -16,4 +16,22 @@ public class ArrayManipulation {
         }
         return maxManipulation;
     }
+
+    static long arrayManipulation2(int n, int[][] queries) {
+        long[] resultingQueries = new long[n+2];
+        long maxManipulation = 0;
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            int k = queries[i][2];
+            resultingQueries[a] += k;
+            resultingQueries[b+1] -= k;
+        }
+        long max = Long.MIN_VALUE;
+        for (int i = 0; i < resultingQueries.length; i++) {
+                maxManipulation += resultingQueries[i];
+                max = Math.max(max, maxManipulation);
+            }
+        return max;
+    }
 }
