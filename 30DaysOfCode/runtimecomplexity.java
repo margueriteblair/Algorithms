0fa7543e1a926470aeb6679ad474526a8c7d4f3c
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-14 18:17:13 -0500

    this was very silly of me i figured it out

--- 30DaysOfCode/runtimecomplexity.java
+++ 30DaysOfCode/runtimecomplexity.java
@@ -13,4 +13,20 @@ public class runtimecomplexity {
             else System.out.println("Prime");
         }
     }
+
+    public static void main2(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        while (N-- > 0) {
+            int testNum = scanner.nextInt();
+            int decidingFactor = 0;
+            for (int i = 2; i <= Math.sqrt(testNum); i++) {
+                if (testNum % i == 0) {
+                    decidingFactor = 1;
+                }
+            }
+            if (decidingFactor == 0 && testNum != 1) System.out.println("Prime");
+            else System.out.println("Not prime");
+        }
+    }
 }
