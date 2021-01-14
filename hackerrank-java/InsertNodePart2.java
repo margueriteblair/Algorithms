69a6bd6b0d5b164a83c5c58e4d8b68f6f84333d4
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-14 11:30:55 -0500

    a few updates

--- hackerrank-java/InsertNodePart2.java
+++ hackerrank-java/InsertNodePart2.java
@@ -4,28 +4,24 @@ public class InsertNodePart2 {
     }
 
     static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
-        SinglyLinkedListNode trackedHeadNode = head;
+            SinglyLinkedListNode trackedHeadNode = head;
             SinglyLinkedListNode nodeToInsert = new SinglyLinkedListNode(data);
-            nodeToInsert.data = data;
-            //Empty List - Returned newly created node or null
+
             if (head==null){
                 return nodeToInsert;
             }
-            //Inserting a Node ahead of the List
-            if (position == 0){nodeToInsert.next = head; return nodeToInsert;}    
-            //Traverse the Singly Linked List to 1 Position Prior
-            //Stop traversing if you reached the end of the List
-            int currPosition = 0;
+            if (position == 0){
+                nodeToInsert.next = head; return nodeToInsert;
+            }    
+            int i = 0;
             while (currPosition < position -1 && head.next != null){
                 head = head.next;        
-                currPosition++;       
+                i++;       
             }
-            //Inserting a Node in-between a List or at the end of of a List
             SinglyLinkedListNode nodeAtPosition = head.next;
             head.next = nodeToInsert;
             head = head.next;
             head.next = nodeAtPosition;
             return trackedHeadNode;
-    
         }
 }
