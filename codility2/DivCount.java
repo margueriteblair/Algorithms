36e9d2bc141309933145e5d1839cb863e961797a
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-22 13:13:01 -0500

    more correct anser

--- codility2/DivCount.java
+++ codility2/DivCount.java
@@ -2,8 +2,9 @@ public class DivCount {
     public static void main(String[] args) {
         solution(6, 11, 2);
     }
-    public static int solution(int A, int B, int K) {
-        int base = (B-A) / K;
-        return A % K != 0 ? base : base + 1;
+    public int solution(int A, int B, int K) {
+        double nStart = Math.ceil(A / (double) K);
+        double nEnd = Math.floor(B / (double) K);
+        return (int) (nEnd - nStart) + 1;
     }
 }
