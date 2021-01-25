1d86e27a18f7e0cdbe6a84a8c556dafd86d96727
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-24 19:30:18 -0500

    counting triplets solution

--- hackerrank-java/CountingTriplets.java
+++ hackerrank-java/CountingTriplets.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class CountingTriplets {
     public static void main(String[] args) {
-        
+        System.out.println(Math.pow(100, 3));
     }
 
     static long countTriplets(List<Long> arr, long r) {
@@ -24,5 +26,37 @@ public class CountingTriplets {
         }
         System.out.println(countMap);
         return count;
+    }
+
+    static long countTriplets2(List<Long> arr, long r) {
+        Map<Long, Long> leftMap = new HashMap<>();
+        Map<Long, Long> rightMap = new HashMap<>();
+
+    for (long item : arr) {
+        rightMap.put(item, rightMap.getOrDefault(item, 0L) + 1);
+    }
+
+    long count = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+        long midTerm = arr.get(i);
+        long c1 = 0, c3 = 0;
+
+        rightMap.put(midTerm, rightMap.getOrDefault(midTerm, 0L) - 1);
+
+        if (leftMap.containsKey(midTerm / r) && midTerm % r == 0) {
+            c1 = leftMap.get(midTerm / r);
+        }
+
+        if (rightMap.containsKey(midTerm * r))
+            c3 = rightMap.get(midTerm * r);
+
+        count += c1 * c3;
+
+        leftMap.put(midTerm, leftMap.getOrDefault(midTerm, 0L) + 1);
+
+    }
+    return count;
+
     }
 }
