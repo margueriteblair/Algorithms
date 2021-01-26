6e1c210e49f550bb26eefc07b795f2cc37437e3a
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-25 22:55:48 -0500

    first draft

---
+++ hackerrank-java/ChocolateBarProblem.java
@@ -0,0 +1,20 @@
+public class ChocolateBarProblem {
+    public static void main(String[] args) {
+        
+    }
+
+    static int birthday(List<Integer> s, int d, int m) {
+        int possibilities = 0;
+        for (int i = 0; i < s.size()-m; i++) {
+            int sum = 0;
+            for (int j = i; j < m+i; j++) {
+                sum += s.get(j);
+            }
+            if (sum == d) {
+                possibilities++;
+            }
+        }
+        return possibilities;
+
+    }
+}
