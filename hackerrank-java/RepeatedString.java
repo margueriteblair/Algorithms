1e5f59388b110dd487d428ee36d469a8899b7925
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-25 21:55:14 -0500

    repeated string

--- hackerrank-java/RepeatedString.java
+++ hackerrank-java/RepeatedString.java
@@ -4,13 +4,20 @@ public class RepeatedString {
     }
 
     static long repeatedString(String s, long n) {
-        String newStr = StringUtils.repeat(s, (int)n);
-        System.out.println(newStr);
-        if (s.length() == 1 && s.equals("a")) return n;
-        long aCount = 0;
-        for (long i = 0; i < n; i++) {
-            if (newStr.charAt((int)i) == 'a') aCount++;
+        long count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                count++;
+            }
         }
-        return aCount;
+        long repititions = n / (long)s.length();
+        count *= repititions;
+        
+        for (int i = 0; i < n % s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                count++;
+            }
+        }
+        return count;
     }
 }
