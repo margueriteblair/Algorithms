4235b2d23a8aefd7f1b64f8204f8d4bd921a0519
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-26 14:25:25 -0500

    acm but redone a bit to be how I would code it

--- hackerrank-java/ACMTeam.java
+++ hackerrank-java/ACMTeam.java
@@ -2,25 +2,24 @@ public class ACMTeam {
     public static void main(String[] args) {
         
     }
-
-        // Complete the acmTeam function below.
-        int count = 1,max = Integer.MIN_VALUE;
-        for(int i=0;i<topic.length-1;i++){
-            for(int j=i+1;j<topic.length;j++){
-                int temp = 0;
-                for(int k = 0;k<topic[i].length();k++)
-                    if(topic[i].charAt(k)=='1' || topic[j].charAt(k)=='1')
-                        temp++;
-                if(temp>max){
-                    max = temp;
-                    count = 1;
+        static int[] acmTeam(String[] topic) {
+            int count = 1;
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<topic.length-1;i++){
+                for(int j=i+1;j<topic.length;j++){
+                    int teamSum = 0;
+                    for (int k = 0; k < topic[i].length(); k++) {
+                        if (topic[i].charAt(k) == '1') teamSum++;
+                        if (topic[j].charAt(k) == '1' && topic[i].charAt(k) == '0') teamSum++;
+                    }
+                    if (teamSum > max) {
+                        max = teamSum;
+                        count = 1;
+                    } else if (teamSum == max) {
+                        count++;
+                    }
                 }
-                else if(temp == max)
-                    count++;
             }
-  
+            int[] arr = {max,count};
+            return arr;
         }
-        int[] ar = {max,count};
-        return ar;
-
-}
