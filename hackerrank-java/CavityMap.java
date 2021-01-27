9ed51c78449f9b1c97fa1e2d9f9a5c9c7b0279cb
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-27 13:23:08 -0500

    removed extraneous line

--- hackerrank-java/CavityMap.java
+++ hackerrank-java/CavityMap.java
@@ -7,7 +7,6 @@ public class CavityMap {
     static String[] cavityMap(String[] grid) {
         for (int i = 1; i < grid.length-1; i++) {
             for (int j = 1; j < grid[i].length()-1; j++) {
-                char toReplace = grid[i].charAt(j);
                 int test = Character.getNumericValue(grid[i].charAt(j));
                 int above = Character.getNumericValue(grid[i-1].charAt(j));
                 int below = Character.getNumericValue(grid[i+1].charAt(j));
