8662343161631effe676e80e203de68bf5ba86bb
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-27 14:41:49 -0500

    game stop question using simple for loops

--- hackerrank-java/GameStop.java
+++ hackerrank-java/GameStop.java
@@ -5,19 +5,11 @@ public class GameStop {
 
     static int howManyGames(int p, int d, int m, int s) {
         int games = 0;
-        while (s >= 0) {
-            if (s - p > 0) {
-                games++;
-                s -= p;
-                if (p - d < m){
-                    p = m;
-                } else {
-                    p -= d;
-                }
-            } else {
-                return games;
-            }
-        }
-        return games;
+        while (s >= p) {
+            games++;
+            s -= p;
+            p = Math.max(p - d, m);
+    }
+    return games;
     }
 }
