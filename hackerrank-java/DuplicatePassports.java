839095d20057f59587a79b2a830aad0c9c7aa8d5
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-26 19:23:34 -0500

    solution

--- hackerrank-java/DuplicatePassports.java
+++ hackerrank-java/DuplicatePassports.java
@@ -25,4 +25,46 @@ public class DuplicatePassports {
       public static void main(String[] args) {
     
       }
+
+      static int[] findDuplicates2(int[] arr1, int[] arr2) {
+        // your code goes here
+        int p1 = 0;
+        int p2 = 0;
+        List<Integer> answer = new ArrayList<>();
+        
+        while(p1 < arr1.length && p2 < arr2.length){
+          int num1 = arr1[p1]; //1
+          int num2 = arr2[p2]; //2
+          
+          if(num1 == num2){
+            answer.add(num1);
+            p1++;
+          p2++; 
+          }else if (num1 > num2){
+            while((num1 > num2) && (p2 < arr2.length - 1)){
+              p2++;
+              num2 = arr2[p2];
+            }
+            if(num1 == num2) answer.add(num1);
+            p1++; 
+          }else if(num1 < num2){
+            while((num1 < num2) && (p1 < arr1.length - 1)){
+              p1++;
+              num1 = arr1[p1]; //3
+            }
+            if(num1 == num2) answer.add(num1);
+            p2++;
+            
+          }
+          
+        }
+        
+        int [] arrayAnswer = new int[answer.size()];
+        for(int i = 0; i < answer.size(); i++){
+          arrayAnswer[i] = answer.get(i);
+        }
+     
+        return arrayAnswer; 
+       
+      }
 }
