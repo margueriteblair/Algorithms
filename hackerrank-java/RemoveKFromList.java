dae9325e46dcb6079d329958cbded0e0ca13e858
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-26 22:09:12 -0500

    remove all instances of a target k within a linkedlist

---
+++ hackerrank-java/RemoveKFromList.java
@@ -0,0 +1,15 @@
+public class RemoveKFromList {
+    ListNode<Integer> removeKFromList(ListNode<Integer> l, int k) {
+        ListNode list = new ListNode(0);
+        ListNode temp=list;
+        while(l!=null){
+               if(l.value!=k){
+                   temp.next = l;
+                   temp= temp.next;
+               } 
+               l = l.next;
+        }
+        temp.next=null;
+        return list.next;
+}
+}
