2d52fc7c306fd45a547b7558372b29071cf14a91
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-26 20:39:12 -0500

    refactored

--- hackerrank-java/CharacterArray.java
+++ hackerrank-java/CharacterArray.java
@@ -3,45 +3,36 @@ public class CharacterArray {
         
     }
 
-    static String getShortestUniqueSubstring(char[] arr, String str) {
-    
-        //Explore 30 day 
-        // Medium
-        // Sliding Window Technique
-        
-        // 
-        
-        https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-100-LeetCode-Questions-to-Save-Your-Time-OaM1orEU
-        
-        
+    public String minWindow(String s, String t) {
         Map<Character, Integer> charMap = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-          if (!charMap.containsKey(arr[i])) {
-            charMap.put(arr[i], 1);
-          } else {
-            charMap.put(arr[i], charMap.get(arr[i])+1);
-          }
-        }
-        
-        String temp = "";
-        for (int i = 0; i < str.length(); i++) {
-          Map<Character, Integer> charMap2 = new HashMap<>(charMap);
-          System.out.println(charMap2);
-          for (int j = i; j < str.length(); j++) {
-            if (charMap2.containsKey(str.charAt(j))) {
-            charMap2.put(str.charAt(j), charMap2.get(str.charAt(j))-1);
-              if (charMap2.get(str.charAt(j)) == 0) charMap2.remove(str.charAt(j));
+        for (int i = 0; i < t.length(); i++) {
+            if (!charMap.containsKey(t.charAt(i))) {
+                charMap.put(t.charAt(i), 1);
+            } else {
+                charMap.put(t.charAt(i), charMap.get(t.charAt(i))+1);
             }
-            
-            if (charMap2.isEmpty()) {
-              if (str.substring(i, j).length() < temp.length() && !temp.equals("")) {
-                temp = str.substring(i, j);
-              } else if (temp.equals("")) temp=str.substring(i, j);
-              break;
-            }
-          }
         }
+        System.out.println(charMap);
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            HashMap<Character, Integer> charMap2 = new HashMap<Character, Integer>(charMap);
+            for (int j = i; j < s.length(); j++) {
+                if (charMap2.containsKey(s.charAt(j))) {
+                    charMap2.put(s.charAt(j), charMap2.get(s.charAt(j))-1);
+                    if (charMap2.get(s.charAt(j)) == 0) {
+                        charMap2.remove(s.charAt(j));
+                    }
+                }
+                if (charMap2.isEmpty()) {
+                    if (!answer.equals("") && s.substring(i, j).length() < answer.length()) {
+                        answer = s.substring(i, j+1);
+                    } else if (answer.equals("")) {
+                        answer = s.substring(i, j+1);
+                    }
+                }
+            }
+        }
+        return answer;
         
-        return temp;
-      }
+    }
 }
