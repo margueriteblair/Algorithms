7985d9bac3f2bbdc010cdb839e8bf3bde788f2a7
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-26 21:27:36 -0500

    valid palindrome are we able to change a given string into a palindrome

--- hackerrank-java/ValidPalindrome.java
+++ hackerrank-java/ValidPalindrome.java
@@ -6,13 +6,13 @@ public class ValidPalindrome {
     public boolean isPalindrome(String s) {
         Map<Character, Integer> dict = new HashMap<>();
         int oddCounter = 0;
-        String newStr = s.replaceAll("[^A-Za-z0-9]", "");
+        String newStr = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
         System.out.println(newStr);
         for (int i = 0; i < newStr.length(); i++) {
             int count = dict.getOrDefault(newStr.charAt(i), 0);
             dict.put(newStr.charAt(i), count+1);
-            if (count+1 % 2 == 1) oddCounter++;
-            if (count+1 % 2 == 0) oddCounter--;
+            if (dict.get(newStr.charAt(i)) % 2 == 1) oddCounter++;
+            if (dict.get(newStr.charAt(i))% 2 == 0) oddCounter--;
         }
         return oddCounter > 1 ? false : true;
     }
