3346ad517f6ee8135a990b934ca89bcca614928a
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-28 13:49:41 -0500

    ransom note part 2

--- hackerrank-java/RansomeNote.java
+++ hackerrank-java/RansomeNote.java
@@ -11,5 +11,23 @@ public class RansomNote {
         }
         System.out.println("Yes");
     }
+
+    static void checkMagazine2(String[] magazine, String[] note) {
+        Map<String, Integer> magazineMap = new HashMap<>();
+        for (int i = 0; i < magazine.length; i++) {
+            int count = magazineMap.getOrDefault(magazine[i], 0)+1;
+            magazineMap.put(magazine[i], count);
+        }
+        for (int i = 0; i < note.length; i++) {
+            if (!magazineMap.containsKey(note[i]) || magazineMap.get(note[i]) <= 0) {
+                System.out.println("No");
+                return;
+            } else {
+                magazineMap.put(note[i], magazineMap.get(note[i])-1);
+            }
+        }
+        System.out.println("Yes");
+
+    }
 }
 
