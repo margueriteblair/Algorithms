a042daba8a782dd94f4059d03531fef3d07dacf1
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-28 13:34:30 -0500

    algo that accepts a tree and will determine whether or not its a bst

---
+++ hackerrank-java/IsBST.java
@@ -0,0 +1,22 @@
+public class IsBST {
+    public static void main(String[] args) {
+        
+    }
+    boolean check(Node root, int min, int max) {
+        if (root != null) {
+            if (root.data >= max || root.data <= min) {
+                return false;
+            }
+            else {
+                return check(root.left, min, root.data) & check(root.right, root.data, max);
+            }   
+        }
+        else {
+            return true;
+        }
+    }
+    
+    boolean checkBST(Node root) {
+        return check(root, 0, Integer.MAX_VALUE);
+    }
+}
