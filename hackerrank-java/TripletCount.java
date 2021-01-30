f455ed0132322b102da59003ebe0a9fde08ac673
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-29 22:17:47 -0500

    triple count

--- hackerrank-java/TripletCount.java
+++ hackerrank-java/TripletCount.java
@@ -30,4 +30,31 @@ public class TripletCount {
         
         return false;
     }
+
+    static long triplets2(int[] a, int[] b, int[] c) {
+        Arrays.sort(a);
+       a=Arrays.stream(a).distinct().toArray();
+       Arrays.sort(b);
+       b=Arrays.stream(b).distinct().toArray();
+       Arrays.sort(c);
+       c=Arrays.stream(c).distinct().toArray();
+       int aIndex = 0;
+       int bIndex = 0;
+       int cIndex = 0;
+       long result = 0;
+
+       while(bIndex<b.length){
+           while(aIndex<a.length && a[aIndex]<=b[bIndex])
+               aIndex++;
+
+           while(cIndex<c.length && c[cIndex]<=b[bIndex])
+               cIndex++;
+                       
+           // you should convert int to long first, 
+           // avoid int overflow and lead to fail the test case 4
+           result += ((long)aIndex)*((long)cIndex);
+           bIndex++;
+       }
+       return result;
+   }
 }
