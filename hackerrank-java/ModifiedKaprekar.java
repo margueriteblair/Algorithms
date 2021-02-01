346c766346665260d769b3499adab6ae2fff43af
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-01 11:01:49 -0500

    changed result of squared val to a long and added exception for when there's no kaprekar nums

--- hackerrank-java/ModifiedKaprekar.java
+++ hackerrank-java/ModifiedKaprekar.java
@@ -4,10 +4,15 @@ public class ModifiedKaprekar {
     }
 
     static void kaprekarNumbers(int p, int q) {
+        int count = 0;
         for (int i = p; i <= q; i++) {
             if (isModified(i)) {
+                count++;
                 System.out.print(i + " ");
             }
+        }
+        if (count == 0) {
+            System.out.println("INVALID RANGE");
         }
 
     }
@@ -16,7 +21,7 @@ public class ModifiedKaprekar {
         if (num == 1) return true;
         String n = String.valueOf(num);
         int d = n.length();
-        String n2 = String.valueOf((int) Math.pow(num, 2));
+        String n2 = String.valueOf((long) Math.pow(num, 2));
         String r = n2.substring(n2.length()-d, n2.length());
         String l = n2.substring(0, n2.length()-d);
         try {
