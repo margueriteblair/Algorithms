9aa38b00ffe0693c6f59ad8658ad55453c84737f
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-04 17:46:05 -0500

    grading students

---
+++ hackerrank-java/GradingStudents.java
@@ -0,0 +1,17 @@
+public class GradingStudents {
+    public static void main(String[] args) {
+        System.out.println(73 % 5);
+    }
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> newGrades = new ArrayList<>();
+        for (int i = 0; i < grades.size(); i++) {
+            if (grades.get(i) >= 38 && grades.get(i) % 5 >= 3) {
+                newGrades.add(grades.get(i)+(grades.get(i)%5)-1);
+            } else {
+                newGrades.add(grades.get(i));
+            }
+        }
+        return newGrades;
+    }
+}
