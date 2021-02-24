5e3892aeebb7b5af88db4b547d1e70174537504c
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-23 20:15:30 -0500

    now accounts for negative nums

--- hackerrank-java/ReverseInteger.java
+++ hackerrank-java/ReverseInteger.java
@@ -5,7 +5,9 @@ public class ReverseInteger {
         for (int i = str.length()-1; i >= 0; i--) {
             intStr += str.charAt(i);
         }
-        
+        if (intStr.charAt(intStr.length()-1) == '-') {
+            return Integer.parseInt(intStr.substring(0, intStr.length()-1))*-1;
+        }
         return Integer.parseInt(intStr);
     }
 }
