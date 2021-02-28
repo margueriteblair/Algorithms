d64fe26891048aff5acf9789c32bd78df92c23c7
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-27 22:35:21 -0500

    remove dups

---
+++ hackerrank-java/RemoveDups.java
@@ -0,0 +1,20 @@
+public class RemoveDups {
+    public static void main(String[] args) {
+        
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int i=0, j=0;
+        
+        while(j<nums.length){
+            if(nums[j] == nums[i])  j++;
+            else{
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+        return i+1;
+       
+    }
+}
