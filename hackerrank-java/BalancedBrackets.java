939fbb1b2283d19f8c78d029f9d4f6338170433f
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-27 22:10:04 -0500

    one last mod

--- hackerrank-java/BalancedBrackets.java
+++ hackerrank-java/BalancedBrackets.java
@@ -24,3 +24,4 @@ public class BalancedBrackets {
         }
         return stacky.isEmpty() ? "YES" : "NO";
     }
+}
--- hackerrank-java/ValidParenthesis.java
+++ hackerrank-java/ValidParenthesis.java
@@ -5,20 +5,21 @@ public class ValidParenthesis {
             if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
                 stack.push(s.charAt(i));
             } else if (!stack.isEmpty() && s.charAt(i) ==')') {
-                if (stack.pop() != s.charAt(i)) {
+                if (stack.pop() != '(') {
                     return false;
                 }
             } else if (!stack.isEmpty() && s.charAt(i) ==']') {
-                if (stack.pop() != s.charAt(i)) {
+                if (stack.pop() != '[') {
                     return false;
                 }
             } else if (!stack.isEmpty() && s.charAt(i) =='}') {
-                if (stack.pop() != s.charAt(i)) {
+                if (stack.pop() != '{') {
                     return false;
                 }
+            } else if (stack.isEmpty()) {
+                return false;
             }
         }
-        System.out.println(stack);
         return stack.isEmpty() ? true : false;
     }
 }
