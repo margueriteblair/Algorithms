19e36e766b68ed704f3f507b79299a221f61f03f
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-28 20:19:17 -0500

    remove val

---
+++ hackerrank-java/RemoveVal.java
@@ -0,0 +1,14 @@
+public class RemoveVal {
+    public int removeElement(int[] nums, int val) {
+        int len = 0 ;
+        if(Objects.isNull(nums) || nums.length == 0){
+            return len ;
+        }
+        for(int num : nums){
+            if(num != val){
+                 nums[len++] = num ;
+            }
+        }
+        return len;
+    }
+}
