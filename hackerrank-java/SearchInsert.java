e362868d2bf18bee102ca00764478d6e98cf7d2f
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-03 23:31:24 -0500

    search insert

---
+++ hackerrank-java/SearchInsert.java
@@ -0,0 +1,17 @@
+public class SearchInsert {
+    public static void main(String[] args) {
+        
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) return i;
+            if (i+1 < nums.length && nums[i] < target && nums[i+1] > target) {
+                return i+1;
+            }
+            if (target < nums[0]) return 0;
+            if (target > nums[nums.length -1]) return nums.length - 1;
+        }
+        return 1;
+    }
+}
