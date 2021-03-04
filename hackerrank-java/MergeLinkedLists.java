a262e2226ba430a78c807bede2b8c9a1c36e33ae
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-04 17:40:50 -0500

    def doing this wrong lol

--- hackerrank-java/MergeLinkedLists.java
+++ hackerrank-java/MergeLinkedLists.java
@@ -6,16 +6,17 @@ public class MergeLinkedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode copy1 = l1;
         ListNode copy2 = l2;
-        ListNode end = null;
+        ListNode end = copy1.val <= copy2.val ? copy1 : copy2;
+        copy1 = l1.next;
+        copy2 = l2.next;
         while (copy1 != null && copy2 != null) {
             if (copy1.val >= copy2.val) {
-                end = copy2;
+                end.next = copy2;
                 copy2 = copy2.next;
             } else {
-                end = copy1;
+                end.next = copy1;
                 copy1 = copy1.next;
             }
-            end = end.next == null ? null : copy1;
         }
         return end;
     }
