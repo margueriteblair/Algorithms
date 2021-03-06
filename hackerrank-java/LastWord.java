53ab7141ea9f92b749f24f94769651b38ebd1d2c
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-06 18:42:56 -0500

    last word

---
+++ hackerrank-java/LastWord.java
@@ -0,0 +1,7 @@
+public class LastWord {
+    public int lengthOfLastWord(String s) {
+        if (s.trim().equals("")) return 0;
+        String[] arr = s.split(" ");
+        return arr[arr.length-1].length();
+    }
+}
