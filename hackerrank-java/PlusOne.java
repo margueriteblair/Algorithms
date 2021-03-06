645a7961761177886e462bff5dfafa8912b990d7
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-06 18:58:22 -0500

    lets try this again now that i understand the problem

--- hackerrank-java/PlusOne.java
+++ hackerrank-java/PlusOne.java
@@ -1,6 +1,17 @@
 public class PlusOne {
     public int[] plusOne(int[] digits) {
-        digits[digits.length-1]++;
-        return digits;
+        String str = "";
+        for (int i = 0; i < digits.length; i++) {
+            str += String.valueOf(digits[i]);
+        }
+        Integer num = Integer.parseInt(str);
+        num++;
+        String str2 = String.valueOf(num);
+        String[] strDig = str2.split("");
+        int[] digits2 = new int[strDig.length];
+        for (int i = 0; i < strDig.length; i++) {
+            digits2[i] = Integer.parseInt(strDig[i]);
+        }
+        return digits2;
     }
 }
