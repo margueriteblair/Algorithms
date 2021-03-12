2a8d107f000e192673aeaf42c845910a379f0710
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-12 18:34:44 -0500

    null pointer exception

--- hackerrank-java/HasCycle.java
+++ hackerrank-java/HasCycle.java
@@ -2,12 +2,12 @@ public class HasCycle {
     public static void main(String[] args) {
         
     }
-
     public boolean hasCycle(ListNode head) {
         if (head == null || head.next == null) return false;
         ListNode tortoise = head;
         ListNode hare = head.next;
         
+        try {
         while (hare != null) {
             if (hare == tortoise) {
                 return true;
@@ -15,8 +15,11 @@ public class HasCycle {
             
             tortoise = tortoise.next;
             hare = hare.next.next;
+        } 
+        } catch (Exception ex) {
+            return false;
         }
-        
+
         return false;
     }
 }
