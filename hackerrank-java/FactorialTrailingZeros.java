e09a22fffd9aa8c3dda3cefb1af045a08e195eed
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-18 13:18:14 -0400

    attempted solution with big integer

--- hackerrank-java/FactorialTrailingZeros.java
+++ hackerrank-java/FactorialTrailingZeros.java
@@ -3,16 +3,18 @@ public class FactorialTrailingZeros {
         
     }
     public int trailingZeroes(int n) {
-        int fac = 1;
+        BigInteger fac = BigInteger.ONE;
         for (int i = n; i > 1; i--) {
-            fac *= i;
+            fac = fac.multiply(BigInteger.valueOf(i));
         }
         
         String facString = String.valueOf(fac);
+        System.out.println(facString);
         int zeroCount = 0;
         int i = facString.length()-1;
         while (i >= 0 && facString.charAt(i) == '0') {
             System.out.println(facString.charAt(i));
+            zeroCount++;
             i--;
         }
         
