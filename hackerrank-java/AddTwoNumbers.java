d5b056e9bcc135d9325d3126a99d7b6c6d6015bd
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-20 20:17:25 -0400

    were getting closer

--- hackerrank-java/AddTwoNumbers.java
+++ hackerrank-java/AddTwoNumbers.java
@@ -4,41 +4,43 @@ public class AddTwoNumbers {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> sums = new Stack<>();
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        
         while (l1 != null) {
-            if (l2 == null) {
-               sums.push(l1.val + 0); 
-            } else {
-               sums.push(l1.val + l2.val); 
-                l2 = l2.next;
-            }
+            stack1.push(l1.val);
             l1 = l1.next;
-            
         }
         
         while (l2 != null) {
-            if (l1 == null) {
-                sums.push(l2.val + 0);
-            } else {
-                sums.push(l2.val + l1.val);
-                l1 = l1.next;
-            }
-            
+            stack2.push(l2.val);
             l2 = l2.next;
         }
-        ListNode newHead = new ListNode(sums.pop());
-        ListNode realHead = newHead;
-        int recentRemainder = 0;
-        while (!sums.isEmpty()) {
-            int test = sums.pop();
-            realHead.next = new ListNode((test + recentRemainder) % 10);
-            if (test + recentRemainder > 9) {
-                recentRemainder = test + recentRemainder - 9;
-            } else {
-                recentRemainder = 0;
-            }
-            realHead = realHead.next;
+        
+        String str1 = "";
+        String str2 = "";
+        
+        while (!stack1.isEmpty()) {
+            str1 += stack1.pop();
         }
-        return newHead;
+        
+        while (!stack2.isEmpty()) {
+            str2 += stack2.pop();
+        }
+        
+        
+        int sumInt = Integer.parseInt(str1) + Integer.parseInt(str2);
+        String sumStr = String.valueOf(sumInt);
+        
+        ListNode head = new ListNode(Character.getNumericValue(sumStr.charAt(sumStr.length()-1)));
+        ListNode copy = head.next;
+        
+        System.out.println(sumStr);
+        for (int i = sumStr.length()-2; i >= 0; i--) {
+            copy = new ListNode(Character.getNumericValue(sumStr.charAt(i)));
+            copy = copy.next;
+        }
+        
+        return head;
     }
 }
