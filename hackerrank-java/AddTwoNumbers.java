aac8001cbca999552bf73a330a47a91467d4d67c
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-20 20:28:03 -0400

    hello

--- hackerrank-java/AddTwoNumbers.java
+++ hackerrank-java/AddTwoNumbers.java
@@ -43,4 +43,27 @@ public class AddTwoNumbers {
         
         return head;
     }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum, carry=0;
+ ListNode temp = null, prev =null,res=null;
+ while(l1 !=null || l2 !=null)
+ {
+     sum = carry+ (l1 != null? l1.val: 0) + (l2 !=null ? l2.val :0);
+     carry = (sum >=10) ? 1:0;
+     sum = sum%10;
+     temp = new ListNode(sum);
+     if(res == null)  res =temp;
+     else  prev.next = temp; 
+     prev = temp;
+     if(l1!=null)  l1 =l1.next;  
+     if(l2!=null) l2 =l2.next; 
+ }
+ if(carry > 0)
+ {
+    temp.next = new ListNode(carry); 
+ }
+ return res;
+
+}
 }
