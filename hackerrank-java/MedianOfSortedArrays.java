b574553a156910fe1041d1c07617b3cd60532f1c
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-20 20:34:40 -0400

    clearly i dont know what a median is mathematically

--- hackerrank-java/MedianOfSortedArrays.java
+++ hackerrank-java/MedianOfSortedArrays.java
@@ -4,9 +4,19 @@ public class MedianOfSortedArrays {
     }
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int min = Math.min(nums1[0], nums2[0]);
-        int max = Math.max(nums1[nums1.length-1], nums2[nums2.length-1]);
+        int min = 0;
+        int max = 0;
         
+        if (nums1.length >0 && nums2.length >0) {
+            min = Math.min(nums1[0], nums2[0]);
+            max = Math.max(nums1[nums1.length-1], nums2[nums2.length-1]);
+        } else if (nums1.length == 0) {
+            min = nums2[0];
+            max = nums2[nums2.length-1];
+        } else if (nums2.length == 0) {
+            min = nums1[0];
+            max = nums1[nums1.length-1];
+        }
         return (double)(min + max) / 2;
     }
 }
