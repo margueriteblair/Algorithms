482ae7c45cb0d6803cd9aa0cc51ad1073aa6b5b8
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-22 15:46:28 -0400

    recusive solutiong

--- hackerrank-java/ExcelColumnTitles.java
+++ hackerrank-java/ExcelColumnTitles.java
@@ -8,9 +8,18 @@ public class ExcelColumnTitles {
         String[] arr = str.split("");
         
         if (columnNumber <= 26) {
+            System.out.println(701 % 26);
             return arr[columnNumber-1];
         } else {
-            
+            String ans = "";
+            if (columnNumber % 26 != 0) {
+                ans += convertToTitle(columnNumber/26);
+                ans += convertToTitle(columnNumber%26);
+            } else {
+                ans += convertToTitle((columnNumber/26)-1);
+                ans += 'Z';
+            }
+            return ans;
         }
     }
 }
