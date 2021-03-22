1116a6c6937ce07d4020b57c02db690a578437af
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-22 15:27:37 -0400

    reverse bits

--- hackerrank-java/ReverseBits.java
+++ hackerrank-java/ReverseBits.java
@@ -7,6 +7,11 @@ public class ReverseBits {
         for (int i = resultWithPadding.length()-1; i >= 0; i--) {
             reverse += resultWithPadding.charAt(i);
         }
-        return Integer.parseInt(reverse, 2);
+        if (n < 0) {
+            String end = "-" + reverse;
+            return Integer.parseInt(end, 2);
+        } else {
+            return Integer.parseInt(reverse, 2);
+        }  
     }
 }
