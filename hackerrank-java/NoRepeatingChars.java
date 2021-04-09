338c72a54fa2c403ed9f4e8e9b13ffa8062eea99
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-09 15:12:13 -0400

    auxiliary method

--- hackerrank-java/NoRepeatingChars.java
+++ hackerrank-java/NoRepeatingChars.java
@@ -8,15 +8,25 @@ public class NoRepeatingChars {
         String temp = "";
         for (int i = 0; i < s.length()-1; i++) {
             for (int j = i+1; j < s.length(); j++) {
-                temp = s.substring(i, j-1);
-                if (!temp.contains(String.valueOf(s.charAt(j)))) {
-                    maxLength = Math.max(temp.length(), maxLength);
-                } else {
-                    i = j;
+                temp = s.substring(i, j);
+                if (isRepeating(temp)) {
+                    maxLength = Math.max(maxLength, temp.length());
                 }
             }
         
     }
         return maxLength;
+    }
+    
+    public boolean isRepeating(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            
+            int count = map.getOrDefault(str.charAt(i), 0);
+            if (count > 1 || count+1 > 1) return false;
+            map.put(str.charAt(i), count+1);
+        }
+        System.out.println(map);
+        return true;
     }
 }
