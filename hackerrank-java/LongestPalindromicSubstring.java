1cfb5b0d5ecf8a6da38dee663a2a6d9276f0215a
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-10 17:43:36 -0400

    longest palindromic substring

---
+++ hackerrank-java/LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        
+    }
+
+        
+    String max="", curr;
+    for( int i=0;i<s.length();i++){
+            curr=find(s,i,i);
+        if(curr.length()>max.length())
+            max=curr;
+            curr=find(s,i,i+1);
+        if(curr.length()>max.length())
+            max=curr;
+      
+      
+        
+        
+    }
+    return max;
+    
+}
+        String find(String s , int low , int high){
+    int n=s.length();
+    while(low>=0 && high <n && (s.charAt(low))==(s.charAt(high)))
+    {
+    low--;
+    high++;
+    }
+    return s.substring(low+1, high);
+
+}
+}
