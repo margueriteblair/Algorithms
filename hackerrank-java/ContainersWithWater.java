5986d4f14c6185c9db719e3f235119545f9167a1
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-13 22:36:57 -0400

    wee bit of refactoring

--- hackerrank-java/ContainersWithWater.java
+++ hackerrank-java/ContainersWithWater.java
@@ -1,14 +1,14 @@
 public class ContainersWithWater {
     public int maxArea(int[] height) {
-        int maxAmount = 0;
-        for (int i = 0; i < height.length; i++) {
-            // if ((height.length-(i+1))*height[i] > maxAmount) {
-            for (int j = i+1; j < height.length-1; j++) {
-                if (height[j] <= height[i] && (j-i)*height[i] > maxAmount){
-                    maxAmount = ((j-i)*height[i]);
+        int maxWater = 0;
+        for (int i = 0; i < height.length-1; i++) {
+            for (int j = i+1; j < height.length; j++) {
+                int lower = Math.min(height[i], height[j]);
+                if ((j-i)*lower > maxWater) {
+                    maxWater = (j-i)*lower;
                 }
             }
         }
-        return maxAmount;
+        return maxWater;
     }
 }
