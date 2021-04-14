8b314c5ab5df675929267ce5cb9e1e97b22c8d4b
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-13 22:57:07 -0400

    no timeout

--- hackerrank-java/ContainersWithWater.java
+++ hackerrank-java/ContainersWithWater.java
@@ -11,4 +11,24 @@ public class ContainersWithWater {
         }
         return maxWater;
     }
+
+    public int maxArea2(int[] height) {
+        int i = 0;
+        int j = height.length-1;
+        int maxWater = 0;
+        while (i < j) {
+            if (height[i] < height[j]) {
+                maxWater = Math.max(maxWater, (j-i)*height[i]);
+                while(i<j && height[i+1]<=height[i])
+                    i++;
+                i++;
+            } else {
+                maxWater = Math.max(maxWater, (j-1)*height[j]);
+                while(i<j && height[j-1]<=height[j])
+                    j--;
+                j--;
+            }
+        }
+        return maxWater;
+    }
 }
