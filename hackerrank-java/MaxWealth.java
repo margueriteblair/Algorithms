5b29e54b6424043d5afa3cb324497d7907d0b4c2
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-15 10:39:34 -0400

    max wealth with nested arrays

---
+++ hackerrank-java/MaxWealth.java
@@ -0,0 +1,19 @@
+public class MaxWealth {
+    public static void main(String[] args) {
+        
+    }
+
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            if (sum > maxWealth) {
+                maxWealth = sum;
+            }
+        }
+        return maxWealth;
+    }
+}
