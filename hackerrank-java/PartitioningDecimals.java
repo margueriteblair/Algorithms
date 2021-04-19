dfade127c57426e8f33c47e75c0ab1ca3bc1461d
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-18 23:27:26 -0400

    partition

--- hackerrank-java/PartitioningDecimals.java
+++ hackerrank-java/PartitioningDecimals.java
@@ -14,4 +14,17 @@ public class PartitioningDecimals {
         
         return nInt % eleventInt == 0 ? (int) nInt / eleventInt : (int) nInt / eleventInt + 1;
     }
+
+    public int minPartitions2(String n) {
+        int ans=0;
+       //see hints for explaination
+       char carr[]=n.toCharArray();
+       for(int i=0;i<carr.length;i++){
+          int digit=carr[i]-'0';
+          if(digit>ans) ans=digit;
+       }
+      
+          
+        return ans;
+     }
 }
