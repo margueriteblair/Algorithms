5e1a8cf8858546a7994a4106f593111a33cc1e86
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-29 14:12:40 -0400

    group people

---
+++ hackerrank-java/GroupPeople.java
@@ -0,0 +1,27 @@
+public class GroupPeople {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        HashMap<Integer, List<Integer>> map = new HashMap();
+        List<List<Integer>> result = new ArrayList();
+        
+        int index = 0;
+        for (int size: groupSizes) {
+            List<Integer> list = map.get(size);
+            
+            if (list == null) {
+                list = new ArrayList<Integer>();
+            }
+            
+            list.add(index);
+
+            if (list.size() == size) {
+                result.add(list);
+                map.remove(size);
+            } else {
+                map.put(size, list);
+            }
+            
+            index++;
+        }
+        return result;
+    }
+}
