52e3e329aed381dbb9a6b202149cdb94c71e2537
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-02 16:43:13 -0400

    still wrong

--- hackerrank-java/ReplaceDigitsWithChars.java
+++ hackerrank-java/ReplaceDigitsWithChars.java
@@ -3,16 +3,25 @@ public class ReplaceDigitsWithChars {
         
     }
 
+    public static String alpha = "abcdefghijklmnopqrstuvwxyz";
     public String replaceDigits(String s) {
-        let alpha = "abcdefghijklmnopqrstuvwxyz";
+        
         for (int i = 0; i < s.length(); i++) {
             if (i % 2 == 1) {
-                s.charAt(i) = shift(s.charAt(i), Integer.valueOf(String.valueOf(s.charAt(i))));
+                s.replace(s.charAt(i), shift(s.charAt(i), Integer.valueOf(String.valueOf(s.charAt(i)))));
             }
         }
+        return s;
     }
     
     public char shift(char c, int x) {
-        
+        char c2 = 'a';
+        for (int i = 0; i < alpha.length(); i++) {
+            if (alpha.charAt(i) == c) {
+                c2 = alpha.charAt(i+5);
+                return alpha.charAt(i+5);
+            }
+        }
+        return c2;
     }
 }
