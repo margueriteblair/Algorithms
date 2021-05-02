250c8008d44587aed5b25d26ef9858f24f6c8431
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-02 16:57:21 -0400

    worked

--- hackerrank-java/ReplaceDigitsWithChars.java
+++ hackerrank-java/ReplaceDigitsWithChars.java
@@ -24,4 +24,12 @@ public class ReplaceDigitsWithChars {
         }
         return c2;
     }
+
+    public String replaceDigits2(String s) {
+        char[] charArray = s.toCharArray();
+       for(int i = 1; i<charArray.length; i = i + 2) {
+         charArray[i] = (char) (charArray[i - 1] + charArray[i] - '0');
+       }
+       return String.valueOf(charArray);   
+   }
 }
