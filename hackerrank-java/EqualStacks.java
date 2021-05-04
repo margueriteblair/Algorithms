72eae038b290e8f98e440bc349277cae379edbc2
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-03 21:28:01 -0400

    equal stacks

--- hackerrank-java/EqualStacks.java
+++ hackerrank-java/EqualStacks.java
@@ -35,4 +35,53 @@ public class EqualStacks {
             }
     
         }
+
+        public static int equalStacks2(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+            // Write your code here
+             int sum1=0, sum2=0 , sum3=0;
+            for(int i=0 ; i<h1.size() ; i++)
+            {
+                sum1=sum1+h1.get(i);
+            }
+                for(int i=0 ; i<h2.size() ; i++)
+            {
+                sum2=sum2+h2.get(i);
+            }
+                for(int i=0 ; i<h3.size() ; i++)
+            {
+                sum3=sum3+h3.get(i);
+            }
+            int n=1;
+            while(n!=0)
+            {
+                if(sum1==sum2 && sum2==sum3)
+                {
+                    n=0;
+                    return sum1;
+                }
+                else if(sum1>=sum2 && sum1>=sum3)
+                {
+                    int val=h1.remove(0);
+                    sum1=sum1-val;
+                    continue;
+                }
+                else if(sum2>=sum1 && sum2>=sum3)
+                {
+                    int val=h2.remove(0);
+                    sum2=sum2-val;
+                    continue;
+                }
+                else if(sum3>=sum1 && sum3>=sum1)
+                {
+                    int val=h3.remove(0);
+                    sum3=sum3-val;
+                    continue;
+                }
+                else return 0;
+            }
+            return 0;
+        
+            }
+        
+        }
 }
