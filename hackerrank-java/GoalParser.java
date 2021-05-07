cb1a8e5cdb8a4211a4dddb258a9f1540bba42226
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-07 14:06:31 -0400

    first draft

---
+++ hackerrank-java/GoalParser.java
@@ -0,0 +1,15 @@
+public class GoalParser {
+    public String interpret(String command) {
+        String res = "";
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                res += "G";
+            } else if (command.charAt(i) == '(' && command.charAt(i+1) == ')') {
+                res += "o";
+            } else if (command.charAt(i) == 'a') {
+                res += "al";
+            }
+        }
+        return res;
+    }
+}
