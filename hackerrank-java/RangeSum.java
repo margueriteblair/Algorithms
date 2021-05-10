afc5cf873e216a3d30b62a222eba64bfa99e7740
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-10 19:15:25 -0400

    still wrong lol

--- hackerrank-java/RangeSum.java
+++ hackerrank-java/RangeSum.java
@@ -1,11 +1,14 @@
 public class RangeSum {
     public int rangeSumBST(TreeNode root, int low, int high) {
         int sum = 0;
+        if (root.val >= low && root.val <= high) {
+            sum += root.val;
+        }
         if (root.left != null) {
-            sum += rangeSumBST(root.left, low, high);
+            return rangeSumBST(root.left, low, high);
         }
         if (root.right != null) {
-            sum += rangeSumBST(root.right, low, high);
+            return rangeSumBST(root.right, low, high);
         }
         
         return sum;
