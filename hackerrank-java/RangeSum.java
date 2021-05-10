48251390231d40df19114eed65686af641df2d9d
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-10 19:35:12 -0400

    solution

--- hackerrank-java/RangeSum.java
+++ hackerrank-java/RangeSum.java
@@ -1,16 +1,25 @@
 public class RangeSum {
+    ArrayList<Integer> nums = new ArrayList<Integer>();
+    int h = 0;
+    int l = 0;
+    void preOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        if(root.val <= h && root.val >= l){
+            nums.add(root.val);
+        }
+        preOrder(root.left);
+        preOrder(root.right);
+    }
     public int rangeSumBST(TreeNode root, int low, int high) {
+        h = high;
+        l = low;
+        preOrder(root);
         int sum = 0;
-        if (root.val >= low && root.val <= high) {
-            sum += root.val;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums.get(i);
         }
-        if (root.left != null) {
-            return rangeSumBST(root.left, low, high) + sum;
-        }
-        if (root.right != null) {
-            return rangeSumBST(root.right, low, high) + sum;
-        }
-        
         return sum;
     }
 }
