8cd53e12977abe7294d342c6f0a1922e7512a5d3
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-11 01:08:35 -0400

    a second solution

--- hackerrank-java/RangeSum.java
+++ hackerrank-java/RangeSum.java
@@ -22,4 +22,26 @@ public class RangeSum {
         }
         return sum;
     }
+
+    private int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        helper(root,low,high);
+        sum+=low;
+        sum+=high;
+        return sum;
+    }
+    public void helper(TreeNode root, int low, int high){
+        if(root == null)
+            return;
+        if(root.val>low && root.val<high)
+            sum+=root.val;
+        if(low<root.val && high<root.val)
+             helper(root.left,low,high);
+        else if(low>root.val && high>root.val)
+             helper(root.right,low,high);
+        else{
+            helper(root.left,low,high);
+            helper(root.right,low,high);
+        }
+    }
 }
