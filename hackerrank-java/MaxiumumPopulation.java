dee281406d561cb3bf7d1c1d54cc31e4fc7f4179
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-13 12:54:29 -0400

    correct

--- hackerrank-java/MaxiumumPopulation.java
+++ hackerrank-java/MaxiumumPopulation.java
@@ -4,24 +4,22 @@ public class MaxiumumPopulation {
     }
 
     public int maximumPopulation(int[][] logs) {
-        List<Integer> years = new ArrayList<>();
-        int year;
-        int globalYear = 0;
-        int globalPop = 0;
-        for (int i = 0; i < logs.length; i++) {
-            year = logs[i][0];
-            int population = 0;
-            for (int j = 0; j < logs.length; j++) {
-                if (logs[i][0] <= logs[j][0] && logs[j][1] > logs[i][1]) {
-                    population++;
-                }
-            }
-            if (population > globalPop) {
-                globalPop = population;
-                globalYear = year;
-            }
-            
+        int[] years = new int[2051];
+        for (int[] log : logs) {            
+            years[log[0]] += 1;
+            years[log[1]] -= 1;
         }
-        return globalYear;
+        
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+        int result = -1;
+        for (int i = 1950;i<2051;i++) {
+            count += years[i];
+            if (count > max) {
+                max = count;
+                result = i;
+            }
+        }
+        return result;
     }
 }
