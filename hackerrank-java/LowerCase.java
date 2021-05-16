11aca05f77f55488b04ce0df72de27c35639725e
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-16 17:13:59 -0400

    not to lwoer case

--- hackerrank-java/LowerCase.java
+++ hackerrank-java/LowerCase.java
@@ -6,4 +6,17 @@ public class LowerCase {
     public String toLowerCase(String str) {
         return str.toLowerCase();
     }
+
+    public String toLowerCase(String str) {
+        char[] c=str.toCharArray();
+        for(int i=0;i<c.length;i++)
+        {
+        if(c[i]>='A' && c[i]<='Z')
+        {
+        int num=(int)c[i];
+        c[i]=(char)(num+32);
+        }
+        }
+        return String.valueOf(c);
+    }
 }
