ac696afd27569455b2c96a7edf5d92608e24c1f9
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-19 14:50:27 -0400

    good triplet fixed bug

--- hackerrank-java/GoodTriplets.java
+++ hackerrank-java/GoodTriplets.java
@@ -3,13 +3,14 @@ public class GoodTriplets {
         int countGood = 0;
         for (int i = 0; i < arr.length-2; i++) {
             for (int j = i+1; j < arr.length-1; j++) {
-                for (int k = j+1; j < arr.length; j++) {
+                for (int k = j+1; k < arr.length; k++) {
                     if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j]-arr[k]) <= b && Math.abs(arr[i]-arr[k]) <= c) {
+                        System.out.println(i + " " + j + " " + k);
                         countGood++;
                     }
                 }
             }
         }
-        return countGood-1;
+        return countGood;
     }
 }
