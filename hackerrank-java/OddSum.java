8c79caab2d062a778dc6a672b655d8145f0f3573
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-24 23:04:29 -0400

    passes test case

--- hackerrank-java/OddSum.java
+++ hackerrank-java/OddSum.java
@@ -4,9 +4,10 @@ public class OddSum {
     }
     public int sumOddLengthSubarrays(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j < arr.length; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j <= arr.length; j++) {
                 int[] sub = Arrays.copyOfRange(arr, i, j);
+                System.out.println(Arrays.toString(sub));
                 if ((j-i) % 2 == 1) {
                     for (int k = 0; k < sub.length; k++) {
                         sum += sub[k];
