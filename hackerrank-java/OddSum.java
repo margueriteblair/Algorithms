0c028567b6b49a99bf121a45b6a19f46cec0b82e
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-24 23:02:27 -0400

    this is also wrong

--- hackerrank-java/OddSum.java
+++ hackerrank-java/OddSum.java
@@ -2,13 +2,15 @@ public class OddSum {
     public static void main(String[] args) {
         
     }
-
     public int sumOddLengthSubarrays(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if ((j-i+1)%2 == 1) {
-                    sum += arr[j];
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                int[] sub = Arrays.copyOfRange(arr, i, j);
+                if ((j-i) % 2 == 1) {
+                    for (int k = 0; k < sub.length; k++) {
+                        sum += sub[k];
+                    }
                 }
             }
         }
