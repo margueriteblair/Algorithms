9a8242f30153a64905b7b13b45a2d9c6f2de1adc
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-31 21:31:40 -0400

    working on it

--- hackerrank-java/AbsolutePermutations.java
+++ hackerrank-java/AbsolutePermutations.java
@@ -3,5 +3,19 @@ public class AbsolutePermutations {
         
     }
 
-    
+    public static List<Integer> absolutePermutation(int n, int k) {
+        List<Integer> permutation = new ArrayList<>();
+        int[] arr = new int[n];
+        for (int i = 1; i <= n; i++) {
+            arr[i-1] = i;
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (Math.abs(arr[i] - (i+1)) != k) {
+                break;
+            }
+        }
+        return new ArrayList<>();
+    }
+
 }
