b8c3ba30d94271adfdb8ffda470bad59353d5973
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-01 17:52:59 -0400

    fleshing our variables

--- hackerrank-java/BearGenes.java
+++ hackerrank-java/BearGenes.java
@@ -7,11 +7,17 @@ public class BearGenes {
         // Write your code here
             Map<Character, Integer> map = new HashMap<>();
             for (int i = 0; i < gene.length(); i++) {
-                int count = map.getOrDefault(s.charAt(i), 0) + 1;
-                map.put(s.charAt(i), count);
+                int count = map.getOrDefault(gene.charAt(i), 0) + 1;
+                map.put(gene.charAt(i), count);
             }
             
             System.out.println(map);
+            int maxChar = 0;
+            int steady = gene.length()/4;
+            for (int i : map.values()) {
+                if (i > maxChar) maxChar = i;
+            }
+            System.out.println(maxChar + " " + steady);
             return 1;
     
         }
