e0457cbacfdc6857fb30f563fc8a40419231ad4c
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-14 17:41:58 -0400

    truncate str

---
+++ hackerrank-java/TruncateStr.java
@@ -0,0 +1,11 @@
+public class TruncateStr {
+    public String truncateSentence(String s, int k) {
+        String res = "";
+        String[] arr = s.split(" ");
+        for (int i = 0; i < k; i++) {
+            res += arr[i] + " ";
+        }
+        
+        return res;
+    }
+}
