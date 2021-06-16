cb3bf36c100cc8a5166b4bfc36d37a01373c06e4
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-15 21:34:16 -0400

    morse code

--- hackerrank-java/MorseCode.java
+++ hackerrank-java/MorseCode.java
@@ -5,7 +5,7 @@ public class MorseCode {
 
     public int uniqueMorseRepresentations(String[] words) {
         String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        String alpha = "abcdefchijklmnopqrstuvwxyz";
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
         Map<Character, String> map = new HashMap<>();
         for (int i = 0; i < alpha.length(); i++) {
             map.put(alpha.charAt(i), morse[i]);
@@ -13,15 +13,16 @@ public class MorseCode {
         
         Set<String> set = new HashSet<>();
         for (int i = 0; i < words.length; i++) {
-            set.add(toMorseCode(words[i]));
+            set.add(toMorseCode(words[i], map));
         }
         System.out.println(map);
         return set.size();
     }
     
-    public static String toMorseCode(String word, HashMap<Character, String> map) {
+    public static String toMorseCode(String word, Map<Character, String> map) {
         String res = "";
         for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
             res += map.get(word.charAt(i));
         }
         
