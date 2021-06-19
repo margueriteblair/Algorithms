4f88cfcc50801a110cf3d61351f9c8418593a5eb
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-19 16:32:50 -0400

    complete Breadth-first search of a graph

--- hackerrank-java/BFS-Java.java
+++ hackerrank-java/BFS-Java.java
@@ -41,4 +41,22 @@ public class BFSJava {
             }
         }
     }
+
+    public static void main(String[] args) {
+        BFSJava graph = new BFSJava(6);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 1);
+        graph.addEdge(4, 1);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 0);
+        graph.addEdge(2, 1);
+        graph.addEdge(4, 1);
+        graph.addEdge(3, 1);
+        graph.addEdge(5, 4);
+        graph.addEdge(5, 3);
+
+        graph.BFS(0);
+    }
 }
