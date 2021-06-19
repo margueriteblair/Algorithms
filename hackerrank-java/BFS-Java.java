909694ef3a112622e6e5945770c0d6e67c633a1d
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-19 16:19:14 -0400

    added a method to add edges to the adjacency list

--- hackerrank-java/BFS-Java.java
+++ hackerrank-java/BFS-Java.java
@@ -12,5 +12,10 @@ public class BFSJava {
         for (int i = 0; i < v; i++) {
             adj[i] = new LinkedList<>();
         }
+        queue = new LinkedList<>();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
     }
 }
