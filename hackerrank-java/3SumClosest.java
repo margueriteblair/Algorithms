49f1c24fc94e2218e0d4633d0ac02d1bf539d572
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-23 21:30:38 -0400

    O3 solution

--- hackerrank-java/3SumClosest.java
+++ hackerrank-java/3SumClosest.java
@@ -34,13 +34,14 @@ public class 3SumClosest {
         
     }
 
-    public int threeSumClosest3(int[] nums, int target) {
+    public int threeSumClosest(int[] nums, int target) {
         int sum = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length-2; i++) {
             for (int j = i+1; j < nums.length-1; j++) {
                 for (int k = j+1; k < nums.length; k++) {
                     if (Math.abs(nums[i]+nums[j]+nums[k]-target) < sum) {
-                        sum = Math.abs(nums[i]+nums[j]+nums[k]-target);
+                        System.out.println(nums[i] + " " + nums[j] + " " + nums[k]);
+                        sum = nums[i]+nums[j]+nums[k];
                     }
                 }
             }
