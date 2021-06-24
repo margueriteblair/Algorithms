77b2ae36d58b53d38329ff9db0c39dce530ff304
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-23 21:23:55 -0400

    3sum

--- hackerrank-java/3SumClosest.java
+++ hackerrank-java/3SumClosest.java
@@ -33,4 +33,18 @@ public class 3SumClosest {
         return ans;
         
     }
+
+    public int threeSumClosest3(int[] nums, int target) {
+        int sum = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length-2; i++) {
+            for (int j = i+1; j < nums.length-1; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    if (Math.abs(nums[i]+nums[j]+nums[k]-target) < sum) {
+                        sum = Math.abs(nums[i]+nums[j]+nums[k]-target);
+                    }
+                }
+            }
+        }
+        return sum;
+    }
 }
