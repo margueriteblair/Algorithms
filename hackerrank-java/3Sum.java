43c94a485e6390205dc50b482a90eb5193201cd8
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-25 15:15:08 -0400

    incredibly time inefficient

--- hackerrank-java/3Sum.java
+++ hackerrank-java/3Sum.java
@@ -5,9 +5,19 @@ public class 3Sum {
 
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            
+        for (int i = 0; i < nums.length-2; i++) {
+            for (int j = i+1; j < nums.length-1; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        res.add(list);
+                    }
+                }
+            }
         }
-
+        return res;
     }
 }
