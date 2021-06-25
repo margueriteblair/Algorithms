2735a12ec0a652379ff7c1bb75fcbfbacec40248
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-25 15:10:48 -0400

    set up

---
+++ hackerrank-java/3Sum.java
@@ -0,0 +1,13 @@
+public class 3Sum {
+    public static void main(String[] args) {
+        
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            
+        }
+
+    }
+}
