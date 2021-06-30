c51c878b5b3f1410422f5433fadee974c017f575
Marguerite Blair <marguerite.blair@gmail.com>
2021-06-30 19:55:11 -0400

    combination

--- hackerrank-java/CombinationSum.java
+++ hackerrank-java/CombinationSum.java
@@ -1,5 +1,8 @@
 public class CombinationSum {
     public static void main(String[] args) {
         List<List<Integer>> res = new ArrayList<>();
+                //if candidates[i] is a factor of target
+        //if the numbers just sum up
+        //combo of sum and factor of target
     }
 }
