6d2a1f3b97145c36d208c16ea8b8367c224cf2ba
Marguerite Blair <marguerite.blair@gmail.com>
2021-07-27 01:49:59 -0400

    creating individual nodes then adding them as edges to one another

---
+++ GraphAlgos/Graph.java
@@ -0,0 +1,6 @@
+package graphAlgos;
+//Next, we'll implement the graph object and traversal methods.
+//The code below will create an undirected graph
+public class Graph {
+    
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+import graphAlgos.Node;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Node n0 = new Node(0); //all you pass into the node constructor is the int value;
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+
+        //we make a bunch of loose nodes that arent connected initially
+        //but now, we will use our add edge method to add edges to the nodes
+    }
+}
