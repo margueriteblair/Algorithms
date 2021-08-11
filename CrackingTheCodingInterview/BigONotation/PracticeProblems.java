a476f97cd11ae88340ec737bbf1daab05b4777a3
Marguerite Blair <margueriteblair@MargueritesMBP2.fios-router.home>
2021-08-10 20:54:45 -0400

    third for loop that's constnat

--- CrackingTheCodingInterview/BigONotation/PracticeProblems.java
+++ CrackingTheCodingInterview/BigONotation/PracticeProblems.java
@@ -24,4 +24,15 @@ public class PracticeProblems {
         }
     }
     //this is O(nm) we've got two inputs of potentially varying size
+
+    void printUnorderedPairs3(int[] arrayA, int[] arrayB) {
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                for (int k = 0; k < 10000; k++) {
+                    System.out.println(arrayA[i] + ", " + arrayB[j]);
+                }
+            }
+        }
+    }
+    //still O(nm) because the 10000 is constant
 }
