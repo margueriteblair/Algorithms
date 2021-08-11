7044a66be09b8503f04dc562f8a28ebab601b3f6
Marguerite Blair <margueriteblair@MargueritesMBP2.fios-router.home>
2021-08-10 20:52:27 -0400

    O(mn)

--- CrackingTheCodingInterview/BigONotation/PracticeProblems.java
+++ CrackingTheCodingInterview/BigONotation/PracticeProblems.java
@@ -12,4 +12,16 @@ public class PracticeProblems {
     }
     //it's the sum of 1 through n-1; the sum of 1 -> n-1 is (n(n-1))/2
     //this one has a runtime of O(n^2)
+
+    public void printUnorderedPairs2(int[] arrayA, int[] arrayB) {
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayA[i] < arrayB[i]) {
+                    //this if statement is constant time
+                    System.out.println(arrayA[i] + " " + arrayB[i]);
+                }
+            }
+        }
+    }
+    //this is O(nm) we've got two inputs of potentially varying size
 }
