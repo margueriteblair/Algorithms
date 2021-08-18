926ffcf4cf7a3809470069bce2a02268869919d9
Marguerite Blair <marguerite.blair@gmail.com>
2021-08-17 20:05:42 -0400

    another iteration

--- CrackingTheCodingInterview/TechnicalQuestions/SumOfCubes.java
+++ CrackingTheCodingInterview/TechnicalQuestions/SumOfCubes.java
@@ -11,7 +11,38 @@ class SubOfCubes {
                 for (int c = 1; c <= limit; c++) {
                     for (int d = 1; d <= limit; d++) {
                         if ((Math.pow(a, 3)+Math.pow(b, 3)) == (Math.pow(c, 3)+Math.pow(d, 3))) {
-                            System.out.println("a: " + a + " b: " + b + " c: " + c + " d: " + d);                    }
+                            System.out.println("a: " + a + " b: " + b + " c: " + c + " d: " + d);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    static void sumCubes2() {
+        int limit = 1000;
+        for (int a = 1; a <= limit; a++) {
+            for (int b = 1; b <= limit; b++) {
+                for (int c = 1; c <= limit; c++) {
+                    for (int d = 1; d <= limit; d++) {
+                        if ((Math.pow(a, 3)+Math.pow(b, 3)) == (Math.pow(c, 3)+Math.pow(d, 3))) {
+                            System.out.println("a: " + a + " b: " + b + " c: " + c + " d: " + d);
+                            break;                    }
+                    }
+                }
+            }
+        }
+    }
+
+    static void sumCubes3() {
+        int limit = 1000;
+        for (int a = 1; a <= limit; a++) {
+            for (int b = 1; b <= limit; b++) {
+                for (int c = 1; c <= limit; c++) {
+                    int d = (int)Math.pow(Math.pow(a, 3)+Math.pow(b, 3)-Math.pow(c, 3), (1/3));
+                    if ((Math.pow(a, 3)+Math.pow(b, 3)) == (Math.pow(c, 3)+Math.pow(d, 3))) {
+                        System.out.println("a: " + a + " b: " + b + " c: " + c + " d: " + d);
+                        break;                    
                     }
                 }
             }
