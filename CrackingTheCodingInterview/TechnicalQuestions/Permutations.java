2fa964be994516c6e2a00ecf23a85ead3a0718a8
Marguerite Blair <marguerite.blair@gmail.com>
2021-08-20 17:33:13 -0400

    permutations count of a big str and a lil string

--- CrackingTheCodingInterview/TechnicalQuestions/Permutations.java
+++ CrackingTheCodingInterview/TechnicalQuestions/Permutations.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Permutations {
@@ -5,7 +6,8 @@ public class Permutations {
         
     }
 
-    static void permutations(String s, String b) {
+    static int permutations(String s, String b) {
+        int countPerms = 0;
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             int count = map.getOrDefault(s.charAt(i), 0);
@@ -14,6 +16,29 @@ public class Permutations {
 
         for (int i = 0; i < b.length(); i++) {
             //check every s.length() chars and see if they meet the chars in the hashmap
+            String sub = b.substring(i, i+s.length());
+            HashMap<Character, Integer> copy = map;
+            if (checkContents(sub, copy)) {
+                countPerms++;
+            }
         }
+
+        return countPerms;
+    }
+
+    static boolean checkContents(String str, HashMap<Character, Integer> map) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!map.containsKey(str.charAt(i))) {
+                return false;
+            } else {
+                int count = map.get(str.charAt(i));
+                count--;
+                if (count == 0) {
+                    map.remove(str.charAt(i));
+                }
+                map.put(str.charAt(i), count);
+            }
+        }
+        return map.isEmpty() ? true : false;
     }
 }
