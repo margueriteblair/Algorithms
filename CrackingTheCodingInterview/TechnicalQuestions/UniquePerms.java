98532b5f9974fa50d091f9b1167645dc5870ee74
Marguerite Blair <marguerite.blair@gmail.com>
2021-08-21 18:47:15 -0400

    perms

--- CrackingTheCodingInterview/TechnicalQuestions/UniquePerms.java
+++ CrackingTheCodingInterview/TechnicalQuestions/UniquePerms.java
@@ -1,5 +1,26 @@
 public class UniquePerms {
     public static void main(String[] args) {
-        s
+        
+    }
+
+    static String swapString(String a, int i, int j) {
+        char[] b = a.toCharArray();
+        char ch;
+        ch = b[i];
+        b[i] = b[j];
+        b[j] = ch; //the swap
+        return String.valueOf(b);
+    }
+
+    static void generatePermutations(String str, int start, int end) {
+        if (start == end-1) {
+            System.out.println(str);
+        } else {
+            for (int i = start; i < end; i++) {
+                str = swapString(str, start, i);
+                generatePermutations(str, start+1, end);
+                str = swapString(str, start, i);
+            }
+        }
     }
 }
