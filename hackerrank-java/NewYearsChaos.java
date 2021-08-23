e8c2af74234dfc6d1f27288d9d83cdd9b30ab216
Marguerite Blair <marguerite.blair@gmail.com>
2021-08-23 18:19:05 -0400

    new years chaos first draft with errors

---
+++ hackerrank-java/NewYearsChaos.java
@@ -0,0 +1,26 @@
+public class NewYearsChaos {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void minimumBribes(List<Integer> q) {
+        // Write your code here
+            int bribes = 0;
+            for (int i = 0; i < q.size(); i++) {
+                if (q.get(i) != (i+1)) {
+                    int j = i;
+                    while (q.get(j) != q.get(i)-1) {
+                        j++;
+                    }
+                    if ((j - i) > 2) {
+                        System.out.println("Too Chaotic");
+                        return;
+                    } else {
+                        bribes += (j-i);
+                    }
+                }
+            }
+            System.out.println(bribes);
+        }
+    }
+}
