54f7f45cfb44981c2564f8b3c8a6fcb8c12be59c
Marguerite Blair <marguerite.blair@gmail.com>
2021-08-24 16:09:50 -0400

    valid string

---
+++ hackerrank-java/ValidString.java
@@ -0,0 +1,29 @@
+public class ValidString {
+    public static void main(String[] args) {
+        
+    }
+
+    public static String isValid(String s) {
+        // Write your code here
+            Map<Character, Integer> charMap = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                int count = charMap.getOrDefault(s.charAt(i), 0);
+                charMap.put(s.charAt(i), count+1);
+            }
+            List<Integer> arr = new ArrayList<>();
+            for (int val : charMap.values()) {
+                arr.add(val);
+            }
+            Collections.sort(arr);
+            System.out.println(arr);
+            if (arr.get(0) == arr.get(arr.size()-1)) {
+                return "YES";
+            } else if (arr.get(0) == arr.get(arr.size()-2) && arr.get(0)+1 == arr.get(arr.size()-1)) {
+                return "YES";
+            } else if (arr.get(0) == 1 && arr.get(1) == arr.get(arr.size()-1)) {
+                return "YES";
+            } else {
+               return "NO"; 
+            }
+        }
+}
