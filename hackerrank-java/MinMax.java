6f911cbb96a06948a7751a5dae3c9e46d8eb1a10
Marguerite Blair <marguerite.blair@gmail.com>
2021-08-29 11:32:52 -0400

    minMax

--- hackerrank-java/MinMax.java
+++ hackerrank-java/MinMax.java
@@ -5,15 +5,13 @@ public class MinMax {
 
     public static int maxMin(int k, List<Integer> arr) {
         // Write your code here
-            int[] arrPrime = new int[k];
             Collections.sort(arr);
             int minMax = arr.get(k-1) - arr.get(0);
             for (int i = 0; i < arr.size()-k; i++) {
-                if (arr.get(i+k) - arr.get(i) < minMax) {
-                    minMax = arr.get(k-1) - arr.get(0);
+                if (arr.get(i+k-1) - arr.get(i) < minMax) {
+                    minMax = arr.get(k+i-1) - arr.get(i);
                 }
             }
             return minMax;
-    
         }
 }
