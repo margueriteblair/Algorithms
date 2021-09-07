afa9fdb3994560b5f1883b5d47a2b055c40276ad
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-07 13:38:12 -0400

    very inefficient solution

--- CrackingTheCodingInterview/Udemy/TrappingRainWater.java
+++ CrackingTheCodingInterview/Udemy/TrappingRainWater.java
@@ -9,19 +9,20 @@ public class TrappingRainWater {
         int totalWater = 0;
         for (int i = 0; i < height.length-1; i++) {
             
-            int maxHeightLeft = 0;
+            int left = 0;
             for (int j = 0; j < i; j++) {
-                if (height[j] > maxHeightLeft) {
-                    maxHeightLeft = height[j];
-                }
+                left = Math.max(left, height[j]);
             }
-            int maxHeightRight = 0;
+            int right = 0;
             for (int j = i+1; j < height.length; j++) {
-                if (height[j] > maxHeightRight) {
-                    maxHeightRight = height[j];
-                }
+                right = Math.max(height[j], right);
             }
-            totalWater += (Math.min(maxHeightRight, maxHeightLeft)-height[i]);
+            if ((Math.min(right, left)-height[i]) < 0) {
+                totalWater += 0;
+            } else {
+                totalWater += (Math.min(right, left)-height[i]);
+            }
+            
         }
         return totalWater;
     }
