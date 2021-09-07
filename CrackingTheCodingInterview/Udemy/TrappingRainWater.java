f20d214b17916df99866cc05e7ed7db54b297087
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-07 13:35:33 -0400

    new

--- CrackingTheCodingInterview/Udemy/TrappingRainWater.java
+++ CrackingTheCodingInterview/Udemy/TrappingRainWater.java
@@ -8,16 +8,20 @@ public class TrappingRainWater {
     public int trap(int[] height) {
         int totalWater = 0;
         for (int i = 0; i < height.length-1; i++) {
-            int temp = 0;
-            for (int j = i+1; j < height.length; j++) {
-                if (height[j] < height[i]) {
-                    temp += height[i]-height[j];
-                } else {
-                    totalWater += temp;
-                    i = j;
-                    continue;
+            
+            int maxHeightLeft = 0;
+            for (int j = 0; j < i; j++) {
+                if (height[j] > maxHeightLeft) {
+                    maxHeightLeft = height[j];
                 }
             }
+            int maxHeightRight = 0;
+            for (int j = i+1; j < height.length; j++) {
+                if (height[j] > maxHeightRight) {
+                    maxHeightRight = height[j];
+                }
+            }
+            totalWater += (Math.min(maxHeightRight, maxHeightLeft)-height[i]);
         }
         return totalWater;
     }
