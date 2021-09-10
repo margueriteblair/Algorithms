fb90a964e6a5fa8be885b1fd49e9bdcb7f71c5c0
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-10 18:56:04 -0400

    valid palindrome

---
+++ CrackingTheCodingInterview/Udemy/ValidPalindrome.java
@@ -0,0 +1,22 @@
+package CrackingTheCodingInterview.Udemy;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        
+    }
+
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        System.out.println(s);
+        int i = 0;
+        int j = s.length()-1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
