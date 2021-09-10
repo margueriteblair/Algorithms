ebeecf237b2f2be0bd293d51f6d8e39858ebc85b
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-09 23:08:37 -0400

    full first draft

--- CrackingTheCodingInterview/Udemy/LongestSubstringNoRepeats.java
+++ CrackingTheCodingInterview/Udemy/LongestSubstringNoRepeats.java
@@ -1,5 +1,8 @@
 package CrackingTheCodingInterview.Udemy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubstringNoRepeats {
     public static void main(String[] args) {
         
@@ -16,7 +19,11 @@ public class LongestSubstringNoRepeats {
         for (int j = 0; j < s.length(); j++) {
             if (!set.contains(s.charAt(j))) {
                 set.add(s.charAt(j));
+                maxLen = Math.max(maxLen, set.size());
+            } else {
+                set = new HashSet<>();
             }
         }
+        return maxLen;
     }
 }
