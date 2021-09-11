eac5bacff85c2e4268c7a1245aa315c599b09b52
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-11 14:43:09 -0400

    hi

--- CrackingTheCodingInterview/Udemy/ValidPalindrome.java
+++ CrackingTheCodingInterview/Udemy/ValidPalindrome.java
@@ -5,23 +5,9 @@ public class ValidPalindrome {
         
     }
 
-    public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        System.out.println(s);
-        int i = 0;
-        int j = s.length()-1;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-
     public boolean validPalindrome(String s) {
-        if (isPalindrome2(s, null)) {
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        if (isPalindrome(s, s.length())) {
             return true;
         }
         int i = 0;
@@ -41,10 +27,10 @@ public class ValidPalindrome {
             }
         }
         
-        
+        return false;
     }
     
-    public static boolean isPalindrome2(String s, int index) {
+    public static boolean isPalindrome(String s, int index) {
         s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
         
         int i = 0;
