182e89914143366aaab92a12ac0f18b6d2d32679
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-13 17:34:44 -0400

    linkedlist2

---
+++ CrackingTheCodingInterview/Udemy/ReverseLinkedList2.java
@@ -0,0 +1,22 @@
+package CrackingTheCodingInterview.Udemy;
+
+public class ReverseLinkedList2 {
+    public static void main(String[] args) {
+        
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null) {
+            return new ListNode(0);
+        }
+        int counter = 1;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            counter++;
+            if (counter >= left && count <= right) {
+                ListNode next
+            }
+        }
+    }
+}
