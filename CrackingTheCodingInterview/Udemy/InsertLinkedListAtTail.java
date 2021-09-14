e515ecd6e579f068391f3523e2441b2b8417994d
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-14 14:28:47 -0400

    insert with a while loop

--- CrackingTheCodingInterview/Udemy/InsertLinkedListAtTail.java
+++ CrackingTheCodingInterview/Udemy/InsertLinkedListAtTail.java
@@ -19,6 +19,20 @@ public class InsertLinkedListAtTail {
         head = new Node(data);
         return head;
     }
+
+    public static  Node insert2(Node head,int data) {
+        Node newhead = new Node(data);
+        if(head != null) {
+            Node current = head;
+            while(current.next != null) {
+                current = current.next;
+            }
+            current.next = newhead;
+            return head;
+        } else {
+            return newhead;
+        }
+  }
 }
 
 class Node {
