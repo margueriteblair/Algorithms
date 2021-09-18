c4960f81f63f6b57ba056ff0c522db4c63f6de38
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-17 20:25:56 -0400

    list node cycle detection

---
+++ CrackingTheCodingInterview/Udemy/LinkedListCycle.java
@@ -0,0 +1,19 @@
+package CrackingTheCodingInterview.Udemy;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        
+    }
+    public ListNode detectCycle(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        ListNode curr = head;
+        while (curr.next != null) {
+            if (list.contains(curr)) {
+                return curr;
+            }
+            list.add(curr);
+            curr = curr.next;
+        }
+        return curr;
+    }
+}
