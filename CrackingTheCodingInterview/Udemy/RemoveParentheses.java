5f439255dc331554706cfe286ded52b3fa9ed505
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-21 22:37:18 -0400

    stacks

---
+++ CrackingTheCodingInterview/Udemy/RemoveParentheses.java
@@ -0,0 +1,25 @@
+package CrackingTheCodingInterview.Udemy;
+
+public class RemoveParentheses {
+    public static void main(String[] args) {
+        
+    }
+
+    public String minRemoveToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int leftCount = 0;
+        int rightCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    rightCount++;
+                }
+            }
+        }
+        leftCount += stack.size();
+        System.out.println(leftCount + " " + rightCount);
+        return s;
+    }
+}
