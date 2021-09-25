f86d32719d6e8aada7735076086066818106af68
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-25 15:13:35 -0400

    my queue

--- CrackingTheCodingInterview/Udemy/QueueStacks.java
+++ CrackingTheCodingInterview/Udemy/QueueStacks.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class MyQueue {
     Stack<Integer> s1 ;
     Stack<Integer> s2 ;
@@ -31,5 +33,37 @@ class MyQueue {
     
     public boolean empty() {
         return s1.isEmpty() && s2.isEmpty();
+    }
+}
+
+class MyQueue2 {
+
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        s1.push(x);
+        
+    }
+    
+    public int pop() {
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        return s2.pop();
+    }
+    
+    public int peek() {
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        return s2.peek();
+    }
+    
+    public boolean empty() {
+        return s2.isEmpty() && s1.isEmpty();
     }
 }
