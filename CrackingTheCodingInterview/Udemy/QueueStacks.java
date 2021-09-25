d30d0f8d82ff308ed29f2cb67256bd3c94dd1b16
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-24 22:25:53 -0400

    it works

--- CrackingTheCodingInterview/Udemy/QueueStacks.java
+++ CrackingTheCodingInterview/Udemy/QueueStacks.java
@@ -1,33 +1,35 @@
 class MyQueue {
-    Stack<Integer> ordered = new Stack<>();
-    Stack<Integer> reverse = new Stack<>();
+    Stack<Integer> s1 ;
+    Stack<Integer> s2 ;
+    int top , first;
     public MyQueue() {
-        
+        s1 = new Stack();
+        s2 = new Stack();
+        top = -1;
     }
     
     public void push(int x) {
-        ordered.push(x);
-
+        if(s1.isEmpty()) first = x;
+        s1.push(x);
     }
     
     public int pop() {
-        while (!ordered.isEmpty()) {
-            reverse.push(ordered.pop());
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
         }
-        return reverse.pop();
+        int del = s2.pop();
+        if(!s2.isEmpty()) top = s2.peek();
+        return del;
     }
     
     public int peek() {
-        while (!ordered.isEmpty()) {
-            reverse.push(ordered.pop());
-        }
-        return reverse.peek();
+        if(s2.isEmpty()) return first;
+        return top;
     }
     
     public boolean empty() {
-        if (reverse.size() == 0 && ordered.size() == 0) {
-            return true;
-        }
-        return false;
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
