d8cc233e02028d2782e1073c610429b3dc383e4c
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-27 18:14:27 -0400

    insertion sort

--- CrackingTheCodingInterview/Udemy/Sorting.java
+++ CrackingTheCodingInterview/Udemy/Sorting.java
@@ -32,4 +32,16 @@ public class Sorting {
         }
         System.out.println(arr);
     }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
 }
