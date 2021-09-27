a8ad7436cbb4b88e793b5312f661b743a0e51b76
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-27 17:49:34 -0400

    seletion sort

--- CrackingTheCodingInterview/Udemy/Sorting.java
+++ CrackingTheCodingInterview/Udemy/Sorting.java
@@ -11,6 +11,25 @@ public class Sorting {
                 }
             }
         }
-        return arr;
+        System.out.println(arr);
+    }
+    //space O(1)
+    //time O(N^2)
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int localMin = arr[i];
+            int localMinIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < localMin) {
+                    localMin = arr[j];
+                    localMinIndex = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = localMin;
+            arr[localMinIndex] = tmp;
+        }
+        System.out.println(arr);
     }
 }
