742045a650f88d4d944b158966b0632657a6a156
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-27 17:37:10 -0400

    bubble sort implementation

---
+++ CrackingTheCodingInterview/Udemy/Sorting.java
@@ -0,0 +1,16 @@
+package CrackingTheCodingInterview.Udemy;
+
+public class Sorting {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
