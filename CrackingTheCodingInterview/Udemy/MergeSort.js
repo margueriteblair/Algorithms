e4e477fa6d59bc95eca0398207b13b0fe703bce8
Marguerite Blair <marguerite.blair@gmail.com>
2021-09-29 22:41:22 -0400

    merge sort

--- CrackingTheCodingInterview/Udemy/MergeSort.js
+++ CrackingTheCodingInterview/Udemy/MergeSort.js
@@ -10,3 +10,21 @@ function mergeSort(arr) {
 
     return merge(mergeSort(left), mergeSort(right));
 }
+
+function merge(left, right) {
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex])
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
