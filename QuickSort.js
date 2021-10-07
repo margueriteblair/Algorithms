101775518101ad072ab5aedeb755b4ea437d7e0d
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-06 21:41:34 -0400

    made the partition func for quick sort

--- QuickSort.js
+++ QuickSort.js
@@ -5,3 +5,19 @@ const quickSort = function(array, left, right) {
         quickSort(array, left, partitionIdx+1);
     }
 }
+
+const partition = function(array, left, right) {
+    const pivot = array[right];
+    let partitionInd = left;
+    for (let j = left; j < right; j++) {
+        if (array[j] < pivot) {
+            swap(array, partitionInd, j);
+            partitionInd++;
+        }
+    }
+    swap(array, partitionInd, right);
+}
+
+const swap = function() {
+    
+}
