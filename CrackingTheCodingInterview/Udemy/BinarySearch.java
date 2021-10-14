123b37b9f676d154f2b5bb3ce7375201bfd302c3
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-14 19:43:28 -0400

    binary search

--- CrackingTheCodingInterview/Udemy/BinarySearch.java
+++ CrackingTheCodingInterview/Udemy/BinarySearch.java
@@ -2,14 +2,15 @@ package CrackingTheCodingInterview.Udemy;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        
+        System.out.println(binarySearch(new int[] {1,2,3,4,5}, 2));
     }
 
     public static int binarySearch(int[] arr, int target) {
         int left = 0;
         int right = arr.length-1;
-        int mid = (left+right)/2;
+        
         while (left != right) {
+            int mid = (left+right)/2;
             if (target < mid) {
                 right = mid-1;
             } else if (target > mid) {
