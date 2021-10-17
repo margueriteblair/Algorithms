07bcc6183b5a21e557eae45d88fa124791c8d153
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-17 17:47:06 -0400

    finished

--- CrackingTheCodingInterview/Udemy/FirstAndLastPos.js
+++ CrackingTheCodingInterview/Udemy/FirstAndLastPos.js
@@ -26,6 +26,14 @@ const searchRange = function(nums, target) {
     let temp1, temp2;
     while (startPos !== -1) {
         temp1 = startPos;
-        startPos = binarySearch(nums, 0, )
+        startPos = binarySearch(nums, 0, startPos-1, target);
     }
+    startPos = temp1;
+
+    while (endPos !== -1) {
+        temp2 = endPos;
+        endPos = binarySearch(nums, endPos+1, nums.length-1, target);
+    }
+    endPos = temp2;
+    return [startPos, endPos]
 }
