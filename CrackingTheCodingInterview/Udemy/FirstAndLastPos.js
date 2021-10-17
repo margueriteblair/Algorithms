dcf061c4ff2be3be3a6e11913c47b221861edf90
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-17 17:45:20 -0400

    rewriting search range problem

--- CrackingTheCodingInterview/Udemy/FirstAndLastPos.js
+++ CrackingTheCodingInterview/Udemy/FirstAndLastPos.js
@@ -1,44 +1,31 @@
-const array = [1,3,3,5,5,5,8,9];
-const targetToFind = 5;
-
-const binarySearch = (nums, left, right, target) => {
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    const foundVal = nums[mid];
-    if (foundVal === target) {
-      return mid;
-    } else if (foundVal < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+const binarySearch = function(nums, left, right, target) {
+    while (left <= right) {
+        const mid = Math.floor((left+right)/2);
+        const found = nums[mid];
+        if (found === target) {
+            return mid;
+        } else if (found < target) {
+            left = mid+1;
+        } else if (found > target) {
+            right = mid-1;
+        }
     }
-  }
+    return -1;
+}
 
-  return -1;
-};
-
-const searchRange = function (nums, target) {
-  if (nums.length < 1) return [-1, -1];
-  const firstPos = binarySearch(nums, 0, nums.length - 1, target);
-
-  if (firstPos === -1) return [-1, -1];
-
-  let endPos = firstPos,
-    startPos = firstPos,
-    temp1,
-    temp2;
-
-  while (startPos !== -1) {
-    temp1 = startPos;
-    startPos = binarySearch(nums, 0, startPos - 1, target);
-  }
-  startPos = temp1;
-
-  while (endPos !== -1) {
-    temp2 = endPos;
-    endPos = binarySearch(nums, endPos + 1, nums.length - 1, target);
-  }
-  endPos = temp2;
-
-  return [startPos, endPos];
-};
+const searchRange = function(nums, target) {
+    if (nums.length === 0) {
+        return [-1, -1]
+    }
+    const firstPos = binarySearch(nums, 0, nums.length-1, target);
+    if (firstPos === -1) {
+        return [-1, -1];
+    }
+    let startPos = firstPos;
+    let endPos = firstPos;
+    let temp1, temp2;
+    while (startPos !== -1) {
+        temp1 = startPos;
+        startPos = binarySearch(nums, 0, )
+    }
+}
