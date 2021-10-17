d22c3f3731e0dc5271efb408581ddb9104773201
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-17 17:56:38 -0400

    it works

--- CrackingTheCodingInterview/Udemy/FirstAndLastPos.java
+++ CrackingTheCodingInterview/Udemy/FirstAndLastPos.java
@@ -1,12 +1,25 @@
 package CrackingTheCodingInterview.Udemy;
 
+import java.util.Arrays;
+
 public class FirstAndLastPos {
     public static void main(String[] args) {
-        
+        System.out.println(Arrays.toString(searchRange(new int[] {1,1,2,3,4,5,5,5,7,8}, 5)));
     }
 
     public static int binarySearch(int[] arr, int left, int right, int target) {
-
+        while (left <= right) {
+            int mid = (left+right)/2;
+            int found = arr[mid];
+            if (found == target) {
+                return mid;
+            } else if (found > target) {
+                right = mid-1;
+            } else if (found < target) {
+                left = mid+1;
+            }
+        }
+        return -1;
     }
 
     public static int[] searchRange(int[] arr, int target) {
