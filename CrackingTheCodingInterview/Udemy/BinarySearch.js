179fda6f767cee5519eaa0efdd629f4bc2f1c4ea
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-16 22:11:42 -0400

    binary search in javascript

---
+++ CrackingTheCodingInterview/Udemy/BinarySearch.js
@@ -0,0 +1,15 @@
+const binarySearch = function(arr, target) {
+    let left = 0, right = arr.length-1;
+    while (left <= right) {
+        let mid = Math.floor((left+right)/2);
+        if (target === arr[mid]) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid+1;
+        } else if (arr[mid] > target) {
+            right = mid-1;
+        }
+    }
+    return -1;
+}
+console.log(binarySearch([1,2,3,4,5,6,7]), 7)
