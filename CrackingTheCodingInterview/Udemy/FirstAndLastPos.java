528d0b759b9a0f4c1c0aaebdd8772009ff2485ab
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-17 17:51:17 -0400

    initialized more vars

--- CrackingTheCodingInterview/Udemy/FirstAndLastPos.java
+++ CrackingTheCodingInterview/Udemy/FirstAndLastPos.java
@@ -10,6 +10,20 @@ public class FirstAndLastPos {
     }
 
     public static int[] searchRange(int[] arr, int target) {
-        int res = new int[2];
+        int[] res = new int[2];
+        if (arr.length == 0) {
+            res[0] = -1;
+            res[1] = -1;
+            return res;
+        }
+        int firstPos = binarySearch(arr, 0, arr.length-1, target);
+        if (firstPos == -1) {
+            res[0] = -1;
+            res[1] = -1;
+            return res;
+        }
+        int startPos = firstPos;
+        int endPos = firstPos;
+        int temp1, temp2;
     }
 }
