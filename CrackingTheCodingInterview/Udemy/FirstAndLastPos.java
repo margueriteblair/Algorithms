6d45a2226303cdad4f31b8aa3987e5abbb56560b
Marguerite Blair <marguerite.blair@gmail.com>
2021-10-17 17:49:16 -0400

    set up for java search range algo

---
+++ CrackingTheCodingInterview/Udemy/FirstAndLastPos.java
@@ -0,0 +1,15 @@
+package CrackingTheCodingInterview.Udemy;
+
+public class FirstAndLastPos {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int binarySearch(int[] arr, int left, int right, int target) {
+
+    }
+
+    public static int[] searchRange(int[] arr, int target) {
+        int res = new int[2];
+    }
+}
