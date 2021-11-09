63c92e134822d703b7a59c256671aef25ffe037c
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-08 23:41:58 -0500

    small syntax change so it matches up with the leetcode problem

--- CrackingTheCodingInterview/LevelOrderTraversal.js
+++ CrackingTheCodingInterview/LevelOrderTraversal.js
@@ -1,25 +1,42 @@
-const levelOrder = function(root) {
-    if (!root) {
-        return [];
-    }
-    let res = [];
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if(!root) return [];
+    const result = [];
     const queue = [root];
-    while (queue.length) {
-        let length = queue.length;
-        let count = 0;
-        const currentLevelValues = [];
-        while (count < length) {
-            const currentNode = queue.shift();
-            currentLevelValues.push(currentNode.value);
-            if (currentNode.left) {
-                queue.push(currentNode.left);
-            }
-            if (currentNode.right) {
-                queue.push(currentNode.right);
-            }
-            count++;
+    
+    while(queue.length) {
+      const currentLevelValues = [];
+      let length = queue.length, count = 0;
+  
+      while(count < length) {
+        const currentNode = queue.shift();
+        
+        currentLevelValues.push(currentNode.val);
+        
+        if(currentNode.left) {
+          queue.push(currentNode.left)
         }
-        res.push(currentLevelValues);
+        
+        if(currentNode.right) {
+          queue.push(currentNode.right)
+        }
+  
+        count++;
+      }
+      
+      result.push(currentLevelValues);
     }
-    return res;
-}
+    
+    return result;
+  };
