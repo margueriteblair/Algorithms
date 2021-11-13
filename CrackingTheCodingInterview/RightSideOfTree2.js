7bced25c6beda6fbd2cf15553985d18d5aa7aeef
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-12 23:47:02 -0500

    right side of the tree

--- CrackingTheCodingInterview/RightSideOfTree2.js
+++ CrackingTheCodingInterview/RightSideOfTree2.js
@@ -1,5 +1,7 @@
 //This is the DFS solution of finding the right side viewpoint of our BST
 //In DFS, we explore as far down on one branch as we can before switching nodes
+//1. We prioritize the right side
+//2. We keep track of the level of the nodes
 
 class TreeNode {
     constructor(value) {
