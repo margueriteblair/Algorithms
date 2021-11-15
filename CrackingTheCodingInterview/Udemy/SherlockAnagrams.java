945c13caa7c3d93b6ac90131671c02de1db3706b
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-14 22:45:19 -0500

    adding secondary helper function to see if its actually an anagram

--- CrackingTheCodingInterview/Udemy/SherlockAnagrams.java
+++ CrackingTheCodingInterview/Udemy/SherlockAnagrams.java
@@ -15,4 +15,18 @@ public class SherlockAnagrams {
         return 0;
 
     }
+    public static boolean isAnagram(String s1, String s2) {
+        Map<Character, Integer> s1Map = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            int count = s1Map.getOrDefault(s1.charAt(i), 0);
+            s1Map.put(s1.charAt(i), count++);
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            if (!s1Map.containsKey(s2.charAt(i))) {
+                return false;
+            } else if (s1Map.containsKey(s2.charAt(i))) {
+                
+            }
+        }
+    }
 }
