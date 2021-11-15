c2ad6695953b5e69d083906b27de595c5602d1cb
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-15 15:08:29 -0500

    get or default method set to work

--- CrackingTheCodingInterview/Udemy/SherlockAnagrams.java
+++ CrackingTheCodingInterview/Udemy/SherlockAnagrams.java
@@ -33,18 +33,12 @@ public class SherlockAnagrams {
         Map<Character, Integer> s1Map = new HashMap<>();
         Map<Character, Integer> s2Map = new HashMap<>();
         for (int i = 0; i < s1.length(); i++) {
-            if (s1Map.containsKey(s1.charAt(i))) {
-                s1Map.put(s1.charAt(i), s1Map.get(s1.charAt(i))+1);
-            } else {
-                s1Map.put(s1.charAt(i), 1);
-            }
+            int count = s1Map.getOrDefault(s1.charAt(i), 0);
+            s1Map.put(s1.charAt(i), count+1);
         }
         for (int i = 0; i < s2.length(); i++) {
-            if (s2Map.containsKey(s2.charAt(i))) {
-                    s2Map.put(s2.charAt(i), s2Map.get(s2.charAt(i))+1);
-            } else {
-                    s2Map.put(s2.charAt(i), 1);
-            }
+            int count = s2Map.getOrDefault(s2.charAt(i), 0);
+            s2Map.put(s2.charAt(i), count+1);
         }
         return s1Map.equals(s2Map);
     }
