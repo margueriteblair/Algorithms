c84c61dab81f31b784cf14a9a0ac9715ec22ef1b
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-19 13:35:08 -0500

    beautiful days algorithm

---
+++ CrackingTheCodingInterview/TechnicalQuestions/SunnyDays.java
@@ -0,0 +1,23 @@
+public class SunnyDays {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int beautifulDays(int i, int j, int k) {
+        // Write your code here
+            int count = 0;
+            for (int a = i; a <= j; a++) {
+                String aStr = String.valueOf(a);
+                String reversed = "";
+                for (int b = aStr.length()-1; b >= 0; b--) {
+                    reversed += String.valueOf(aStr.charAt(b));
+                }
+                int revInt = Integer.parseInt(reversed);
+                if (((a - revInt) % k) == 0 ) {
+                    count++;
+                }
+            }
+            return count;
+    
+        }
+}
