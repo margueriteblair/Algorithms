d6732f4269f3495a8c60dd06b2967b40dc054ac1
Marguerite Blair <marguerite.blair@gmail.com>
2021-12-23 10:41:02 -0500

    mixer

--- CrackingTheCodingInterview/Udemy/MinutesToSeconds.java
+++ CrackingTheCodingInterview/Udemy/MinutesToSeconds.java
@@ -3,15 +3,8 @@ public class MinutesToSeconds {
         
     }
 
-    public static String rev(int n) {
-		String returnStr = "";
-		String nString = Integer.toString(n);
-		for (int i = nString.length()-1; i >= 0; i--) {
-			returnStr += String.valueOf(String.valueOf(nString.charAt(i)));
-		}
-		if (n < 0) {
-			return returnStr.substring(0, returnStr.length()-1);
-		}
-		return returnStr;
+	public static int minutesToSeconds(String tm) {
+		String[] arr = tm.split(":");
+		return (Integer.parseInt(arr[0])*60)+Integer.parseInt(arr[1]);
 	}
 }
