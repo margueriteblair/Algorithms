6528cba3196532df25562faac45044e25e44788d
Marguerite Blair <marguerite.blair@gmail.com>
2021-12-23 10:40:44 -0500

    minutes to seconds

---
+++ CrackingTheCodingInterview/Udemy/MinutesToSeconds.java
@@ -0,0 +1,17 @@
+public class MinutesToSeconds {
+    public static void main(String[] args) {
+        
+    }
+
+    public static String rev(int n) {
+		String returnStr = "";
+		String nString = Integer.toString(n);
+		for (int i = nString.length()-1; i >= 0; i--) {
+			returnStr += String.valueOf(String.valueOf(nString.charAt(i)));
+		}
+		if (n < 0) {
+			return returnStr.substring(0, returnStr.length()-1);
+		}
+		return returnStr;
+	}
+}
