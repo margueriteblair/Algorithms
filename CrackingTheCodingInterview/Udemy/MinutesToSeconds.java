f6e7cd0f7a62362c4c6c581ae6ae703eb90f6644
Marguerite Blair <marguerite.blair@gmail.com>
2021-12-23 10:43:16 -0500

    if its over 60 seconds in the second spot, return -1

--- CrackingTheCodingInterview/Udemy/MinutesToSeconds.java
+++ CrackingTheCodingInterview/Udemy/MinutesToSeconds.java
@@ -2,9 +2,11 @@ public class MinutesToSeconds {
     public static void main(String[] args) {
         
     }
-
 	public static int minutesToSeconds(String tm) {
 		String[] arr = tm.split(":");
+		if (Integer.parseInt(arr[1]) > 59) {
+			return -1;
+		}
 		return (Integer.parseInt(arr[0])*60)+Integer.parseInt(arr[1]);
 	}
 }
