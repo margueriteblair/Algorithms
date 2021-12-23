76cd77e2c749d9ba16c43d8d9add59fb369bf089
Marguerite Blair <marguerite.blair@gmail.com>
2021-12-23 00:19:07 -0500

    solved with O^2 time complexity

--- CrackingTheCodingInterview/Udemy/LongestSubstringNoRepeats2.java
+++ CrackingTheCodingInterview/Udemy/LongestSubstringNoRepeats2.java
@@ -4,14 +4,19 @@ public class LongestSubstringNoRepeats2 {
     }
 
     public int lengthOfLongestSubstring(String s) {
+        if (s.length() < 2) {
+            return s.length();
+        }
+        
         int longest = 0;
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i+1; j < s.length()-1; j++) {
-                String sub = s.substring(i, j);
-                if (!sub.contains(String.valueOf(s.charAt(j)))) {
-                    if (sub.length()+1 > longest) {
-                        longest = sub.length()+1;
-                    }
+            Map<Character, Boolean> map = new HashMap<>();
+            int currentLength = 0;
+            for (int j = i; j < s.length(); j++) {
+                if (!map.containsKey(s.charAt(j))) {
+                    currentLength++;
+                    map.put(s.charAt(j), true);
+                    longest = Math.max(currentLength, longest);
                 } else {
                     break;
                 }
