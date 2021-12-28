104cbfef96e3f6533fa0e6ff2714fe4853ba54b6
Marguerite Blair <marguerite.blair@gmail.com>
2021-12-27 20:34:04 -0500

    first working solution but has a high time complexity ON2

---
+++ CrackingTheCodingInterview/Udemy/ValidSubPalindrome.java
@@ -0,0 +1,44 @@
+public class ValidSubPalindrome {
+    public static void main(String[] args) {
+        
+    }
+
+    public boolean validPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int invalidCount = 0;
+        int i = 0;
+        int j = s.length()-1;
+        
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                j--;
+                i++;
+            } else {
+                //"ceac"
+                if (!isPalindrome(s.substring(0, i) + s.substring(i+1))) {
+                    invalidCount++;
+                }
+                if (!isPalindrome(s.substring(0, j) + s.substring(j+1))) {
+                    invalidCount++;
+                }
+            }
+        }
+        if (invalidCount > 1) {
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
