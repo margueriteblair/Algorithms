5f0a7cfd0b61010f8da8cc1e2dcf8504f4bd37ab
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-05 21:40:50 -0500

    still working on it remove first method not working

--- CrackingTheCodingInterview/Udemy/RemoveParentheses.java
+++ CrackingTheCodingInterview/Udemy/RemoveParentheses.java
@@ -8,20 +8,32 @@ public class RemoveParentheses {
     }
     public String minRemoveToMakeValid(String s) {
         Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
                 stack.push(s.charAt(i));
             } else if (s.charAt(i) == ')') {
                 if (stack.isEmpty() || stack.pop() != '(') {
-                    s = s.substring(0, i) + "" + s.substring(i+1);
+                    arr[i] = Character.MIN_VALUE;
                 }
             }
         }
+        System.out.println(Arrays.toString(arr));
         int left = stack.size();
+
+        String res = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == Character.MIN_VALUE) {
+                res += "";
+            } else {
+              res += String.valueOf(arr[i]);  
+            }
+            
+        }
         while (left > 0) {
-            s = s.replaceFirst("(", "");
+            res = res.replaceFirst("\\(", "");
             left--;
         }
-        return s;
+        return res;
     }
 }
