4f6199ef7b8af5857d9a74310f2cb1e90486ea5c
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-06 11:54:19 -0500

    selection sort

---
+++ CrackingTheCodingInterview/Udemy/SelectionSort.java
@@ -0,0 +1,21 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        
+    }
+
+    function selectionSort(arr) {
+        for (let i = 0; i < arr.length; i++) {
+          let min = arr[i];
+          let swapIndex = i;
+          for (let j = i; j < arr.length; j++) {
+            if (arr[j] < min) {
+              min = arr[j];
+              swapIndex = j;
+            }
+          }
+          arr[swapIndex] = arr[i];
+          arr[i] = min;
+        }
+        console.log(arr);
+      }
+}
