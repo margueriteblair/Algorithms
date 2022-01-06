fa9b079f578ba8b1ecc9935bcd582c40f9ac9e10
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-06 00:15:50 -0500

    first attempt almost passed test cases

---
+++ CrackingTheCodingInterview/Udemy/IsListPalindrome.java
@@ -0,0 +1,22 @@
+public class IsListPalindrome {
+    public static void main(String[] args) {
+        
+    }
+
+    boolean solution(ListNode<Integer> l) {
+        Stack<Integer> stack = new Stack<>();
+        ListNode<Integer> copy = l;
+        while (l != null) {
+            stack.push(l.value);
+            l = l.next;
+        }
+        
+        while (copy != null) {
+            if (copy.value != stack.pop()) {
+                return false;
+            }
+            copy = copy.next;
+        }
+        return true;
+    }
+}
