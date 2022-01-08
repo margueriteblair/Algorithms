7a3267db8df5079c6eec30c2fe7ff17a4b6a1a5b
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-08 00:24:02 -0500

    a few changes to the algorithm to pass leetcode

--- CrackingTheCodingInterview/Udemy/BinarySearch.java
+++ CrackingTheCodingInterview/Udemy/BinarySearch.java
@@ -5,26 +5,29 @@ public class BinarySearch {
         System.out.println(binarySearch(new int[] {1,2,3,4,5}, 4));
     }
 
-    public static int binarySearch(int[] arr, int target) {
+    public int binarySearch(int[] nums, int target) {
         int left = 0;
-        int right = arr.length-1;
+        int right = nums.length-1;
+        if (nums[left] == target) return left;
+        if (nums[right] == target) return right;
         
-        while (left != right) {
+        while (left != right && right >= 0 && left <= nums.length-1) {
             int mid = (left+right)/2;
-            if (target == arr[left]) {
+            if (nums[left] == target) {
                 return left;
-            } else if (target == arr[right]) {
+            }
+            if (nums[right] == target) {
                 return right;
             }
-            if (target < arr[mid]) {
-                right = mid-1;
-            } else if (target > arr[mid]) {
-                left = mid+1;
-            } else if (target == arr[mid]) {
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else if (target == nums[mid]) {
                 return mid;
             }
         }
-        //if the target isn't found, return -1
+        
         return -1;
     }
 }
