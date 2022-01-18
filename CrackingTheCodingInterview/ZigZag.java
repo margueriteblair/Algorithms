5f0573e1457ee19dbb14fc646ed1888b32f6b625
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-18 17:08:36 -0500

    finishing up

--- CrackingTheCodingInterview/ZigZag.java
+++ CrackingTheCodingInterview/ZigZag.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 public class ZigZag {
     public static void main(String[] args) {
         int [][] a={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}} ;
+        System.out.println(Arrays.toString(zigzag(a)));
     }
 
-    public static int zigzag(int[][] matrix) {
+    public static int[] zigzag(int[][] matrix) {
         int[] res = new int[matrix.length*(matrix[0].length)];
         int count = 0;
         for (int i = 0; i < matrix.length; i++) {
