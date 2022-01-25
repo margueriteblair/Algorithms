d0219201c7c611a57d95757d67c7a0c5d22f9fbe
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-24 22:41:11 -0500

    slight refactor

--- CrackingTheCodingInterview/TechnicalQuestions/3Sum.java
+++ CrackingTheCodingInterview/TechnicalQuestions/3Sum.java
@@ -3,18 +3,16 @@ public class 3Sum {
         
     }
 
-    class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
             Map<Integer, Integer> map = new HashMap<>();
             List<List<Integer>> res = new ArrayList<>();
             for (int i = 0; i < nums.length; i++) {
-                int target = nums[i];
                 for (int j = 0; j < nums.length; j++) {
-                    if (map.containsKey(target-nums[j]) && i != j && j != map.get(target-nums[j]) && i != map.get(target-nums[j])) {
+                    if (map.containsKey(0 - nums[i] + nums[j]) && i != j && j != map.get(0-nums[i]-nums[j]) && i != map.get(0-nums[i]-nums[j])) {
                         List<Integer> curr = new ArrayList<>();
                         curr.add(nums[i]);
                         curr.add(nums[j]);
-                        curr.add(target-nums[j]);
+                        curr.add(0-nums[i]-nums[j]);
                         res.add(curr);
                     } else {
                         map.put(nums[j], j);
