f08d787dbaa695227d0b1a6d562952b4d7fea6ae
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-24 22:45:19 -0500

    better

--- CrackingTheCodingInterview/TechnicalQuestions/3Sum.java
+++ CrackingTheCodingInterview/TechnicalQuestions/3Sum.java
@@ -3,24 +3,27 @@ public class 3Sum {
         
     }
 
-        public List<List<Integer>> threeSum(int[] nums) {
-            Map<Integer, Integer> map = new HashMap<>();
-            List<List<Integer>> res = new ArrayList<>();
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = 0; j < nums.length; j++) {
-                    if (map.containsKey(0 - nums[i] + nums[j]) && i != j && j != map.get(0-nums[i]-nums[j]) && i != map.get(0-nums[i]-nums[j])) {
-                        List<Integer> curr = new ArrayList<>();
-                        curr.add(nums[i]);
-                        curr.add(nums[j]);
-                        curr.add(0-nums[i]-nums[j]);
+    public List<List<Integer>> threeSum(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (map.containsKey(0 - nums[i] - nums[j]) && i != j && j != map.get(0-nums[i]-nums[j]) && i != map.get(0-nums[i]-nums[j])) {
+                    List<Integer> curr = new ArrayList<>();
+                    curr.add(nums[i]);
+                    curr.add(nums[j]);
+                    curr.add(0-nums[i]-nums[j]);
+                    Collections.sort(curr);
+                    if (!res.contains(curr)) {
                         res.add(curr);
-                    } else {
-                        map.put(nums[j], j);
                     }
+                } else {
+                    map.put(nums[j], j);
                 }
             }
-            
-            System.out.println(map);
-            return res;
         }
+        
+        System.out.println(map);
+        return res;
+    }
 }
