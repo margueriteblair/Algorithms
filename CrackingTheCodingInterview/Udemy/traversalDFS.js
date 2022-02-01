c4236b74dd99d1dd905c9629918ab82ea572de18
Marguerite Blair <marguerite.blair@gmail.com>
2022-02-01 15:05:46 -0500

    traversal dfs

--- CrackingTheCodingInterview/Udemy/traversalDFS.js
+++ CrackingTheCodingInterview/Udemy/traversalDFS.js
@@ -1,12 +1,25 @@
-const traversalDFS = function(vertex, graph, values, seen) {
-    values.push(vertex);
-    seen[vertex] = true;
-    const connections = graph[vertex];
-    for (let i = 0; i < connections.length; i++) {
-        const connection = connections[i];
-        if (!seen[connection]) {
-            traversalDFS(connection, graph, values, seen);
+var numOfMinutes = function(n, headID, manager, informTime) {
+    const adjList = manager.map(() => []);
+    for (let e = 0; e < n; e++) {
+        const man = manager[e];
+        if (man == -1) {
+            continue;
         }
+        adjList[man].push(e);
     }
-    return values;
+    return dfs(headID, adjList, informTime);
+};
+
+const dfs = function(currentId, adjList, informTime) {
+  if(adjList[currentId].length === 0) {
+    return 0;
+  }
+  
+  let max = 0;
+  const subordinates = adjList[currentId];
+  for(let i = 0; i < subordinates.length; i++) {
+    max = Math.max(max, dfs(subordinates[i], adjList, informTime));
+  }
+  
+  return max + informTime[currentId];
 }
