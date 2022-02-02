6ad475afd4eeec0282d9a8b15c1a1c37c44ec5fd
Marguerite Bliar <marguerite.blair@gmail.com>
2022-02-02 12:33:52 -0500

    organizing containers

--- hackerrank-java/OrganizingContainers.java
+++ hackerrank-java/OrganizingContainers.java
@@ -22,4 +22,25 @@ public class OrganizingContainers {
             return rows.equals(columns) ? "Possible" : "Impossible";
     
         }
+
+        static String organizingContainers2(int[][] container) {
+            List<Integer> rows = new ArrayList<>();
+            List<Integer> columns = new ArrayList<>();
+            for (int i = 0; i < container.length; i++) {
+                int rowSum = 0;
+                for (int j = 0; j < container[0].length; j++) {
+                    rowSum += container[i][j];
+                }
+                rows.add(rowSum);
+            }
+            for (int i = 0; i < container[0].length; i++) {
+                int colSum = 0;
+                for (int j = 0; j < container.length; j++) {
+                    colSum += container[j][i];
+                }
+                columns.add(colSum);
+            }
+    
+            return rows.containsAll(columns) ? "Possible" : "Impossible";
+        }
 }
