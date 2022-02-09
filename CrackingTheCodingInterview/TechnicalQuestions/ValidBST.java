40770a1b20c8c751db57cb6a46b6be10886568d3
Marguerite Bliar <marguerite.blair@gmail.com>
2022-02-09 17:05:23 -0500

    is valid bst 2

--- CrackingTheCodingInterview/TechnicalQuestions/ValidBST.java
+++ CrackingTheCodingInterview/TechnicalQuestions/ValidBST.java
@@ -24,4 +24,20 @@ public class ValidBST {
             inOrderTraversal(node.right,arr);
         }
     }
+
+    public boolean isValidBST2(TreeNode root) {
+        return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    
+    public boolean isValid(TreeNode root, long min,long max){
+        if(root==null){
+            return true;
+        }
+        if(root.val<=min || root.val>=max){
+            return false;
+        }
+            
+        return isValid(root.left,min,root.val) && isValid(root.right,root.val,max);
+            
+    }
 }
