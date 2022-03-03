858cba2c78cd54868e2a67875576acef9b1cabcf
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-03 09:54:14 -0500

    getting some practice in

--- CrackingTheCodingInterview/Udemy/FirstAndLastPos3.java
+++ CrackingTheCodingInterview/Udemy/FirstAndLastPos3.java
@@ -44,4 +44,41 @@ public class FirstAndLastPos3 {
         
         return -1;
     }
+
+    public int[] searchRange2(int[] nums, int target) {
+        int[] res = new int[2];
+        int firstPos = binarySearch2(nums, target);
+        if (firstPos == -1) {
+            res[0] = -1;
+            res[1] = -1;
+            return res;
+        }
+        int i = firstPos, j = firstPos;
+        while (i > 0 && nums[i-1] == target) {
+            i--;
+        }
+        while (j < nums.length-1 && nums[j+1] == target) {
+            j++;
+        }
+        
+        res[0] = i;
+        res[1] = j;
+        return res;
+    }
+    
+    public static int binarySearch2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left <= right) {
+            int mid = (left+right)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid+1;
+            } else if (nums[mid] > target) {
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
 }
