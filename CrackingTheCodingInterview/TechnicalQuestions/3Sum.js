a80804b29afbaa4092a6f7894e598c863bd15b19
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-02 22:24:31 -0500

    3sum

--- CrackingTheCodingInterview/TechnicalQuestions/3Sum.js
+++ CrackingTheCodingInterview/TechnicalQuestions/3Sum.js
@@ -4,12 +4,15 @@ var threeSum = function(nums) {
     });
     let triplets = []; //will be a nested array;
     for (let i= 0; i < nums.length-2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
         let left = i+1;
         let right = nums.length-1;
         while (left < right) {
             let currSum = nums[i] + nums[left] + nums[right];
             if (currSum === 0) {
                 triplets.push([nums[i], nums[left], nums[right]]);
+                while (nums[left] === nums[left+1]) left++;
+                while (nums[right] === nums[right+1]) right--;
                 left++;
                 right--;
             } else if (currSum < 0) {
@@ -19,9 +22,5 @@ var threeSum = function(nums) {
             }
         }
     }
-
-    let arr = new Set(triplets.map(x => JSON.stringify(x)));
-    let uniques = [...arr].map(x => JSON.parse(x));
-    
-    return uniques;  
+    return triplets;  
 };
