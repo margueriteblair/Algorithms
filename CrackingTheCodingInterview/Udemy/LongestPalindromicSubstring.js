7d797407d8118fbbdcb18c2de0352d19a279638e
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-04 10:09:19 -0500

    longest palindromic substring optimized

--- CrackingTheCodingInterview/Udemy/LongestPalindromicSubstring.js
+++ CrackingTheCodingInterview/Udemy/LongestPalindromicSubstring.js
@@ -26,3 +26,25 @@ function longestPalindromicSubstring(string) {
       }
       return true;
   }
+
+
+//following two functions go from O(N^2), and O(1) space
+function longestPalindromicSubstring2(string) {
+    currentLongest = [0, 1]
+    for (let i = 1; i < string.length; i++) {
+        let odd = getLongestPalindromeFrom(string, i-1, i+1); //palindrome of odd length
+        let even = getLongestPalindromeFrom(string, i-1, i);
+        let longest = even[1] - even[0] > odd[1] - odd[0] ? even : odd; //remember, the even and odd variables are tuples
+        currentLongest = currentLongest[1] - currentLongest[0] > longest[1] - longest[0] ? currentLongest : longest;
+    }
+    return string.substring(currentLongest[0], currentLongest[1]);
+}
+
+function getLongestPalindromeFrom(string, left, right) {
+    while (left >= 0 && right < string.length) {
+        if (string[left] !== string[right]) break;
+        left--;
+        right++;
+    }
+    return [left+1, right];
+}
