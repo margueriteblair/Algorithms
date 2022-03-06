150f01c8d4bd3fda1f4f3423e1e1e0fc2a633140
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-06 13:00:14 -0500

    max path sum

--- CrackingTheCodingInterview/MaxPathSum.js
+++ CrackingTheCodingInterview/MaxPathSum.js
@@ -1,23 +1,25 @@
 function maxPathSum(tree) {
     // Write your code here.
-      let branch, maxSum = [...findMaxSum(tree)];
+      const [_, maxSum] = findMaxSum(tree);
       return maxSum
   }
   
   function findMaxSum(tree) {
       //recursive
-      if (!tree) {
-          return [0, 0];
+      if (tree === null) {
+          return [0, -Infinity];
       }
       
       //then computer lsb, ls, and rsb, rs
       //our two recursive calls
-      let leftSumBranch, leftMaxPathSum = findMaxSum(tree.left)
-      let rightSumBranch, rightMaxPathSum = findMaxSum(tree.right)
+      let [leftSumBranch, leftMaxPathSum] = findMaxSum(tree.left)
+      let [rightSumBranch, rightMaxPathSum] = findMaxSum(tree.right)
       let maxChildSumAsBranch = Math.max(leftSumBranch, rightSumBranch)
-      let value = tree.value;
+      
+      let {value} = tree;
       let maxSumAsBranch = Math.max(maxChildSumAsBranch + value, value);
       let maxSumAsTriangle = Math.max(leftSumBranch + value + rightSumBranch, maxSumAsBranch)
-      let maxPathSum = Math.max(leftMaxPathSum,rightMaxPathSum, maxSumAsTriangle); //could be a triangle, or the root branch
+      let maxPathSum = Math.max(leftMaxPathSum,rightMaxPathSum, maxSumAsTriangle); 
+      //could be a triangle, or the root branch
       return [maxSumAsBranch, maxPathSum];
   }
