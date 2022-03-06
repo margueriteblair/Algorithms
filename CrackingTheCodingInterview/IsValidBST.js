7ea5d18d7296912844ea4539524b4f166e2c8ec7
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-06 14:45:12 -0500

    validate bst

--- CrackingTheCodingInterview/IsValidBST.js
+++ CrackingTheCodingInterview/IsValidBST.js
@@ -1,13 +1,14 @@
-var isValidBST = function(root) {
-    return isValid(root, -Infinity, Infinity);
-};
-
-const isValid = function(root, min, max) {
-    if (!root) return true;
-    
-    if (root.val <= min || root.val >= max){
-        return false;
-    }
-    
-    return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
-}
+function validateBst(tree) {
+    // Write your code here.
+      return isValid(tree, -Infinity, Infinity);
+  }
+  
+  const isValid = function(root, min, max) {
+      if (!root) return true;
+      
+      if (root.value < min || root.value >= max){
+          return false;
+      }
+      
+      return (isValid(root.left, min, root.value) && isValid(root.right, root.value, max));
+  }
