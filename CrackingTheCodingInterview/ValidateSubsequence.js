1c908e0fd819f62ca7b16bd3fa65db367c910f52
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-06 14:18:55 -0500

    valid subsequence

--- CrackingTheCodingInterview/ValidateSubsequence.js
+++ CrackingTheCodingInterview/ValidateSubsequence.js
@@ -13,3 +13,13 @@ function isValidSubsequence(array, sequence) {
       
       return j === sequence.length ? true : false;
   }
+ 
+  function isValidSubsequence2(array, sequence) {
+    // Write your code here.
+      let seqIndx = 0;
+      for (const val of array) {
+          if (seqIndx === sequence.length) break;
+          if (sequence[seqIndx] === val) seqIndx++;
+      }
+      return seqIndx === sequence.length;
+  } 
