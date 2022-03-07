ba0a284f6f512e29b03a1c6a95065dd5ac93f273
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-07 10:45:48 -0500

    iterative in order traversal comments

--- CrackingTheCodingInterview/TechnicalQuestions/IterativeInOrderTraversal.java
+++ CrackingTheCodingInterview/TechnicalQuestions/IterativeInOrderTraversal.java
@@ -9,17 +9,17 @@ public class IterativeInOrderTraversal {
 
     //push non null node values to the stack, pop when the value is null
     public static void iterativeInOrder(Node root) {
-        if (root == null) return;
-        Stack<Node> stack = new Stack<>();
-        while (true) {
-            if (root != null) {
+        if (root == null) return; //if the root is null theres nothing to print
+        Stack<Node> stack = new Stack<>(); //initialize a stack that takes in nodes, so we can refer to the pointers
+        while (true) { //while true
+            if (root != null) { //if the root is not null, we push the root on to the stack, and then set the root = to left node
                 stack.push(root);
                 root = root.left;
-            } else {
+            } else { //otherwise, if the stack is empty, break out
                 if (stack.isEmpty()) break; //exit the loop if the stack is empty
-                root = stack.pop();
-                System.out.println(root.value);
-                root = root.right;
+                root = stack.pop(); //pop the root value off of the stack, because this is our node (remember, L->N->R)
+                System.out.println(root.value); //print out the root val
+                root = root.right; //then set it equal to right
             }
         }
     }
