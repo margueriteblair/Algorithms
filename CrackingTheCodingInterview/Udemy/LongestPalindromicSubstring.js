6aad87943cf3ba6cf93625dc5de408f6f40d868d
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-07 16:54:32 -0500

    rotate array

--- CrackingTheCodingInterview/Udemy/LongestPalindromicSubstring.js
+++ CrackingTheCodingInterview/Udemy/LongestPalindromicSubstring.js
@@ -51,3 +51,28 @@ function getLongestPalindromeFrom(string, left, right) {
     //the right is too far, but the substring isn't inclusive of the right index
     return [left+1, right];
 }
+/*
+Joseph's python code
+def longestPalindrome(string):
+    if len(string) == 0:
+        return string
+    longest = (0, 1)
+    for i in range(len(string)):
+        odd_len = getLongestPalindrome(string, i, i)
+        even_len = getLongestPalindrome(string, i, i + 1)
+        longest = max(odd_len, even_len, longest, key=lambda x: x[1] - x[0])
+    return string[longest[0]: longest[1]]
+
+             
+def getLongestPalindrome(string, left, right):
+    """
+    return (left_idx, right_idx(exclusively))
+    """
+    while left >= 0 and right < len(string):
+        if string[left] != string[right]:
+            break
+        else:
+            left -= 1
+            right += 1
+    return (left+1, right)
+*/
