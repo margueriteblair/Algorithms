0f52bfeee10e06493fab156a359a01f3da06b989
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-08 16:37:07 -0500

    notes on space and time complexity

--- CrackingTheCodingInterview/Udemy/NumberOfIslands.js
+++ CrackingTheCodingInterview/Udemy/NumberOfIslands.js
@@ -1,6 +1,15 @@
 /**
  * @param {character[][]} grid
  * @return {number}
+ * DFS and BFS is a bit faster for space complexity
+ * BFS isn't always space efficient
+ * Time complexity: nested for loop for sequential search takes O(M x N) time
+ *                  Within our sequential search, we'll be doing a BFS which is also O(N)
+ *
+ * Space complexity:  Space complexity will be determined by the size of the queue
+ *                     The BFS will only happen when we encounter a 1.
+ *                     Every time you remove a value, you can only add one more value
+ *                     Space: O(max(n, m)) either the row or column value is greater
  */
 var numIslands = function(grid) {
     if (!grid.length) return 0;
