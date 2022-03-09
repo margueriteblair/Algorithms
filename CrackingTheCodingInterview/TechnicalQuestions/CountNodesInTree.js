ce86b4e41b2f29bc0fff7527196e884913d3a4b5
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-08 22:36:16 -0500

    count nodes in a binary tree

---
+++ CrackingTheCodingInterview/TechnicalQuestions/CountNodesInTree.js
@@ -0,0 +1,12 @@
+var countNodes = function(root) {
+    if (!root) return 0;
+    let count = 0;
+    let q = [root];
+    while (q.length) {
+        let curr = q.shift();
+        count++;
+        if (curr.left) q.push(curr.left);
+        if (curr.right) q.push(curr.right);
+    }
+    return count;
+};
