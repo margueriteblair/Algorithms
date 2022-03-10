1af58a0df3f3ae023a4d2b9ffa89010d7013f3fe
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-10 09:25:31 -0500

    array of products

--- CrackingTheCodingInterview/TechnicalQuestions/ArrayOfProducts.js
+++ CrackingTheCodingInterview/TechnicalQuestions/ArrayOfProducts.js
@@ -11,4 +11,28 @@ function arrayOfProducts(array) {
           output[i] = prod;
       }
       return output;
+}
+
+function arrayOfProducts2(array) {
+    // Write your code here.
+      let output = [];
+      //two loops, one left & one right, then a third loop to calculate output
+      let left = new Array(array.length).fill(0) //where the 1 is at the beginning
+      let right = new Array(array.length).fill(0) //where the 1 is at the end
+      left[0] = 1;
+      right[array.length-1] = 1;
+      
+      for (let i = 1; i < array.length; i++) {
+          left[i] = left[i-1]*array[i-1];
+      }
+      console.log(left);
+      
+      for (let i = array.length-2; i >= 0; i--) {
+          right[i] = right[i+1]*array[i+1];
+      }
+      console.log(right);
+      for (let i = 0; i < left.length; i++) {
+          output[i] = left[i]*right[i]
+      }
+      return output;
   }
