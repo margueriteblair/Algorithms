79ab63dbbd17b78193e8b7271dab3d2629c21978
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-09 20:32:05 -0500

    tornament winner

---
+++ CrackingTheCodingInterview/TechnicalQuestions/TournamentWinner.js
@@ -0,0 +1,22 @@
+function tournamentWinner(competitions, results) {
+    // Write your code here.
+      let scores = new Map();
+      let winner = "";
+      let max = 0;
+      for (let i = 0; i < results.length; i++) {
+          if (results[i] == 1) {
+              scores.set(competitions[i][0], scores.get(competitions[i][0])+3 || 3);
+  
+          } else {
+              scores.set(competitions[i][1], scores.get(competitions[i][1])+3 || 3);
+  
+          }
+      }
+      for (const [key, value] of scores) {
+          if (value > max) {
+              max = value;
+              winner = key;
+          }
+      }
+    return winner;
+  }
