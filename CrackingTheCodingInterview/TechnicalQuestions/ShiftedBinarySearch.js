6d7de622f0a622ee5ab9f8f024c92ea971334ae7
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-12 16:19:20 -0500

    another iteration of shifted binary search

--- CrackingTheCodingInterview/TechnicalQuestions/ShiftedBinarySearch.js
+++ CrackingTheCodingInterview/TechnicalQuestions/ShiftedBinarySearch.js
@@ -26,3 +26,32 @@ function shiftedBinarySearch(array, target) {
       }
       return -1;
   }
+
+  var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length-1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left+right)/2);
+        console.log(left, right);
+        if (nums[mid] === target) return mid;
+        //***WE CAN EITHER HAVE THE LEFT AND RIGHT INDEX CHECKS ORRRR WE CAN SAY NUMS[LEFT] <= NUMS[MID] */
+        // if (nums[left] === target) return left;
+        // if (nums[right] === target) return right;
+        if (nums[left] <= nums[mid]) {
+            //this part is sorted
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid-1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && nums[right] >= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
