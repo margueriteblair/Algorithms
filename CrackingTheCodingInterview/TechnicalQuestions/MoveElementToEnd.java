d8ab6398a625f31122d0e2bff2f5af372e83bb1b
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-12 11:21:18 -0500

    element to move in java

---
+++ CrackingTheCodingInterview/TechnicalQuestions/MoveElementToEnd.java
@@ -0,0 +1,21 @@
+package CrackingTheCodingInterview.TechnicalQuestions;
+
+public class MoveElementToEnd {
+    public static void main(String[] args) {
+        
+    }
+
+    public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+        // Write your code here.
+            int i = 0;
+            int j = array.size()-1;
+            while (i < j) {
+                while (array.get(j) == toMove && i < j) j--;
+                while (array.get(i) != toMove && i < j) i++;
+                int tmp = array.get(j);
+                array.set(j, array.get(i));
+                array.set(i, tmp);
+            }
+        return array;
+      }
+}
