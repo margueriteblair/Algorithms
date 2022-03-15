ad1e87316eb02f2da9e8550ebd0daceec37e2b4d
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-15 14:05:00 -0400

    more manual transposition of a matrix

--- CrackingTheCodingInterview/TransposeMatrix.js
+++ CrackingTheCodingInterview/TransposeMatrix.js
@@ -6,5 +6,17 @@ var transpose = function(matrix) {
     return Object.keys(matrix[0]).map(function(c) {
         return matrix.map(function(r) { return r[c]; });
     });
-    return matrix;
+};
+
+var transpose = function(matrix) {
+    let res = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        res.push([]);
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            res[j].push(matrix[i][j]);
+        }
+    }
+    return res;
 };
