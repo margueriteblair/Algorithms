21d7342c2505fda65ce3c70645c559739ea49060
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-15 12:28:02 -0400

    minimum window size for a given target subarr

--- CrackingTheCodingInterview/MaxSumSubarray.java
+++ CrackingTheCodingInterview/MaxSumSubarray.java
@@ -1,6 +1,7 @@
 public class MaxSumSubarray {
     public static void main(String[] args) {
         System.out.println(findMaxSumSubarray(new int[] {4,2,1,7,8,1,2,8,1,0}, 3));
+        System.out.println(smallestSubArray(new int[] {4,2,2,7,8,1,2,8,10}, 8));
         
     }
 
@@ -15,5 +16,21 @@ public class MaxSumSubarray {
             }
         }
         return maxVal;
+    }
+
+    public static int smallestSubArray(int[] arr, int targetSum) {
+        int j = 0; //this is the start of our window, i is the end of our window
+        int currentSum = 0;
+        int windowSize = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
+            while (currentSum >= targetSum) {
+                windowSize = Math.min(windowSize, i - j + 1);
+                currentSum -= arr[j];
+                j++;
+
+            }
+        }
+        return windowSize;
     }
 }
