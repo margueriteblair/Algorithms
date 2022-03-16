65e223c94a3f19203891a7650a829d56d05b0909
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-16 11:05:26 -0400

    working solution to grumpy bookstore

--- CrackingTheCodingInterview/MaxSumSubarray.java
+++ CrackingTheCodingInterview/MaxSumSubarray.java
@@ -34,3 +34,33 @@ public class MaxSumSubarray {
         return windowSize;
     }
 }
+
+var maxSatisfied2 = function(customers, grumpy, X) {
+    let totalSatisfaction = 0;
+    for (let i = 0; i < customers.length; i++) if (!grumpy[i]) totalSatisfaction += customers[i];
+    
+    
+    // Handle some easy edge cases
+    if (X === 0) return totalSatisfaction;
+    if (customers.length < X) return customers.reduce((acc, current) => acc + current, 0);
+    
+    
+    // Use a sliding window of size X including customers that
+    // would have been previously exluded when they were grumpy
+    
+    let slidingWindowSum = totalSatisfaction;
+    let maxSum = totalSatisfaction;
+    let currentEndPosition = -1;
+    
+    while (currentEndPosition < customers.length - 1) {
+        currentEndPosition += 1;
+        const previousStartPosition = currentEndPosition - X;
+        
+        if (previousStartPosition >= 0 && grumpy[previousStartPosition]) slidingWindowSum -= customers[previousStartPosition];
+        if (grumpy[currentEndPosition]) slidingWindowSum += customers[currentEndPosition];
+        
+        maxSum = Math.max(slidingWindowSum, maxSum);
+    }
+    
+    return maxSum;
+};
