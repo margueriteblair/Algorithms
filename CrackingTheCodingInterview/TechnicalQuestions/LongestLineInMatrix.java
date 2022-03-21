4d0a2e57a776e7d19c57303f20ad765f44c41b8d
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-21 14:22:19 -0400

    longest line 2 using dynamic programming

--- CrackingTheCodingInterview/TechnicalQuestions/LongestLineInMatrix.java
+++ CrackingTheCodingInterview/TechnicalQuestions/LongestLineInMatrix.java
@@ -36,4 +36,24 @@ public class LongestLineInMatrix {
         }
         return longest;
     }
+
+    public int longestLine2(int[][] mat) {
+        if (mat.length == 0) return 0;
+        int ones = 0;
+        int[][][] dp = new int[mat.length][mat[0].length][4];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j= 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 1) {
+                    dp[i][j][0] = j > 0 ? dp[i][j-1][0] + 1 : 1;
+                    dp[i][j][1] = i > 0 ? dp[i-1][j][1] + 1 : 1;
+                    dp[i][j][2] = (i > 0 && j > 0) ? dp[i-1][j-1][2] + 1 : 1;
+                    dp[i][j][3] = (i > 0 && j < mat[0].length-1) ? dp[i-1][j+1][3] + 1 : 1;
+                    
+                    ones =
+              Math.max(ones,Math.max(Math.max(dp[i][j][0], dp[i][j][1]), Math.max(dp[i][j][2], dp[i][j][3])));
+                }
+            }
+        }
+        return ones;
+    }
 }
