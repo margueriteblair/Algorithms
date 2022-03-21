e38ccd16b490bb0393f48aef84996775b35c30fe
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-20 20:54:04 -0400

    summing up all node depths

---
+++ CrackingTheCodingInterview/Udemy/SumNodeDepths.js
@@ -0,0 +1,21 @@
+function nodeDepths(root) {
+    // Write your code here.
+      if (!root) return 0;
+      let q = [root];
+      let level = 0;
+      let sum = 0;
+      console.log(root)
+      while (q.length) {
+          let count = 0;
+          let length = q.length;
+          while (count < length) {
+              let curr = q.shift(); //pop in java's native queue
+              if (curr.left) q.push(curr.left)
+              if (curr.right) q.push(curr.right);
+              sum += level;
+              count++
+          }
+          level++;
+      }
+      return sum;
+  }
