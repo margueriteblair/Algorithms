95bb4c1f3c111dc2aef7ce1b55e4e8cba8bb5bda
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-23 10:09:21 -0400

    cleaned up logic

--- CrackingTheCodingInterview/TechnicalQuestions/SubarraySumK.js
+++ CrackingTheCodingInterview/TechnicalQuestions/SubarraySumK.js
@@ -2,18 +2,16 @@ var subarraySum = function(nums, k) {
     let totalCount = 0;
     let map = new Map();
     map.set(0, 1);
-    //[1,2,3] k = 3
+    //[1,1,1] k = 2
     //   i 
-    //map = {-2: 1, 0: 1}
+    //map = {0: 2, -1: 1, 1:1}
     //runningSum = 3;
+    //totalCount = 1
     let runningSum = 0;
     for (let i = 0; i < nums.length; i++) {
         runningSum += nums[i]
-        if (map.has(runningSum - k)) {
-            totalCount += map.get(runningSum - k);
-        } else {
-            map.set(runningSum - k, map.get(runningSum - k) + 1 || 1);
-        }
+        if (map.has(runningSum - k)) totalCount += map.get(runningSum - k);
+        map.set(runningSum, map.get(runningSum) + 1 || 1);
     }
     return totalCount;
 };
