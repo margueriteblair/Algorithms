9a7bc1c4f482bfe49c40ea6b5ae051767c3385ac
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-24 17:02:30 -0400

    merge k sorted lists

--- CrackingTheCodingInterview/TechnicalQuestions/MergeKSortedLinkedLists.js
+++ CrackingTheCodingInterview/TechnicalQuestions/MergeKSortedLinkedLists.js
@@ -20,3 +20,39 @@ var mergeKLists = function(lists) {
     }
     return head;
 };
+
+var mergeKLists2 = function (lists) {
+    //helper function to merge 2 lists together
+    var merge2Lists = (l1, l2) => {
+      let dummy = new ListNode();
+      let tail = dummy;
+  
+      while (l1!=null && l2!=null) {
+        if (l1.val < l2.val) {
+          tail.next = l1;
+          l1 = l1.next;
+        } else {
+          tail.next = l2;
+          l2 = l2.next;
+        }
+        tail = tail.next;
+      }
+      if (l1 != null) tail.next = l1;
+      if (l2 != null) tail.next = l2;
+      return dummy.next;
+    };
+  
+    //edge case
+    if (lists.length == 0) return null;
+  
+  //we say > 1 because we want to merge lists 2 by 2
+    while (lists.length > 1) {
+      let mergedLists = []
+      //take the first 2 lists of the input lists then merge them using your helper function, store the result in the array you just created then push it in lists
+      let l1 = lists.shift()
+      let l2 = lists.shift()
+      mergedLists = merge2Lists(l1,l2)
+      lists.push(mergedLists)
+    }
+    return lists[0];
+  };
