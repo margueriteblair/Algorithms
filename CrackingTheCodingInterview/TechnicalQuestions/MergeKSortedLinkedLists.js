0f1afd940e5836f8f2c23b75dfa97cd99b49d82c
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-24 12:14:08 -0400

    merge k sorted linked lists

--- CrackingTheCodingInterview/TechnicalQuestions/MergeKSortedLinkedLists.js
+++ CrackingTheCodingInterview/TechnicalQuestions/MergeKSortedLinkedLists.js
@@ -1,14 +1,17 @@
 var mergeKLists = function(lists) {
+    if (lists.length == 0) return null;
     let allNodes = [];
     for (let i = 0; i < lists.length; i++) {
         let curr = lists[i];
+        if (lists[i] == null) continue;
         while (curr) {
             allNodes.push(curr.val);
             curr = curr.next;
         }
     }
     
-    allNodes.sort()
+    allNodes.sort(function(a, b) {return a - b});
+    if (allNodes.length == 0) return null;
     let head = new ListNode(allNodes[0]);
     let curr = head;
     for (let i = 1; i < allNodes.length; i++) {
