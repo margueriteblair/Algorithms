fa59af38181562bf0ce7daaf518cc5f00d80e312
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-24 20:15:33 -0400

    finding permutations of a string

--- CrackingTheCodingInterview/TechnicalQuestions/FindPerms.js
+++ CrackingTheCodingInterview/TechnicalQuestions/FindPerms.js
@@ -2,31 +2,28 @@ const findPerms = function(str) {
     const res = [];
 
     const _swap = function(str, i, j) {
-        let tmp;
-        let charArr = str.split("");
-        tmp = charArr[i];
-        charArr[i] = charArr[j];
-        charArr[j] = tmp;
-        return charArr.join("");
+
+        let tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+        return str.join("");
     }
-    
-    const _backtrack = function(curr, idx) {
-      if (curr.length == idx) {
-        res.push(curr);
+
+    const _backtrack = function(alphabets, idx1, idx2) {
+      if (idx1 === idx2) {
+        res.push(alphabets.join(""));
         return;
       }
 
-      for (let i = 0; i < str.length; i++) {
-          curr = swap(curr, idx, i);
-          _backtrack(curr,idx+1)
-          curr = swap(curr, i, idx);
+      for (let i = idx1; i <= idx2; i++) {
+          _swap(alphabets, idx1, i);
+          _backtrack(alphabets,idx1+1, idx2);
+          _swap(alphabets, i, idx1);
       }
     }
 
-
-  
-    _backtrack("", 0);
+    _backtrack(str, 0, str.length-1)
     return res;
   }
   
-  console.log(findPerms("abc"));
+  console.log(findPerms("abc".split("")));
