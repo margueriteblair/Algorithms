30003e092dd18c06578d55c1b10379bc195c47c1
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-26 17:19:04 -0400

    LRU cache

---
+++ CrackingTheCodingInterview/TechnicalQuestions/LRUCache.java
@@ -0,0 +1,29 @@
+class LRUCache {
+    private int capacity;
+    private Map<Integer, Integer> cache;
+    private int lru = Integer.MAX_VALUE;
+
+    public LRUCache(int capacity) {
+        //positive size capacity, do we need to check?
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+    }
+    
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        } else {
+            return -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        if (value < lru) {
+            lru = key;
+        }
+        this.cache.put(key, value);
+        if (this.cache.size() > capacity) {
+            this.cache.remove(key);
+        }
+    }
+}
