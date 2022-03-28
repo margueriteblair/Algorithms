eae1939ce69e27863e463b9c9e6176bf5d47865d
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-28 11:41:42 -0400

    using a hashmap to store fewer 0 values

--- CrackingTheCodingInterview/TechnicalQuestions/SparseVector.js
+++ CrackingTheCodingInterview/TechnicalQuestions/SparseVector.js
@@ -16,3 +16,32 @@ SparseVector.prototype.dotProduct = function(vec) {
     return dotProduct;
     //computer the dot prod between our instance's nums, and another passed in SparseVector vec
 };
+
+
+var SparseVector = function(nums) {
+    this.nums = nums; //nums is a vector
+    this.mapping = new Map();
+    
+    for (let i= 0; i < this.nums.length; i++) {
+        if (nums[i] !== 0) {
+            this.mapping.set(i, nums[i]);
+        }
+    }
+};
+
+// Return the dotProduct of two sparse vectors
+/**
+ * @param {SparseVector} vec
+ * @return {number}
+ */
+SparseVector.prototype.dotProduct = function(vec) {
+    //vec is another vector, 
+    let dotProduct = 0;
+    for (let i = 0; i < vec.nums.length; i++) {
+        if (this.mapping.has(i)) {
+            dotProduct += (this.mapping.get(i) * vec.nums[i]);
+        }
+    }
+    return dotProduct;
+    //computer the dot prod between our instance's nums, and another passed in SparseVector vec
+};
