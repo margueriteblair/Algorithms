53c381c0383686047f6852e49adb044809d7fff0
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-29 10:45:50 -0400

    dynamic programming

--- CrackingTheCodingInterview/TechnicalQuestions/CanJump.js
+++ CrackingTheCodingInterview/TechnicalQuestions/CanJump.js
@@ -24,3 +24,25 @@ var canJump2 = function(nums) {
     
     return _backtrack(0, nums);
 };
+
+var canJump3 = function(nums) {
+    let memo = new Array(nums.length).fill("UNKNOWN");
+    var _backtrack = function(pos, nums) {
+        if (memo[pos] !== "UNKNOWN") {
+            return memo[pos] === "GOOD" ? true : false;
+        }
+        
+        let furthestJump = Math.min(pos + nums[pos], nums.length-1);
+        for (let nextPosition = pos+1; nextPosition <= furthestJump; nextPosition++) {
+            if (_backtrack(nextPosition, nums)) {
+                memo[nextPosition] = "GOOD";
+                return true;
+            }
+        }
+        memo[pos] = "BAD";
+        return false;
+    }
+    
+    memo[memo.length-1] = "GOOD"; //we have to fill this facebook
+    return _backtrack(0, nums);
+};
