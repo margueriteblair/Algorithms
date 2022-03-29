f79c2406c1eb530a802f25539ad9044dee78bf34
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-29 10:13:28 -0400

    backtracking solution

--- CrackingTheCodingInterview/TechnicalQuestions/CanJump.js
+++ CrackingTheCodingInterview/TechnicalQuestions/CanJump.js
@@ -8,3 +8,19 @@ var canJump = function(nums) {
     
     return lastGoodIndex == 0;
 };
+
+var canJump2 = function(nums) {
+    var _backtrack = function(pos, nums) {
+        if (pos === nums.length-1) return true;
+        
+        let furthestJump = Math.min(pos + nums[pos], nums.length-1);
+        for (let nextPosition = pos+1; nextPosition <= furthestJump; nextPosition++) {
+            if (_backtrack(nextPosition, nums)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    return _backtrack(0, nums);
+};
