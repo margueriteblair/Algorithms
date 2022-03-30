35239161523871a1487217cdfd8d275b46f0c993
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-30 14:08:05 -0400

    merge two sorted

--- CrackingTheCodingInterview/TechnicalQuestions/MergeTwoSorted.js
+++ CrackingTheCodingInterview/TechnicalQuestions/MergeTwoSorted.js
@@ -5,3 +5,38 @@ var merge = function(nums1, m, nums2, n) {
     
     return nums1.sort((a, b) =>{return a-b})
 };
+
+var merge = function(nums1, m, nums2, n) {
+    let nums1Copy = [];
+    for (let i = 0; i < m; i++) {
+        nums1Copy.push(nums1[i])
+    }
+    
+    let p1 = 0;
+    let p2 = 0;
+    let p = 0;
+    
+    while (p1 < m && p2 < n) {
+        if (nums1Copy[p1] < nums2[p2]) {
+            nums1[p] = nums1Copy[p1];
+            p1++;
+        } else {
+            nums1[p] = nums2[p2];
+            p2++;
+        }
+        p++;
+    }
+    
+    while (p1 < m) {
+        nums1[p] = nums1Copy[p1];
+        p1++;
+        p++;
+    }
+    
+    while (p2 < n) {
+        nums1[p] = nums2[p2];
+        p++;
+        p2++;
+    }
+    return nums1;
+};
