da6c8f9292130a56bbf2c778578e0c553db2bd2d
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-30 13:46:17 -0400

    merge twosorted

--- CrackingTheCodingInterview/TechnicalQuestions/MergeTwoSorted.js
+++ CrackingTheCodingInterview/TechnicalQuestions/MergeTwoSorted.js
@@ -1,5 +1,5 @@
 var merge = function(nums1, m, nums2, n) {
-    for (let i = nums1.length-1; i >= nums1.length-n; i--) {
+    for (let i = m+n-1; i >= m; i--) {
         nums1[i] = nums2.pop();
     }
     
