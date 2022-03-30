7d090665721736d7e83051c5569ec62ea3bd8a84
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-30 13:16:12 -0400

    omitting the second base case

--- CrackingTheCodingInterview/TechnicalQuestions/FindMinInSorted.js
+++ CrackingTheCodingInterview/TechnicalQuestions/FindMinInSorted.js
@@ -2,9 +2,6 @@ var findMin = function(nums) {
     if (nums.length === 1) {
         return nums[0];
     }
-    if (nums.length === 2) {
-        return Math.min(nums[0], nums[1])
-   }
     return binarySearch(nums, 0, nums.length-1);
 };
 
