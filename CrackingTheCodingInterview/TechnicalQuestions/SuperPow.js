422cde58ff2a9cc859fe7cfdd209ecb97a7f78c0
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-30 22:52:13 -0400

    getting closer to recursive solution

--- CrackingTheCodingInterview/TechnicalQuestions/SuperPow.js
+++ CrackingTheCodingInterview/TechnicalQuestions/SuperPow.js
@@ -1,24 +1,21 @@
 var superPow = function(a, b) {
-    let realB = 0;
-    for (let i = 0; i < b.length; i++) {
-        realB += b[i] * (Math.pow(10, i));
-    }
-    
+    let realB = Number(b.join(""));
+
     let expo;
     if (realB % 2 === 0) {
         expo = boilDown(realB, a);
     } else{
         expo = boilDown(realB-1, a) * (a % 1337);
     }
-    
+    //console.log(expo)
     return expo;
     
 };
 
 var boilDown = function(realB, a) {
-    if (realB === 1) {
+    if (realB == 1) {
         return a % 1337;
     }
     
-    return a % 1337 * boilDown(realB / 2, a);
+    return boilDown(realB/2, a) * boilDown(realB / 2, a);
 }
