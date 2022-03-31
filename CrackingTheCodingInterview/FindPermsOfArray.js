42f23f6986387092d571378271e1e784b939c238
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-31 11:17:27 -0400

    find perms

--- CrackingTheCodingInterview/FindPermsOfArray.js
+++ CrackingTheCodingInterview/FindPermsOfArray.js
@@ -15,3 +15,25 @@ var permute = function(nums) {
     backtrack([]);
     return res;
 };
+
+
+var permute2 = function(nums) {
+    let res = [];
+    let tmp = [];
+    
+    const backtrack = (nums, tmp, res) => {
+        if (tmp.length === nums.length) {
+            res.push([...tmp]);
+        }
+        
+        for (let i = 0; i < nums.length; i++) {
+            if (tmp.includes(nums[i])) continue;
+            tmp.push(nums[i]);
+            backtrack(nums, tmp, res);
+            tmp.pop();
+        }
+    }
+    
+    backtrack(nums,[], res);
+    return res;
+};
