cb56f6e15321feb847743a2e0fb4a3fe10e32980
Marguerite Bliar <marguerite.blair@gmail.com>
2022-03-30 20:13:11 -0400

    second solution

--- CrackingTheCodingInterview/TechnicalQuestions/RPNEval.js
+++ CrackingTheCodingInterview/TechnicalQuestions/RPNEval.js
@@ -24,3 +24,38 @@ var evalRPN = function(tokens) {
     }
     return stack.pop();
 };
+
+
+const OPERATORS = ["+", "-", "/", "*"];
+
+var evalRPN2 = function(tokens) {
+    const stack = [];
+
+    for (const token of tokens) {
+        if (!OPERATORS.includes(token)) {
+            stack.push(Number(token));
+            continue;
+        }
+
+        const number2 = stack.pop();
+        const number1 = stack.pop();
+
+        switch (token) {
+            case "+": {
+                stack.push(number1 + number2);
+                break;
+            } case "-": {
+                stack.push(number1 - number2);
+                break;
+            } case "/": {
+                stack.push(Math.trunc(number1 / number2));
+                break;
+            } case "*": {
+                stack.push(number1 * number2);
+                break;
+            }
+        }
+    }
+
+    return stack.pop();
+};
