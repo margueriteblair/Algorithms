69be8a8472482eea98065cdbe3040e8a19bb6917
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-01 11:12:13 -0400

    pow xn

--- CrackingTheCodingInterview/PowXN.java
+++ CrackingTheCodingInterview/PowXN.java
@@ -7,8 +7,15 @@ public class PowXN {
 
     public double myPow(double x, int n) {
         double expo = 1;
-        for (int i = 0; i < n; i++) {
-            expo *= x;
+        for (int i = 0; i < Math.abs(n); i++) {
+            if (n > 0) {
+               expo *= x; 
+            } else if (n < 0) {
+                expo = expo / x;
+            } else {
+                return 1;
+            }
+            
         }
         
         return expo;
