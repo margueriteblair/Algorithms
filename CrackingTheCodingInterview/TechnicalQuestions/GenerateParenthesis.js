f037f12f755d5893e3cd85b49152f2f3ffb3acf6
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-03 15:27:00 -0400

    generate parentheses

---
+++ CrackingTheCodingInterview/TechnicalQuestions/GenerateParenthesis.js
@@ -0,0 +1,24 @@
+var generateParenthesis = function(n) {
+    let res = [];
+    var _backtrack = function(curr, res, open, close, max) {
+        if (curr.length === max * 2) {
+            res.push(curr);
+            return;
+        }
+        
+        if (open < max) {
+            curr += "(";
+            _backtrack(curr, res, open+1, close, max);
+            curr = curr.substring(0, curr.length-1);
+        }
+        
+        if (close < open) {
+            curr += ")";
+            _backtrack(curr, res, open, close+1, max);
+            curr = curr.substring(0, curr.length-1);
+        }
+    }
+    
+    _backtrack("", res, 0, 0, n);
+    return res;
+};
