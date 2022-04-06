030acc9d7fee28db7d0e6ab374cfb0af58de0632
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-06 10:50:34 -0400

    closer

--- CrackingTheCodingInterview/ConvertStringKMoves.js
+++ CrackingTheCodingInterview/ConvertStringKMoves.js
@@ -1,17 +1,16 @@
 var canConvertString = function(s, t, k) {
-    let kArray = new Array(k + 1).fill(false);
+    let charMap = new Array(26).fill(0);
     if (s.length !== t.length) return false;
     for (let i=0; i < s.length; i++) {
         if (s[i] === t[i]) continue;
         let charDifference = Math.abs(s[i].charCodeAt(0) - t[i].charCodeAt(0));
-        if (charDifference > k) {
+       // let charMapIdx = s[i].charCodeAt(0) - 'a'.charCodeAt(0)
+        if (((charMap[charDifference] * 26) + charDifference) > k) {
             return false;
         } 
-        if (kArray[charDifference]) {
-            return false;
-        }
-
-        kArray[charDifference] = true;
+        console.log(charMap)
+        charMap[charDifference]++;
+        
     }
     
     return true;
