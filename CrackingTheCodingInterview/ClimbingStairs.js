92900a6fc602987e68ce4f1d0fcb87c205a5912a
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-07 16:10:26 -0400

    memoized solution for climbing stairs

--- CrackingTheCodingInterview/ClimbingStairs.js
+++ CrackingTheCodingInterview/ClimbingStairs.js
@@ -9,3 +9,17 @@ var minCost = function(i, cost) {
     
     return cost[i] + Math.min(minCost(i-1, cost), minCost(i-2, cost));
 }
+
+var minCostClimbingStairs = function(cost) {
+    const n = cost.length;
+    let memo = new Map();
+    memo.set(0, cost[0]);
+    memo.set(1, cost[1]);
+    memo.set(-1, 0); //-1 will cover all negative numbers
+    for (let i = 2; i <= n; i++) {
+        //console.log(memo);
+        memo.set(i, cost[i] + Math.min(memo.get(i-1), memo.get(i-2)));
+    }
+    
+    return Math.min(memo.get(n-1), memo.get(n-2));
+};
