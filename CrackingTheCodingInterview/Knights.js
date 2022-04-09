cf9a44349a050c1abf54e36da6fa2580131c893f
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-09 16:39:29 -0400

    buttom up

--- CrackingTheCodingInterview/Knights.js
+++ CrackingTheCodingInterview/Knights.js
@@ -44,3 +44,30 @@ var recurse = function(n, k, r, c, dp){
     dp[k][r][c] =res;
     return dp[k][r][c]
 }
+
+var knightProbability = function(n, k, r, c) {
+    let dp = new Array(k+1).fill(0).map(() => new Array(n).fill(0).map(()=>new Array(n).fill(0)));
+    console.log(dp);
+    dp[0][r][c] = 1;
+    for (let step = 1; step <= k; step++) {
+        for (let row = 0; row < n; row++) {
+            for (let col = 0; col < n; col++) {
+                for (let i = 0; i < directions.length; i++) {
+                    const dir = directions[i];
+                    const prevRow = row + dir[0];
+                    const prevCol = col + dir[1];
+                    if (prevRow >= 0 && prevRow < n && prevCol >= 0 && prevCol < n) {
+                        dp[step][row][col] += dp[step-1][prevRow][prevCol] /8
+                    }
+                }
+            }
+        }
+    }
+    let res = 0; 
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            res += dp[k][i][j];
+        }
+    }
+    return res;
+};
