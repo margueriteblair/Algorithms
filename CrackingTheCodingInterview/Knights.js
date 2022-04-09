2c466520e9adf0f993da8b21f978109996cc485c
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-09 14:07:31 -0400

    resucir

--- CrackingTheCodingInterview/Knights.js
+++ CrackingTheCodingInterview/Knights.js
@@ -1,43 +1,4 @@
 let directions = [[-2, -1], [-2, 1], [2, 1], [2, -1], [-1, 2], [-1, -2], [1, 2], [1, -2]];
-var knightProbability = function(n, k, row, column) {
-    if (k === 0) return 1.000;
-    let seen = new Array(n).fill(-1).map(() => new Array(n).fill(-1));
-    let q = [[row, column]];
-    let i = 0;
-    let totalP = 0;
-    while (i < k) {
-        let count = 0;
-        let len = q.length;
-        while (count < len) {
-            let curr = q.shift();
-            let currRow = curr[0]
-            let currCol = curr[1];
-            
-            let canMove = 0;
-            for (const dir of directions) {
-                let newRow = currRow + dir[0];
-                let newCol = currCol + dir[1];
-                
-                if (newRow >= 0 && newRow < n && newCol >=0 && newCol < n ) {
-                    canMove++;
-                    q.push([newRow, newCol]);
-                }
-            }
-            
-            seen[currRow][currCol] = canMove/8;
-            if (i == k-1) {
-            totalP += seen[currRow][currCol];
-            }
-            count++;
-        }
-        i++;
-
-    }
-    //console.log(seen)
-    return totalP/8;
-};
-
-let directions = [[-2, -1], [-2, 1], [2, 1], [2, -1], [-1, 2], [-1, -2], [1, 2], [1, -2]];
 var knightProbabilityRecursive = function(n, k, row, col) {
         if (row < 0 || row >= n || col < 0 || col >= n) {
             return 0;
@@ -54,3 +15,32 @@ var knightProbabilityRecursive = function(n, k, row, col) {
         }
         return res;
 };
+
+let directions = [[-2, -1], [-2, 1], [2, 1], [2, -1], [-1, 2], [-1, -2], [1, 2], [1, -2]];
+var knightProbability = function(n, k, row, col) {
+        let dp = new Array(k+1).fill(0).map(() => new Array(n).fill(0)
+                                            .map(()=>new Array(n).fill(undefined)));
+                                            
+        return recurse(n, k, row, col, dp);
+};
+
+var recurse = function(n, k, r, c, dp){
+    if (k == 0) {
+        return 1;
+    }
+    if (r < 0 || c < 0 || r >= n || c >= n) {
+        return 0;
+    }
+    
+    if (dp[k][r][c] !== undefined) {
+        return dp[k][r][c];
+    }
+    let res = 0;
+    for (const dir of directions) {
+        let x = dir[0]
+        let y = dir[1]
+        res += recurse(n, k-1, x+r, c +y, dp) / 8;
+    }
+    dp[k][r][c] =res;
+    return dp[k][r][c]
+}
