3e2e8f1ea654a2c05b24b86cdc17b83e53401664
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-13 09:31:24 -0400

    permutations 2

---
+++ CrackingTheCodingInterview/TechnicalQuestions/Permutations2.js
@@ -0,0 +1,30 @@
+var permuteUnique = function(nums) {
+    let res = [];
+    const map = new Map();
+    for (let num of nums) {
+        map.set(num, map.get(num)+1||1);
+    }
+    var _backtrack = function(curr) {
+        
+        if (curr.length === nums.length) {
+            console.log(curr)
+            res.push(curr);
+            console.log(res)
+            return;
+        }
+        
+        for (const [num, count] of map.entries()) {
+            if (count === 0) {
+                continue;
+            }
+            curr.push(num);
+            map.set(num, count-1);
+            _backtrack(curr, map);
+            curr.pop();
+            map.set(num, count);
+        }
+    }
+    
+    _backtrack([]);
+    return res;
+};
