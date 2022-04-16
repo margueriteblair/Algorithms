8b03402d7cd0fbfcd3c9787769a0ab403bc15f32
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-15 20:22:17 -0400

    update to make cleaner

--- CrackingTheCodingInterview/TechnicalQuestions/PathSumII.js
+++ CrackingTheCodingInterview/TechnicalQuestions/PathSumII.js
@@ -1,23 +1,17 @@
 var pathSum = function(root, targetSum) {
     let res = [];
     let pathNodes = [];
-    recurseTree(root, targetSum, pathNodes, res);
+    let sumCount = 0;
+    dfs(root, targetSum, pathNodes, res, sumCount);
     return res;
-    
 };
 
-var recurseTree = function(node, remainingSum, pathNodes, pathsList) {
+
+var dfs = function(node, remainingSum, currPath, pathsList) {
     if (!node) return;
-    
-    pathNodes.push(node.val);
-    if (remainingSum === node.val && node.left === null && !node.right) {
-        pathsList.push([...pathNodes]);
-    } else {
-        //otherwise, we recurse on the left and right children
-        recurseTree(node.left, remainingSum - node.val, pathNodes, pathsList);
-        recurseTree(node.right, remainingSum - node.val, pathNodes, pathsList);
-        
-        //then we need to pop the node once we are done processing all of its subtrees
-    }
-    pathNodes.pop(); 
+    currPath.push(node.val);
+    dfs(node.left, remainingSum - node.val, currPath, pathsList);
+    dfs(node.right, remainingSum - node.val, currPath, pathsList);
+    if (!node.left && !node.right && node.val === remainingSum) pathsList.push([...currPath]);
+    currPath.pop(); 
 }
