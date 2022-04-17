7c259b898971ebee99a1ca969affe90d1ec3f8a6
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-17 10:00:25 -0400

    validate stacks

---
+++ CrackingTheCodingInterview/TechnicalQuestions/ValidateStacks.js
@@ -0,0 +1,13 @@
+var validateStackSequences = function(pushed, popped) {
+    let stack = [];
+    let i = 0;
+    for (let num of pushed) {
+        stack.push(num)
+        while (stack.length && i < pushed.length && stack[stack.length-1] === popped[i]) {
+            stack.pop();
+            i++;
+        }
+    }
+    
+    return i == pushed.length;
+};
