35046339cee0eb8973268038fa4a5bb9a9a66929
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-17 11:27:42 -0400

    addition

--- Python/addition.py
+++ Python/addition.py
@@ -1,2 +1,14 @@
 def addition(a, b):
 	return a + b
+
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+    print(a // b)
+    print(a / b)
+
+def square_range(n):
+    for i in range(0, n):
+        print(i ** 2)
