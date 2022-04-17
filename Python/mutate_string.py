33653099223d7b9e8bb4ad03285769af9726cdc0
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-17 12:12:52 -0400

    second mutate string option

--- Python/mutate_string.py
+++ Python/mutate_string.py
@@ -3,3 +3,6 @@ def mutate_string(string, position, character):
     s[position] = character
     string = ''.join(s)
     return string
+
+def mutate_string2(string, position, character):
+    return string[:position] + character + string[position+1:]
