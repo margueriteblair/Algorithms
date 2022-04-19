8a589a1d40647095094008620b87fc693aa440c8
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-18 21:32:08 -0400

    monarchy implementation

---
+++ CrackingTheCodingInterview/Udemy/Monarchy.js
@@ -0,0 +1,15 @@
+class Monarchy {
+    //n-ary tree
+    //represent the children as an array
+    //children: [Jane, Farah]...
+    //DFS, preorder 
+    //instantiate an internal hashmap
+}
+
+class Person {
+    constructor(name) {
+        this.name = name;
+        this.isAlive = true; //we'll just add a boolean and switch it to False
+        this.children = [];
+    }
+}
