857e7e4c11edae486be799ef0ba4f4fb5631871b
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-18 21:51:27 -0400

    monarchy

--- CrackingTheCodingInterview/Udemy/Monarchy.js
+++ CrackingTheCodingInterview/Udemy/Monarchy.js
@@ -4,6 +4,25 @@ class Monarchy {
     //children: [Jane, Farah]...
     //DFS, preorder 
     //instantiate an internal hashmap
+    constructor(king) {
+        this.king = new Person(king);
+        this.personMap = new Map();
+        this.personMap.set(this.king.name, this.king);
+    }
+
+    birth = function(child, parent) { //both child and parent are of people classes
+        let childPerson = new Person(child);
+        this.personMap.set(parent, this.personMap.get(parent).push(childPerson))
+        this.personMap.set(child, this.child)
+    }
+
+    death = function(name) {
+
+    }
+
+    getOrderOfSuccession = function() {
+        //traverse Tree here
+    }
 }
 
 class Person {
