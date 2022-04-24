330044c281a16407ba278d920b2451dea8ce9b4e
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-24 18:02:00 -0400

    reduce TLE exception

--- CrackingTheCodingInterview/TechnicalQuestions/MinKnightMoves.js
+++ CrackingTheCodingInterview/TechnicalQuestions/MinKnightMoves.js
@@ -3,6 +3,7 @@ var minKnightMoves = function(x, y) {
     let queue = [];
     queue.push([0,0])
     let moves = 0;
+    let seen = new Array(607).fill(false).map(() => new Array(607).fill(false));
     while (queue.length) {
         let count = 0;
         let len = queue.length;
@@ -10,11 +11,17 @@ var minKnightMoves = function(x, y) {
             let curr = queue.shift();
             let currX = curr[0]
             let currY = curr[1];
+            seen[currX+302][currY+302] = true;
             if (currX === x && currY === y) return moves;
             for (const dir of knightMoves) {
                 if (currX + dir[0] === x && currY + dir[1] === y) return moves+1;
-                queue.push([currX + dir[0], currY + dir[1]]);
+                if (!seen[currX + dir[0] + 302][currY + dir[1] + 302]) {
+                    seen[currX + dir[0] + 302][currY + dir[1] + 302] = true;
+                    queue.push([currX + dir[0], currY + dir[1]]);
+                }
+                
             }
+
             count++;
         }
         moves++;
