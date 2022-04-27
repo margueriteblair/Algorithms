889d80fbcc0a34741faf9ec1b3f3c6e073760471
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-26 20:11:58 -0400

    non overlapping intervals

--- NonOverlappingIntervals.js
+++ NonOverlappingIntervals.js
@@ -20,3 +20,23 @@ var eraseOverlapIntervals = function(intervals) {
     
     return _backtrack(-1, 0, intervals);
 };
+
+var eraseOverlapIntervals = function(intervals) {
+    intervals.sort((a, b) => {return a[0] - b[0]}); //sort by starting index
+    let dp = new Array(intervals.length);
+    if (intervals.length === 0) return 0;
+    
+    dp[0] = 1; //saying if we just have one element then the max number of valid intervals is 1
+    let ans = 1;
+    for (let i = 1; i < dp.length; i++) {
+        let max = 0;
+        for (let j = i-1; j >= 0; j--) {
+            if (intervals[j][1] <= intervals[i][0]) {
+                max = Math.max(dp[j], max);
+            }
+        }
+        dp[i] = max + 1;
+        ans = Math.max(ans, dp[i]);
+    }
+    return intervals.length - ans;
+};
