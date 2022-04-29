964fa47d98ba0a4fe7ee5ee1382c732618b99fe8
Marguerite Bliar <marguerite.blair@gmail.com>
2022-04-29 14:00:24 -0400

    hi

---
+++ CrackingTheCodingInterview/TechnicalQuestions/CompleteCrossword.js
@@ -0,0 +1,38 @@
+function completeCrossword(words) {
+    let n = words[0].length;
+    // const crossword = [];
+  
+    function _backtrack(currBoard, currNum) {
+     // console.log(currBoard);
+      if (currNum === n) {
+        if (isValid(currBoard, words)) {
+          return currBoard;
+        } 
+        return;
+      }
+  
+      for (const word of words) {
+        currBoard.push(word);
+        _backtrack(currBoard, currNum+1);
+        currBoard.pop();
+      }
+    }
+  
+    return _backtrack([], 0);
+  }
+  
+  function isValid(crossboard, words) {
+    for (let i = 0; i < crossboard.length; i++) {
+      let currWord = "";
+      for (let j = 0; j < crossboard.length; j++) {
+        currWord += crossboard[j][i];
+      }
+  
+      if (!words.includes(currWord)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  console.log(completeCrossword(["owe", "cot", "cat", "ten", "awe", "car"]));
