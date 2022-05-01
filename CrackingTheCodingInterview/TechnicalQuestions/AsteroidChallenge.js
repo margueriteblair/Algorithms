81c030cdc1db54c370d0ea4ffc77e8a883203a0c
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-01 18:33:18 -0400

    followup

--- CrackingTheCodingInterview/TechnicalQuestions/AsteroidChallenge.js
+++ CrackingTheCodingInterview/TechnicalQuestions/AsteroidChallenge.js
@@ -1,9 +1,5 @@
 var asteroidCollision = function(asteroids) {
     const stack = [];
-    // while (asteroids[i] < 0) {
-    //     stack.push(asteroids[i]);
-    //     i++;
-    // }
     
     for (let i = 0; i < asteroids.length; i++) {
         if (asteroids[i] > 0) {
@@ -12,11 +8,12 @@ var asteroidCollision = function(asteroids) {
             if (stack.length === 0 || stack[stack.length-1] < 0) {
                 stack.push(asteroids[i]);
             } else {
-                while (Math.abs(asteroids[i]) >= stack[stack.length-1]) {
+                while (Math.abs(asteroids[i]) >= stack[stack.length-1] && stack[stack.length-1] > 0) {
                     stack.pop();
                 }
             }
         }
+        console.log(stack)
     }
     return stack;
 };
