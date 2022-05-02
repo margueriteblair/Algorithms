07729ec6beb6a9b70387db1a6166d0aa5f0fd241
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-02 11:40:55 -0400

    single pass using hm

--- CrackingTheCodingInterview/TechnicalQuestions/MaxSubarraysDivisibleByK.js
+++ CrackingTheCodingInterview/TechnicalQuestions/MaxSubarraysDivisibleByK.js
@@ -9,3 +9,26 @@ var subarraysDivByK = function(nums, k) {
     }
     return count;
 };
+
+var subarraysDivByK2 = function(nums, k) {
+    const map = new Map();
+    map.set(0, 1);
+    let count = 0;
+    let currSum = 0;
+    //[4,5,0,-2,-3,1] k = 5
+    //{0: 1, 4: 2, 9: 2, 7: 1, 5: 1}
+    for (let i = 0; i < nums.length; i++) {
+        currSum += nums[i];
+        let rem = currSum % k;
+        if (rem < 0) {
+            rem += k;
+        }
+        if (map.has(rem)) {
+            count += map.get(rem);
+            map.set(rem, map.get(rem)+1);
+        } else {
+            map.set(rem, 1);
+        }
+    }
+    return count;
+};
