40f72ea23e097967684409924c62fecbffb5a042
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-03 10:33:57 -0400

    top k freq elems

---
+++ CrackingTheCodingInterview/TechnicalQuestions/TopKFreqElems.js
@@ -0,0 +1,19 @@
+var topKFrequent = function(nums, k) {
+    let map = new Map();
+    for (const num of nums) {
+        map.set(num, map.get(num)+1 || 1);
+    }
+    const mapArr = [];
+    for (const [num, freq] of map) {
+        mapArr.push([num, freq]);
+    }
+    
+    mapArr.sort((a, b) => { return a[1] - b[1]});
+    console.log(mapArr);
+    
+    const res = [];
+    for (let i = mapArr.length-1; i > mapArr.length-1-k; i--) {
+        res.push(mapArr[i][0]);
+    }
+    return res;
+};
