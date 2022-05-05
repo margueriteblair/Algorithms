937fc92cbb39579d8c7ad3d30795267dd1f399f0
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-05 14:13:26 -0400

    least bricks in wall

---
+++ LeastBricksInWall.java
@@ -0,0 +1,17 @@
+    public int leastBricks(List<List<Integer>> wall) {
+        int untouched = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (List<Integer> row : wall) {
+            //prefix sum issue
+            int end = 0;
+            for (int brick = 0; brick < row.size() - 1; brick++) {
+                end += row.get(brick);
+                map.put(end, map.getOrDefault(end, 0) + 1);
+                untouched = Math.max(untouched, map.get(end));
+            }
+        }
+        
+        return wall.size() - untouched;
+        //O(N)
+        //O(N)
+    }
