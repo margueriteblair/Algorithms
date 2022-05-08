d2667121ba0c0959a2001744f6301d42c34d7090
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-08 13:26:28 -0400

    atm

--- OODesign/ATM.java
+++ OODesign/ATM.java
@@ -1,6 +1,41 @@
 package OODesign;
 
+import java.util.*;
+
 public class ATM {
     private int totalBalance;
     private Map<Integer, Integer> denominations; //will count the 1's, 5's, 10's 20's, 50's, 100's
+
+
 }
+
+class User {
+   private Account account;
+   private Card card;
+
+   public User(Account account, Card card) {
+       this.account = account;
+       this.card = card;
+   }
+}
+
+class Account {
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public void updateBalance(double amount) {
+        balance += amount;
+    }
+}
+
+class Card {
+    private int PIN;
+
+    public boolean authenticatePin(int userInputPIN) {
+        return this.PIN == userInputPIN;
+    } 
+}
+
