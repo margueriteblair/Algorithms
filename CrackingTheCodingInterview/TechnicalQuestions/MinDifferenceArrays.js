0f5fb156823791b7530f368254e05620999aa3a2
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-08 10:55:15 -0400

    minimum difference between two arrays

--- CrackingTheCodingInterview/TechnicalQuestions/MinDifferenceArrays.js
+++ CrackingTheCodingInterview/TechnicalQuestions/MinDifferenceArrays.js
@@ -9,7 +9,8 @@ function smallestDifference(arrayOne, arrayTwo) {
       //                    i
       //[15, 17, 26, 134, 135]
       //         j
-  
+      console.log(arrayOne);
+      console.log(arrayTwo);
       let num1 = arrayOne[0];
       let num2 = arrayTwo[0];
       let smallestDiff = Math.abs(num1 - num2);
@@ -17,13 +18,14 @@ function smallestDifference(arrayOne, arrayTwo) {
       let i = 0, j = 0;
       while (i < arrayOne.length && j < arrayTwo.length) {
           let currDiff = Math.abs(arrayOne[i] - arrayTwo[j]);
+          console.log(currDiff);
           if (currDiff < smallestDiff) {
               smallestDiff = currDiff;
               num1= arrayOne[i]
               num2 = arrayTwo[j];
           }
           
-          if (arrayOne[i+1] - arrayOne[i] < arrayTwo[j+1] - arrayTwo[j]) {
+          if (arrayOne[i] < arrayTwo[j]) {
               i++;
           } else {
               j++;
