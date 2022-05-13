5a0f99a9c31c457ea00fd4a7a3346af826e670ab
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-12 20:26:52 -0400

    another more manual caesars cipher

--- CrackingTheCodingInterview/CaesarCipher.js
+++ CrackingTheCodingInterview/CaesarCipher.js
@@ -8,3 +8,16 @@ function caesarCipherEncryptor(string, key) {
       
       return charArr.join("");
   }
+
+  function caesarCipherEncryptor(string, key) {
+    // Write your code here.
+      //String.fromCharCode((c.charCodeAt(0)-65 + key ) % 26 + 65))
+      const newLetters = [];
+      const newKey = key % 26;
+      const alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+      for (const char of string) {
+          const newLetterCode = alphabet.indexOf(char) + key;
+          newLetters.push(alphabet[newLetterCode % 26]);
+      }
+      return newLetters.join("")
+  }
