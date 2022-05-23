51f2d740260decdea036cb8f6f25d7fc0c1a5e5a
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-22 22:27:17 -0400

    merge overlapping

--- CrackingTheCodingInterview/TechnicalQuestions/MergeOverlappingIntervals.js
+++ CrackingTheCodingInterview/TechnicalQuestions/MergeOverlappingIntervals.js
@@ -1,13 +1,13 @@
 function mergeOverlappingIntervals(array) {
     // Write your code here.
       let merged = [];
-      array.sort((a, b) => {a[0] - b[0]});
+      array.sort((a, b) =>  a[0] - b[0] );
       for (let i = 0; i < array.length; i++) {
           let j = i+1;
           let start = array[i][0];
           let end = array[i][1];
           while (j < array.length && array[j][0] <= end) {
-              end = array[j][1];
+              end = Math.max(end, array[j][1]);
               j++;
           }
           
