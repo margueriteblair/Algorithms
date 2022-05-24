9d14693bd600e98d7b87f5784bcfc833a6c947ec
Marguerite Bliar <marguerite.blair@gmail.com>
2022-05-24 13:45:35 -0400

    index equals 2

--- CrackingTheCodingInterview/TechnicalQuestions/IndexEqualsValue.js
+++ CrackingTheCodingInterview/TechnicalQuestions/IndexEqualsValue.js
@@ -7,3 +7,24 @@ function indexEqualsValue(array) {
     }
     return -1;
   }
+
+function indexEqualsValue2(array) {
+    // Write your code here.
+   return binarySearch(array, 0, array.length-1);
+  }
+  
+  function binarySearch(array, left, right) {
+    if (left > right) return -1;
+  
+    let mid = Math.floor((left+right)/2);
+  
+    if (array[mid] < mid) {
+      return binarySearch(array, mid+1, right);
+    } else if (array[mid] === mid && mid === 0) {
+      return mid;
+    } else if (array[mid] === mid && array[mid-1] < mid-1) {
+      return mid;
+    } else {
+      return binarySearch(array, left, mid-1);
+    }
+  }
