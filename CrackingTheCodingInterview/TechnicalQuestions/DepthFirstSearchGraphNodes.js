fc937cda551a9a33a746ba9df90087ea22c1ddf7
Marguerite Bliar <marguerite.blair@gmail.com>
2022-06-02 11:11:59 -0400

    depth first search of a graph node

---
+++ CrackingTheCodingInterview/TechnicalQuestions/DepthFirstSearchGraphNodes.js
@@ -0,0 +1,22 @@
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      // Write your code here.
+      if (this) {
+        array.push(this.name);
+        for (let i = 0; i < this.children.length; i++) {
+          this.children[i].depthFirstSearch(array);
+        }
+      }
+      return array;
+    }
+  }
